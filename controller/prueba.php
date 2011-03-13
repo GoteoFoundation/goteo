9e797e3f51b46fc88f2047c3a6aa1f9c49a967aa
Philipp Keweloh <philipp@keweloh.info>
2011-03-13 20:25:36 +0100

    Probando controlladores

---
+++ controller/prueba.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Goteo\Controller {
+	
+	class Prueba extends \Goteo\Core\Controller {
+		
+		
+		public function index () {
+			
+			echo "Hey!";
+			
+			
+		}
+		
+		
+	}
+	
+}
