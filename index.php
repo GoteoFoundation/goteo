0da766e0605efd626ea4055b452603f4279468a5
Philipp Keweloh <philipp@keweloh.info>
2011-03-13 19:19:44 +0100

    Hey!!!

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+use Goteo\Core\Error,
+    Goteo\Core\Redirection;
+
+require_once 'config.php';
+
+// Include path
+set_include_path(GOTEO_PATH . PATH_SEPARATOR . '.');
+
+// Autoloader
+spl_autoload_register(
+            
+    function ($cls) {
+            
+        $file = __DIR__ . '/' . implode('/', explode('\\', strtolower(substr($cls, 6)))) . '.php';
+    
+        $file = realpath($file);
+        
+        if ($file === false) {
+            
+            // Try in library
+            $file = __DIR__ . '/library/' . implode('/', explode('\\', strtolower(substr($cls, 6)))) . '.php';
+            
+        }
+        
+        if ($file !== false) {        
+            include $file;
+        }
+    
+    }
+    
+);
+
+// Error handler
+set_error_handler (
+        
+    function ($errno, $errstr, $errfile, $errline, $errcontext) {
+    
+        // Insert error into buffer
+        
+    }
+    
+);
+
+// This may not work on other systems
+$uri = substr($_SERVER['PHP_SELF'], strlen($_SERVER['SCRIPT_NAME']));
+
+// Get requested segments
+$segments = preg_split('!\s*/+\s*!', $uri, -1, \PREG_SPLIT_NO_EMPTY);
+
+try {  
+    
+    // Get controller name    
+    if (!empty($segments) && class_exists("Goteo\\Controller\\{$segments[0]}")) {
+        // Take first segment as controller        
+        $controller = array_shift($segments);        
+    } else {
+        $controller = 'index';
+    }
+        
+    // Continue
+    try {
+        
+        $class = new ReflectionClass("Goteo\\Controller\\{$controller}");
+        
+        if (!empty($segments) && $class->hasMethod($segments[0])) {
+            $method = array_shift($segments);                        
+        } else {
+            // Try default method
+            $method = 'index';            
+        }         
+        
+        // ReflectionMethod
+        $method = $class->getMethod($method);
+        
+        // Number of params defined in method
+        $numParams = $method->getNumberOfParameters(); 
+        // Number of required params
+        $reqParams = $method->getNumberOfRequiredParameters();        
+        // Given params
+        $gvnParams = count($segments);
+                
+        if ($gvnParams >= $reqParams && (!($gvnParams > $numParams && $numParams <= $reqParams))) {
+            
+            // Try to instantiate
+            $instance = $class->newInstance();
+
+            // Start output buffer
+            ob_start();
+
+            // Invoke method
+            $method->invokeArgs($instance, $segments);
+
+            // Get buffer contents
+            $buffer = \ob_get_clean();
+
+            // Output buffer
+            echo $buffer;
+
+            // Farewell
+            die;      
+            
+        }        
+        
+    } catch (\ReflectionException $e) {}
+    
+    throw new Error(Error::NOT_FOUND);    
+    
+} catch (Redirection $redirection) {
+    
+    $url = $error->getURL();
+    $code = $error->getCode();
+    header("Location: {$url}");
+    
+} catch (Error $error) {
+    
+    include "view/error.html.php";
+    
+} catch (Exception $exception) {
+    
+    // Default error (500)    
+    include "view/error.html.php";
+}
