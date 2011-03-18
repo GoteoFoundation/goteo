a3f36ff3f188415676104ab8770359556c1761c3
Philipp Keweloh <philipp@keweloh.info>
2011-03-18 02:08:57 +0100

    - Nueva clase Goteo\Core\DB que extiende a \PDO (extensión php_pdo) - Añadido config.ini - Ajustado modelo base Goteo\Core\Model y añadidas funciones 'get' y 'getAll' en el usuario Goteo\Model\User, provisionales y para que sirvan a modo de ejemplo y de concepto.

--- controller/user.php
+++ controller/user.php
@@ -1,23 +1,33 @@
 <?php
 
 namespace Goteo\Controller {
-    
-    use Goteo\Core\Error,
-        Goteo\Model\User;        
-    
-    class User extends \Goteo\Core\Controller {
-        
-        public function index ($id) {
-            
-            $user = User::get($id);
-            
-            if ($user === false) {
-                throw new Error(404);
-            }
-            
-            include 'view/user/profile.html.php';            
-            
-        }
+
+	use Goteo\Core\Error,
+		Goteo\Model;        
+
+	class User extends \Goteo\Core\Controller {
+		
+		/**
+		 * Usuarios
+		 * <id> = Perfil usuario
+		 * <vacio> = Listado de usuarios
+		 * 
+		 * @param string $id
+		 */
+		public function index ($id = null) {
+			if(empty($id)) {
+				$users = Model\User::getAll();
+				var_dump($users); // @FIXME: Pruebas
+			}
+			else {
+				$user = Model\User::get($id);
+				var_dump($user); // @FIXME: Pruebas
+				if ($user === false) {
+					throw new Error(404);
+				}
+			}
+			include 'view/user/profile.html.php';            
+		}
                         
         public function register () {
             
---
+++ core/db.php
@@ -0,0 +1,20 @@
+<?php
+namespace Goteo\Core {
+
+	class DB extends \PDO {
+
+		public function __construct() {
+			$file = 'config.ini';
+			if (!$settings = parse_ini_file($file, TRUE)) {
+				throw new exception("Unable to open {$file}.");
+			}
+			$dsn = "{$settings['database']['driver']}:host={$settings['database']['host']};dbname={$settings['database']['schema']}";
+			if(!empty($settings['database']['port'])) {
+				$dsn .= ";port={$settings['database']['port']}";
+			}
+			parent::__construct($dsn, $settings['database']['username'], $settings['database']['password']);
+		}
+
+	}
+
+}
