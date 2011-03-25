84923d777e1904cf55f768c2990b01d04993c5e6
Kaime <kaime@kaime.info>
2011-03-25 19:00:45 +0100

    Corregidos los include de la vista `login`.

--- view/user/login.html.php
+++ view/user/login.html.php
@@ -1,3 +1,13 @@
-<?php include 'view/header.html.php' ?>
-<?php echo $content; ?>
-<?php include 'view/footer.html.php' ?>
+<?php $bodyClass = 'home'; include 'view/prologue.html.php' ?>
+
+        <?php include 'view/header.html.php' ?>
+
+        <div id="main">
+
+            <?php echo $content; ?>
+            
+        </div>
+
+        <?php include 'view/footer.html.php' ?>
+    
+<?php include 'view/epilogue.html.php' ?>
