29df5dd0cdd708a13434652babab8d6b925ea034
Kaime <kaime@kaime.info>
2011-04-07 20:24:31 +0200

    - Modificado el método `Model::query()` para que reutilice la conexión a la BBDD. - Añadido el método `Model::insertId()`, que devuelve el último AUTO_INCREMENT.

--- core/model.php
+++ core/model.php
@@ -2,17 +2,14 @@
 
 namespace Goteo\Core {
 
-	use Goteo\Core\Error;
-    
+    use Goteo\Core\Error;
+
     abstract class Model {
-        
-        protected static 
-            $__connections = array();
 
         /**
          * Constructor.
          */
-        public function __construct () {
+        public function __construct() {
             if (\func_num_args() >= 1) {
                 $data = \func_get_arg(0);
                 if (is_array($data) || is_object($data)) {
@@ -22,68 +19,95 @@ namespace Goteo\Core {
                 }
             }
         }
-        
+
         /**
          * Obtener.
-         * @param   type mixed  $id     Identificador
-         * @return  type object         Objeto
+         * @param   mixed  $id     Identificador
+         * @return  object         Objeto
          */
-        abstract static public function get ($id);
-        
+        abstract static public function get($id);
+
         /**
-		 * Guardar.
-         * @param   type array  $errors     Errores devueltos pasados por referencia.
-         * @return  type bool   true|false
+         * Guardar.
+         * @param   array  $errors     Errores devueltos pasados por referencia.
+         * @return  bool   true|false
          */
-         abstract public function save (&$errors = array());
-        
+        abstract public function save(&$errors = array());
+
         /**
          * Validar.
-         * @param   type array  $errors     Errores devueltos pasados por referencia.
-         * @return  type bool   true|false
+         * @param   array  $errors     Errores devueltos pasados por referencia.
+         * @return  bool   true|false
          */
-        abstract public function validate (&$errors = array());
-        
+        abstract public function validate(&$errors = array());
+
         /**
          * Consulta.
          * Devuelve un objeto de la clase PDOStatement
          * http://www.php.net/manual/es/class.pdostatement.php
          * 
-         * @param   type string $query      Consulta SQL
-         * @param   type array  $params     Parámetros
-         * $return  type object PDOStatement
+         * @param   string $query      Consulta SQL
+         * @param   array  $params     Parámetros
+         * @return  object PDOStatement
          */
-        public static function query ($query, $params = null) {
-			$db = new DB;
-			$result = $db->prepare($query);
-			$params = func_num_args() === 2 && is_array($params) ? $params : array_slice(func_get_args(), 1);
-			if ($result->execute($params)) {
-				return $result;
-			}
-            else {
-                die("FAIL!! $query <pre>" . print_r($params, 1) . "</pre>");
+        public static function query($query, $params = null) {
+            
+            static $db = null;
+            
+            if ($db === null) {
+                $db = new DB;
             }
-			throw new Exception($query);
+            
+            $params = func_num_args() === 2 && is_array($params) ? $params : array_slice(func_get_args(), 1);            
+            $result = $db->prepare($query);
+            
+            try {
+                
+                $result->execute($params);
+                
+                return $result;
+                
+            } catch (\PDOException $e) {
+                throw new Exception("Error en la consulta: `{$query}`");
+            }
+            
+        }
+        
+        /**
+         * Devuelve el id autoincremental generado en la última consulta, si se 
+         * ha generado uno.
+         * 
+         * @return  int Id de `AUTO_INCREMENT` o `0`, si la última consulta no
+         *          ha generado ninguna valor autoincremental.
+         */
+        public static function insertId() {
+            
+            try {
+                return static::query("SELECT LAST_INSERT_ID();")->fetchColumn();
+            } catch (\Exception $e) {
+                return 0;
+            }
+            
         }
 
-		/**
-		 * Formato.
-		 * Formatea una cadena para ser usada como id varchar(50)
-		 *
-		 * @param string $value
-		 * @return string $id
-		 */
-		public static function idealiza ($value) {
-			$id = trim(strtolower($value));
-			// Acentos
-			$id = strtr($id, "ÁÀÄÂáàâäÉÈËÊéèêëÍÌÏÎíìîïÓÒÖÔóòôöÚÙÛÜúùûüÇçÑñ", "aaaaaaaaeeeeeeeeiiiiiiiioooooooouuuuuuuuccnn");
-			// Separadores
-			$id = preg_replace("/[\s\,\;\_\/\-]+/i", "-", $id);
-			$id = preg_replace("/[^a-z0-9\.\-\+]/", "", $id);
-			$id = substr($id, 0, 50);
+        /**
+         * Formato.
+         * Formatea una cadena para ser usada como id varchar(50)
+         *
+         * @param string $value
+         * @return string $id
+         */
+        public static function idealiza($value) {
+            $id = trim(strtolower($value));
+            // Acentos
+            $id = strtr($id, "ÁÀÄÂáàâäÉÈËÊéèêëÍÌÏÎíìîïÓÒÖÔóòôöÚÙÛÜúùûüÇçÑñ", "aaaaaaaaeeeeeeeeiiiiiiiioooooooouuuuuuuuccnn");
+            // Separadores
+            $id = preg_replace("/[\s\,\;\_\/\-]+/i", "-", $id);
+            $id = preg_replace("/[^a-z0-9\.\-\+]/", "", $id);
+            $id = substr($id, 0, 50);
 
-			return $id;
-		}
+            return $id;
+        }
 
     }
 
