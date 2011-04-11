118a1955d960b1d0a2eab32b3e59b56c33bcd9aa
Julian <onliners-github@doukeshi.org>
2011-04-11 21:20:43 +0200

    codigo y arreglos para comprobar que la grabacion del formulario de proyecto funciona correctamente
    ojo! he añadido un hidden step para saber que datos estoy recibiendo.

--- view/project/overview.html.php
+++ view/project/overview.html.php
@@ -27,6 +27,78 @@ include 'view/prologue.html.php';
 
                 <?php include 'view/project/guide.html.php' ?>
 
+                    <?php //@INTRUSION JULIAN!!! para usarlo sin maquetación
+                    if ($this['nodesign'] == true) :
+                        $project = $this['project'];
+                        $categories = $this['categories'];
+                        $currently = $this['currently'];
+                        ?>
+            <?php if (!empty($project->errors['overview'])) :
+                echo '<p>';
+                foreach ($project->errors['overview'] as $campo=>$error) : ?>
+                    <span style="color:red;"><?php echo "$campo: $error"; ?></span><br />
+            <?php endforeach;
+                echo '</p>';
+                endif;?>
+    <dl>
+        <dt><label for="name">Nombre del proyecto</label></dt>
+        <dd><input type="text" id="name" name="name" value="<?php echo $project->name; ?>"/></dd>
+        <span><?php echo Text::get('tooltip project name'); ?></span><br />
+
+        <dt><label for="image">Imagen del proyecto</label></dt>
+        <dd><input type="file" id="theimage" name="theimage" value=""/> img src="<?php echo $project->image; ?>" </dd>
+        <input type="text" name="image" value="project.jpg" />
+        <span><?php echo Text::get('tooltip project image'); ?></span><br />
+
+        <dt><label for="description">Descripción</label></dt>
+        <dd><textarea id="description" name="description" cols="50" rows="5"><?php echo $project->description; ?></textarea></dd>
+        <span><?php echo Text::get('tooltip project description'); ?></span><br />
+
+        <dt><label for="motivation">Motivación</label></dt>
+        <dd><textarea id="motivation" name="motivation" cols="50" rows="5"><?php echo $project->motivation; ?></textarea></dd>
+        <span><?php echo Text::get('tooltip project motivation'); ?></span><br />
+
+        <dt><label for="about">Qué és</label></dt>
+        <dd><textarea id="about" name="about" cols="50" rows="5"><?php echo $project->about; ?></textarea></dd>
+        <span><?php echo Text::get('tooltip project about'); ?></span><br />
+
+        <dt><label for="goal">Objetivos</label></dt>
+        <dd><textarea id="goal" name="goal" cols="50" rows="5"><?php echo $project->goal; ?></textarea></dd>
+        <span><?php echo Text::get('tooltip project goal'); ?></span><br />
+
+        <dt><label for="related">Experiencia relacionada y equipo</label></dt>
+        <dd><textarea id="related" name="related" cols="50" rows="5"><?php echo $project->related; ?></textarea></dd>
+        <span><?php echo Text::get('tooltip project related'); ?></span><br />
+
+        <dt>Categorías</dt>
+        <dd><?php foreach ($categories as $Id => $Val) : ?>
+            <label><input type="checkbox" id="categories" name="categories[]" value="<?php echo $Id; ?>" <?php if (in_array($Id, $project->categories)) echo ' checked="checked"'; ?> /><?php echo $Val; ?></label>
+            <?php endforeach; ?>
+        </dd>
+        <span><?php echo Text::get('tooltip project category'); ?></span><br />
+
+        <dt><label for="keywords">Palabras clave</label></dt>
+        <dd>Añadir:<input type="text" id="keywords" name="keywords" value="<?php echo $project->keywords; ?>"/></dd>
+        <span><?php echo Text::get('tooltip project keywords'); ?> (separadas por comas)</span><br />
+
+        <dt><label for="media">Media</label></dt>
+        <dd><input type="text" id="media" name="media" value="<?php echo $project->media; ?>"/></dd>
+        <span><?php echo Text::get('tooltip project media'); ?> (url del video en vimeo o youtube)</span><br />
+
+        <dt><label for="currently">Estado del proyecto</label></dt>
+        <dd><select id="currently" name="currently">
+        <?php foreach ($currently as $Id => $Val) : ?>
+                <option value="<?php echo $Id; ?>" <?php if ($project->currently == $Id) echo ' selected="selected"'; ?>><?php echo $Val; ?></option>
+        <?php endforeach; ?>
+            </select>
+        </dd>
+        <span><?php echo Text::get('tooltip project currently'); ?> (estado de desarrollo actual del proyecto)</span><br />
+
+        <dt><label for="project_location">Localización</label></dt>
+        <dd><input type="text" id="project_location" name="project_location" value="<?php echo $project->project_location; ?>"/></dd>
+        <span><?php echo Text::get('tooltip project project_location'); ?> (Si es solo online poner "online")</span><br />
+    </dl>
+                    <?php else : ?>
                 <div class="fields">
                     
                     <ol class="fields">
@@ -39,7 +111,7 @@ include 'view/prologue.html.php';
                                 </div>
                                 <div class="elements"><input class="main" type="text" name="name" value="<?php if (isset($project->name)) echo htmlspecialchars($project->name) ?>" /></div>
                                 
-                            </dov>
+                            </div>
                         </li>
 
                         <li class="field" id="field-image">
@@ -195,9 +267,11 @@ include 'view/prologue.html.php';
                     </ol>
                     
                 </div>
+                    <?php endif; ?>
 
                 <div class="buttons">
-                    <input class="next" type="submit" name="submit" value="Continuar"  />
+                    <input type="hidden" name="step" value="overview" />
+                    <input class="next" type="submit" name="view-step-costs" value="Continuar"  />
                 </div>
 
             </div>
@@ -213,73 +287,3 @@ include 'view/prologue.html.php';
     <?php include 'view/footer.html.php' ?>
     
 <?php include 'view/epilogue.html.php' ?>
-
-<!--
-
-PROYECTO / Descripción<br />
-GUÍA: <?php echo $guideText; ?><br />
-<hr />
-<form action="/project/edit" method="post">
-    <dl>
-        <dt><label for="name">Nombre del proyecto</label></dt>
-        <dd><input type="text" id="name" name="name" value="<?php echo $project->name; ?>"/></dd>
-        <span><?php echo Text::get('tooltip project name'); ?></span><br />
-
-        <dt><label for="image">Imagen del proyecto</label></dt>
-        <dd><input type="file" id="theimage" name="theimage" value=""/> img src="<?php echo $project->image; ?>" </dd>
-        <input type="text" name="image" value="project.jpg" />
-        <span><?php echo Text::get('tooltip project image'); ?></span><br />
-
-        <dt><label for="description">Descripción</label></dt>
-        <dd><textarea id="description" name="description" cols="50" rows="5"><?php echo $project->description; ?></textarea></dd>
-        <span><?php echo Text::get('tooltip project description'); ?></span><br />
-
-        <dt><label for="motivation">Motivación</label></dt>
-        <dd><textarea id="motivation" name="motivation" cols="50" rows="5"><?php echo $project->motivation; ?></textarea></dd>
-        <span><?php echo Text::get('tooltip project motivation'); ?></span><br />
-
-        <dt><label for="about">Qué és</label></dt>
-        <dd><textarea id="about" name="about" cols="50" rows="5"><?php echo $project->about; ?></textarea></dd>
-        <span><?php echo Text::get('tooltip project about'); ?></span><br />
-
-        <dt><label for="goal">Objetivos</label></dt>
-        <dd><textarea id="goal" name="goal" cols="50" rows="5"><?php echo $project->goal; ?></textarea></dd>
-        <span><?php echo Text::get('tooltip project goal'); ?></span><br />
-
-        <dt><label for="related">Experiencia relacionada y equipo</label></dt>
-        <dd><textarea id="related" name="related" cols="50" rows="5"><?php echo $project->related; ?></textarea></dd>
-        <span><?php echo Text::get('tooltip project related'); ?></span><br />
-
-        <dt>Categorías</dt>
-        <dd><?php foreach ($categories as $Id => $Val) : ?>
-            <label><input type="checkbox" id="categories" name="categories[]" value="<?php echo $Id; ?>" <?php if (in_array($Id, $project->categories))
-                echo ' checked="checked"'; ?> /><?php echo $Val; ?></label>
-            <?php endforeach; ?>
-        </dd>
-        <span><?php echo Text::get('tooltip project category'); ?></span><br />
-
-        <dt><label for="keywords">Palabras clave</label></dt>
-        <dd>Añadir:<input type="text" id="keywords" name="keywords" value="<?php echo $project->keywords; ?>"/></dd>
-        <span><?php echo Text::get('tooltip project keywords'); ?> (separadas por comas)</span><br />
-
-        <dt><label for="media">Media</label></dt>
-        <dd><input type="text" id="media" name="media" value="<?php echo $project->media; ?>"/></dd>
-        <span><?php echo Text::get('tooltip project media'); ?> (url del video en vimeo o youtube)</span><br />
-
-        <dt><label for="currently">Estado del proyecto</label></dt>
-        <dd><select id="currently" name="currently">
-        <?php foreach ($currently as $Id => $Val) : ?>
-                <option value="<?php echo $Id; ?>" <?php if ($project->currently == $Id) echo ' selected="selected"'; ?>><?php echo $Val; ?></option>
-        <?php endforeach; ?>
-            </select>
-        </dd>
-        <span><?php echo Text::get('tooltip project currently'); ?> (estado de desarrollo actual del proyecto)</span><br />
-
-        <dt><label for="project_location">Localización</label></dt>
-        <dd><input type="text" id="project_location" name="project_location" value="<?php echo $project->project_location; ?>"/></dd>
-        <span><?php echo Text::get('tooltip project project_location'); ?> (Si es solo online poner "online")</span><br />
-    </dl>
-
-    <input type="submit" name="submit" value="CONTINUAR" />
-</form>
--->
