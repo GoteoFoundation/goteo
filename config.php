c9ff044ca007053a74a35aa7878fc49530e40fd6
Kaime <kaime@kaime.info>
2011-04-11 18:52:07 +0200

    Revertidos los cambios en config.php@19037

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ define('GOTEO_DB_DRIVER', 'mysql');
 define('GOTEO_DB_HOST', 'localhost');
 define('GOTEO_DB_PORT', 3306);
 define('GOTEO_DB_SCHEMA', 'goteo');
-define('GOTEO_DB_USERNAME', 'root');
-define('GOTEO_DB_PASSWORD', 'takoika371');
+define('GOTEO_DB_USERNAME', 'goteo');
+define('GOTEO_DB_PASSWORD', 'goteo1234');
 
