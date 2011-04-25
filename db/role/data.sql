451193b4d4b6cf740237fb659410057a83531fd0
Julian <onliners-github@doukeshi.org>
2011-04-25 11:43:02 +0200

    Por favor, al crear tablas y campo usar InnoDB y collation uft8_general_ci.

--- db/role/data.sql
+++ db/role/data.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 04-04-2011 a las 15:51:35
--- Versión del servidor: 5.1.36
--- Versión de PHP: 5.3.0
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de datos: `goteo`
---
-
---
--- Volcar la base de datos para la tabla `role`
---
-
 INSERT INTO `role` (`id`, `name`) VALUES
 (1, 'Admin'),
 (2, 'Admin de Nodo'),
--- db/role/structure.sql
+++ db/role/structure.sql
@@ -1,32 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 04-04-2011 a las 15:51:43
--- Versión del servidor: 5.1.36
--- Versión de PHP: 5.3.0
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de datos: `goteo`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `role`
---
-
 CREATE TABLE IF NOT EXISTS `role` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=4 ;
