87614204b17a9e7b443e26e060979acaabe34782
Philipp Keweloh <philipp@keweloh.info>
2011-04-29 22:42:14 +0200

    Tabla 'user' actualizada

--- db/user/structure.sql
+++ db/user/structure.sql
@@ -1,21 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 29-04-2011 a las 20:41:24
+-- Versión del servidor: 5.1.36
+-- Versión de PHP: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `goteo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` varchar(50) COLLATE utf8_general_ci NOT NULL,
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `role_id` int(10) unsigned NOT NULL,
-  `name` varchar(100) COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(40) COLLATE utf8_general_ci NOT NULL,
-  `about` text COLLATE utf8_general_ci,
-  `keywords` tinytext COLLATE utf8_general_ci,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `about` text COLLATE utf8_unicode_ci,
+  `keywords` tinytext COLLATE utf8_unicode_ci,
   `active` tinyint(1) NOT NULL,
   `avatar` int(11) DEFAULT NULL,
-  `contribution` text COLLATE utf8_general_ci,
-  `twitter` tinytext COLLATE utf8_general_ci,
-  `facebook` tinytext COLLATE utf8_general_ci,
-  `linkedin` tinytext COLLATE utf8_general_ci,
+  `contribution` text COLLATE utf8_unicode_ci,
+  `twitter` tinytext COLLATE utf8_unicode_ci,
+  `facebook` tinytext COLLATE utf8_unicode_ci,
+  `linkedin` tinytext COLLATE utf8_unicode_ci,
   `worth` int(7) DEFAULT NULL,
   `created` timestamp NULL DEFAULT NULL,
   `modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `token` tinytext COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   KEY `user_FK` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
