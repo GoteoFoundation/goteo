4618909b826b4b99bca60aa9fb3a0eba93b1b4f3
Kaime <kaime@kaime.info>
2011-05-02 18:48:25 +0200

    Recomensas individuales

--- view/project/edit/rewards.html.php
+++ view/project/edit/rewards.html.php
@@ -6,14 +6,26 @@ use Goteo\Library\Text,
 
 $project = $this['project'];
 
+
 $social_rewards_types = array();
 $social_rewards_licenses = array();
 $social_rewards = array();
 
+$individual_rewards = array();
+$individual_rewards_types = array();
+
 foreach ($this['stypes'] as $id => $type) {
     $social_rewards_types[] = array(
         'value' => $id,
-        'class' => "social_{$id}",
+        'class' => "reward_{$id} social_{$id}",
+        'label' => $type
+    );
+}
+
+foreach ($this['itypes'] as $id => $type) {
+    $individual_rewards_types[] = array(
+        'value' => $id,
+        'class' => "reward_{$id} individual_{$id}",
         'label' => $type
     );
 }
@@ -59,6 +71,62 @@ foreach ($project->social_rewards as $social_reward) {
         );
 }
 
+foreach ($project->individual_rewards as $individual_reward) {
+    
+    $individual_rewards["individual_reward-{$individual_reward->id}"] = array(
+            'type'      => 'group',      
+            'class'     => 'reward individual_reward',
+            'children'  => array(                         
+                "individual_reward-{$individual_reward->id}-reward" => array(
+                    'title'     => 'Resumen',
+                    'type'      => 'textbox',
+                    'size'      => 100,
+                    'class'     => 'inline',
+                    'value'     => $individual_reward->reward,
+                    'hint'      => Text::get('tooltip-project-individual_reward-reward'),
+                ),
+                "individual_reward-{$individual_reward->id}-icon" => array(
+                    'title'     => 'Tipo',
+                    'class'     => 'inline  reward-type',
+                    'type'      => 'radios',
+                    'options'   => $individual_rewards_types,
+                    'value'     => $individual_reward->icon,
+                    'hint'      => Text::get('tooltip-individual_reward-social_reward-icon'),
+                ),
+                "individual_reward-{$individual_reward->id}-description" => array(
+                    'type'      => 'textarea',
+                    'title'     => 'Descripción',
+                    'cols'      => 100,
+                    'rows'      => 4,
+                    'class'     => 'inline',
+                    'hint'      => Text::get('tooltip-project-individual_reward-description'),
+                    'value'     => $individual_reward->description
+                ),                                    
+                "individual_reward-{$individual_reward->id}-amount" => array(
+                    'title'     => 'Cantidad mínima',
+                    'type'      => 'textbox',
+                    'size'      => 5,
+                    'class'     => 'inline reward-amount',
+                    'value'     => $individual_reward->amount,
+                    'hint'      => Text::get('tooltip-project-individual_reward-amount'),
+                ),
+                "individual_reward-{$individual_reward->id}-units" => array(
+                    'title'     => 'Unidades',
+                    'type'      => 'textbox',
+                    'size'      => 5,
+                    'class'     => 'inline reward-units',
+                    'value'     => $individual_reward->units,
+                    'hint'      => Text::get('tooltip-project-individual_reward-units'),
+                ),
+                "individual_reward-{$individual_reward->id}-remove" => array(
+                    'type'  => 'submit',
+                    'label' => 'Quitar',
+                    'class' => 'inline remove reward-remove'
+                )
+            )
+        );
+}
+
 echo new SuperForm(array(
 
     'action'        => '',
@@ -77,25 +145,33 @@ echo new SuperForm(array(
     ),    
     'elements'      => array(
         
-        'social' => array(
+        'social_rewards' => array(
             'type'      => 'group',
             'title'     => 'Retornos colectivos',
-            'hint'      => Text::get('tooltip-project-social_reward'),
-            'class'     => 'rewards social_rewards',
+            'hint'      => Text::get('tooltip-project-social_rewards'),
+            'class'     => 'rewards',
             'children'  => $social_rewards + array(
                 'social_reward-add' => array(
                     'type'  => 'submit',
                     'label' => 'Añadir',
-                    'class' => 'add',
+                    'class' => 'add reward-add',
                 )
             )
         ),
         
-        'nsocial' => array(
+        'individual_rewards' => array(
             'type'      => 'group',
             'title'     => 'Recompensas individuales',
-            'hint'      => Text::get('tooltip-project-individual_reward'),
-        ),          
+            'hint'      => Text::get('tooltip-project-individual_rewards'),
+            'class'     => 'rewards',
+            'children'  => $individual_rewards + array(
+                'individual_reward-add' => array(
+                    'type'  => 'submit',
+                    'label' => 'Añadir',
+                    'class' => 'add reward-add',
+                )
+            )
+        ),
         
         'schedule' => array(                        
             'title'     => 'Agenda',            
