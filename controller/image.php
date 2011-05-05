dabf225e9ae8a0c07133964ba1993ab1cc368b84
Kaime <kaime@kaime.info>
2011-05-05 17:42:43 +0200

    Añadido método `Controller\Library->upload()`

--- controller/image.php
+++ controller/image.php
@@ -2,15 +2,32 @@
 
 namespace Goteo\Controller {
 
-	use Goteo\Library;
+    use Goteo\Core\Error,
+        Goteo\Library;
 
-	class Image extends \Goteo\Core\Controller {
+    class Image extends \Goteo\Core\Controller {
 
-	    public function index ($id, $width = 200, $height = 200) {
-		    if($image = Library\Image::get($id)) {
-		        $image->display($width, $height);
-		    }
-		}
+        public function index($id, $width = 200, $height = 200) {
+            if ($image = Library\Image::get($id)) {
+                $image->display($width, $height);
+            } else {
+                throw new Error(Error::NOT_FOUND);
+            }
+        }
+        
+        public function upload ($width = 200, $height = 200) {
+            
+            if (!empty($_FILES) && count($_FILES) === 1) {
+                // Do upload
+                $image = new Library\Image(current($_FILES));
+                
+                if ($image->save()) {                    
+                    return $image->getLink($width, $height);
+                }
+                                
+            }
+            
+        }
 
     }
 
