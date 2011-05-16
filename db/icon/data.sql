f5d7b9eb0b0efb8382ae7e1d950bc1671a78d0aa
Julian <onliners-github@doukeshi.org>
2011-05-16 14:05:23 +0200

    Estructura DB para gestión de iconos, licencias y sus relaciones. * Incluye absolute.sql

--- db/icon/data.sql
+++ db/icon/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('code', 'Código fuente', 'description-icon-code', 'social');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('file', 'Archivos digitales', 'description-icon-file', NULL);
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('manual', 'Manuales', 'description-icon-manual', 'social');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('money', 'Dinero', 'description-icon-money', 'individual');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('other', 'Otro', 'description-icon-other', NULL);
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('product', 'Producto', 'description-icon-product', 'individual');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('service', 'Servicios', 'description-icon-service', NULL);
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('code', 'Código fuente', 'Código fuente', 'social');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('file', 'Archivos digitales', 'Archivos digitales', NULL);
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('manual', 'Manuales', 'Manuales', 'social');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('money', 'Dinero', 'Dinero', 'individual');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('other', 'Otro', 'Otro', NULL);
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('product', 'Producto', 'Producto', 'individual');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('service', 'Servicios', 'Servicios', NULL);
--- db/license/structure.sql
+++ db/license/structure.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `license` (
 `id` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 `name` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`description` VARCHAR(50) NOT NULL DEFAULT 'description-license-default' COMMENT 'id del texto que lo describe',
+`description` TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci,
 `group` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT 'grupo de restriccion de menor a mayor',
+`order` TINYINT NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT = 'Licencias de distribucion';
