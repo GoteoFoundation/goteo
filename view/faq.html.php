ba9cc41e8f3b841ec81722e05abedca01bffdd20
Julian <onliners-github@doukeshi.org>
2011-05-17 14:44:59 +0200

    Página de preguntas frecuentes

---
+++ view/faq.html.php
@@ -0,0 +1,40 @@
+<?php $bodyClass = 'faq'; include 'view/prologue.html.php' ?>
+
+        <?php include 'view/header.html.php' ?>
+
+        <div id="main">
+
+            <h2><?php echo $this['name']; ?></h2>
+            <p><?php echo $this['title']; ?></p>
+
+            <div id="content"><?php echo $this['content']; ?></div>
+
+            <?php foreach ($this['sections'] as $sectionId=>$sectionName) : ?>
+                <div>
+                    <h3><?php echo $sectionName; ?></h3>
+                    <ol>
+                        <?php foreach ($this['faqs'][$sectionId] as $question)  : ?>
+                            <li><a href="#q<?php echo $question->id; ?>"><?php echo $question->title; ?></a></li>
+                        <?php endforeach; ?>
+                    </ol>
+                </div>
+            <?php endforeach; ?>
+
+            <?php foreach ($this['sections'] as $sectionId=>$sectionName) : ?>
+                <div>
+                    <h3><?php echo $sectionName; ?></h3>
+                    <?php foreach ($this['faqs'][$sectionId] as $question)  : ?>
+                        <div>
+                            <a name="q<?php echo $question->id; ?>" />
+                            <h4><?php echo $question->title; ?></h4>
+                            <blockquote><?php echo $question->description; ?></blockquote>
+                            <a href="#">Subir</a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
+        </div>        
+
+        <?php include 'view/footer.html.php' ?>
+    
+<?php include 'view/epilogue.html.php' ?>
