7c3b3a1fb78b8a256bfe8115ef304366346a76ae
Julian <onliners-github@doukeshi.org>
2011-05-18 18:51:15 +0200

    Actualización de Estructura y registros para iconos, licencias y sus relaciones * más absolute * Incluye ACL base en db/acl/data.sql  y en absolute

--- db/acl/data.sql
+++ db/acl/data.sql
@@ -1,3 +1,32 @@
-INSERT INTO `acl` (`id`, `node_id`, `role_id`, `user_id`, `url`, `allow`, `timestamp`) VALUES
-(1, '*', '*', '*', '*', 1, '2011-05-17 20:51:07'),
-(3, '*', '*', '*', '/admin', 1, '2011-05-17 22:04:22');
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb5build0.10.10.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 18-05-2011 a las 18:52:12
+-- Versión del servidor: 5.1.49
+-- Versión de PHP: 5.3.3-1ubuntu9.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `goteo`
+--
+
+--
+-- Volcar la base de datos para la tabla `acl`
+--
+
+INSERT INTO `acl` VALUES(1, '*', 'public', '*', '*', 1, '2011-05-18 16:45:40');
+INSERT INTO `acl` VALUES(2, '*', 'user', '*', '*', 1, '2011-05-18 16:45:40');
+INSERT INTO `acl` VALUES(3, '*', 'root', '*', '*', 1, '2011-05-18 16:45:40');
+INSERT INTO `acl` VALUES(4, '*', '*', '*', '/admin/*', 0, '2011-05-18 16:45:40');
+INSERT INTO `acl` VALUES(5, '*', 'public', '*', '/dashboard/*', 0, '2011-05-18 16:45:40');
+INSERT INTO `acl` VALUES(6, '*', 'root', '*', '/admin/*', 1, '2011-05-18 16:45:40');
+INSERT INTO `acl` VALUES(7, '*', 'public', '*', '/user/edit', 0, '2011-05-18 16:49:36');
--- db/icon/data.sql
+++ db/icon/data.sql
@@ -1,7 +1,33 @@
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('code', 'Código fuente', 'Código fuente', 'social');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('file', 'Archivos digitales', 'Archivos digitales', NULL);
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('manual', 'Manuales', 'Manuales', 'social');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('money', 'Dinero', 'Dinero', 'individual');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('other', 'Otro', 'Otro', NULL);
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('product', 'Producto', 'Producto', 'individual');
-INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('service', 'Servicios', 'Servicios', NULL);
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb5build0.10.10.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 18-05-2011 a las 18:46:15
+-- Versión del servidor: 5.1.49
+-- Versión de PHP: 5.3.3-1ubuntu9.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `goteo`
+--
+
+--
+-- Volcar la base de datos para la tabla `icon`
+--
+
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('code', 'CÃ³digo fuente', '', 'social');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('design', 'DiseÃ±o', '', 'social');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('file', 'Archivos digitales', '', NULL);
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('manual', 'Manuales', '', 'social');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('money', 'Dinero', '', 'individual');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('other', 'Otro', '', NULL);
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('product', 'Producto', '', 'individual');
+INSERT INTO `icon` (`id`, `name`, `description`, `group`) VALUES('service', 'Servicios', '', NULL);
