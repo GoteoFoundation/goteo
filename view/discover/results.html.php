ee18d6797779baa305dfeedcba0e35d1afc450a2
Julian <onliners-github@doukeshi.org>
2011-05-28 15:45:39 +0200

    Aplicando el widget de proyecto (sin ballon) en el descubre proyectos

--- view/discover/results.html.php
+++ view/discover/results.html.php
@@ -1,34 +1,36 @@
-<?php $bodyClass = 'home'; include 'view/prologue.html.php' ?>
+<?php
 
-        <?php include 'view/header.html.php' ?>
+use Goteo\Core\View;
+
+$bodyClass = 'home';
+
+include 'view/prologue.html.php';
+
+include 'view/header.html.php' ?>
 
         <div id="main">
-            <h2>Proyectos encontrados</h2>
 
             <p><?php echo $this['message']; ?></p>
 
-            <?php if (isset($this['results'])) : ?>
-                <?php foreach ($this['results'] as $result) :
-                    // la instancia del proyecto es $result
-                    // se pintan con el mismo widget que en portada
+            <div class="widget projects promos">
+                <h2 class="title">Resultado de búsqueda</h2>
+
+                <?php if (isset($this['results'])) : ?>
+                    <?php foreach ($this['results'] as $result) : ?>
+                    <div>
+                    <?php
+                        // la instancia del proyecto es $result
+                        // se pintan con el mismo widget que en portada
+                        echo new View('view/project/widget/project.html.php', array(
+                            'project' => $result
+                        )); 
                     ?>
-                    <?php echo '<p>
-                        <a href="/project/' . $result->id . '" target="_blank">' . $result->name . '</a>';
-                        if ($result->owner != $_SESSION['user']->id) {
-                            echo '<a href="/project/' . $result->id . '/invest" target="_blank">[Apóyalo]</a>';
-                        }
-                        echo '<a href="/project/' . $result->id . '/messages" target="_blank">[Mensajes]</a>
-                        <a href="/project/' . $result->id . '/supporters" target="_blank">[Cofinanciadores]</a><br />
-                        Obtenido: ' . $result->invested . ' &euro;<br />
-                        Mínimo: ' . $result->mincost . ' &euro;<br />
-                        Óptimo: ' . $result->maxcost . ' &euro;<br />
-                        Quedan: ' . $result->days . ' días<br />
-                        Cofinanciadores: ' . count($result->project->investors) . '<br />
-                   </p>'; ?>
-                <?php endforeach; ?>
-            <?php else : ?>
-                <p>No se encontraron resultados para <?php echo $_GET['query']; ?></p>
-            <?php endif; ?>
+                    </div>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <p>No se encontraron resultados para <?php echo $_GET['query']; ?></p>
+                <?php endif; ?>
+            </div>
         
         </div>        
 
--- view/discover/view.html.php
+++ view/discover/view.html.php
@@ -1,26 +1,28 @@
-<?php $bodyClass = 'home'; include 'view/prologue.html.php' ?>
+<?php
 
-        <?php include 'view/header.html.php' ?>
+use Goteo\Core\View;
+
+$bodyClass = 'home';
+
+include 'view/prologue.html.php';
+
+include 'view/header.html.php' ?>
+
 
         <div id="main">
             
-            <h3><?php echo $this['title']; ?></h3>
-
-            <?php
-            foreach ($this['list'] as $project) {
-                echo '<p>' . $project->name . '<br />';
-                if ($project->status == 3)
-                    echo '<a href="/invest/' . $project->id . '">[Apóyalo]</a>';
-
-                echo '<a href="/project/' . $project->id . '">[Ver proyecto]</a><br />
-                    Obtenido: ' . $project->invested . ' &euro;<br />
-                    Mínimo: ' . $project->mincost . ' &euro;<br />
-                    Óptimo: ' . $project->maxcost . ' &euro;<br />
-                    Quedan: ' . $project->days . ' días<br />
-                    Cofinanciadores: ' . count($project->investors) . '<br />
-               </p>';
-            }
-            ?>
+            <div class="widget projects promos">
+                <h2 class="title"><?php echo $this['title']; ?></h2>
+                <?php foreach ($this['list'] as $project) : ?>
+                    <div>
+                        <?php
+                        echo new View('view/project/widget/project.html.php', array(
+                            'project' => $project
+                            ));
+                        ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         
         </div>        
 
