815ab55845a975174443e0177739b2c86ca21b79
Julian <onliners-github@doukeshi.org>
2011-05-30 18:38:47 +0200

    Scripts registros iniciales categorias e intereses

---
+++ db/category/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(1, 'EducaciÃ³n', 'EducaciÃ³n');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(2, 'EconomÃ­a solidaria', 'EconomÃ­a solidaria');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(3, 'Empresa abierta', 'Empresa abierta');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(4, 'FormaciÃ³n tÃ©cnica', 'FormaciÃ³n tÃ©cnica');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(5, 'Desarrollo', 'Desarrollo');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(6, 'Software', 'Software');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES(7, 'Hardware', 'Hardware');
