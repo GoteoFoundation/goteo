e87d6a554f234e4cb48175d91102d9aeacf8dc85
Julian <onliners-github@doukeshi.org>
2011-06-04 12:12:56 +0200

    Buscador basico (solo campos de búsqueda)

--- view/discover/index.html.php
+++ view/discover/index.html.php
@@ -1,8 +1,15 @@
 <?php
 
-use Goteo\Core\View;
+use Goteo\Core\View,
+    Goteo\Model\Project\Category,
+    Goteo\Model\Project\Reward,
+    Goteo\Library\Location;
 
 $bodyClass = 'home';
+
+$categories = Category::getAll();
+$locations = Location::getList();
+$rewards = Reward::icons('individual');
 
 include 'view/prologue.html.php';
 
@@ -13,7 +20,35 @@ include 'view/header.html.php' ?>
         <form method="get" action="/discover/results">
             <fieldset>
                 <legend>Buscar</legend>
-                <input type="text" name="query"  />
+                <label>Por texto: <input type="text" name="query"  /></label>
+                <br />
+                <label>Por categoria:
+                    <select name="category">
+                        <option value="">Todas las categorias</option>
+                    <?php foreach ($categories as $id=>$name) : ?>
+                        <option value="<?php echo $id; ?>"><?php echo $name; ?></option>
+                    <?php endforeach; ?>
+                    </select>
+                </label>
+                <br />
+                <label>Por lugar:
+                    <select name="location">
+                        <option value="">Todos los lugares</option>
+                    <?php foreach ($locations as $id=>$name) : ?>
+                        <option value="<?php echo $id; ?>"><?php echo $name; ?></option>
+                    <?php endforeach; ?>
+                    </select>
+                </label>
+                <br />
+                <label>Por retorno:
+                    <select name="reward">
+                        <option value="">Todos los tipos</option>
+                    <?php foreach ($rewards as $id=>$reward) : ?>
+                        <option value="<?php echo $id; ?>"><?php echo $reward->name; ?></option>
+                    <?php endforeach; ?>
+                    </select>
+                </label>
+                <br />
                 <input type="submit" value="Buscar" >
             </fieldset>
         </form>
