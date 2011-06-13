9dd832ced69f637ac19c186dafcfa497b39c2270
Julian <onliners-github@doukeshi.org>
2011-06-13 21:17:41 +0200

    Cambio campo sql del nombre del tag

--- db/tag/structure.sql
+++ db/tag/structure.sql
@@ -1,5 +1,8 @@
 CREATE TABLE  `goteo`.`tag` (
 `id` SERIAL NOT NULL ,
-`tag` TINYTEXT NOT NULL ,
+`name` TINYTEXT NOT NULL ,
 `blog` BIGINT( 20 ) UNSIGNED NOT NULL
 ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT =  'Tags de blogs (de nodo)';
+
+-- los alters
+ALTER TABLE `tag` CHANGE `tag` `name` TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
