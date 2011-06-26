e4a3ef3ebff201d7851a6f45519f9f21860a53e8
Julian <onliners-github@doukeshi.org>
2011-06-27 00:16:30 +0200

    Aplicando la Edicion de registros de colaboracion en el dashboard

--- view/dashboard/projects/supports.html.php
+++ view/dashboard/projects/supports.html.php
@@ -3,11 +3,11 @@ use Goteo\Core\View,
     Goteo\Library\Text,
     Goteo\Library\SuperForm;
 
-define('ADMIN_NOAUTOSAVE', true);
-
 echo new View ('view/dashboard/projects/selector.html.php', $this);
 
 $project = $this['project'];
+$errors = $project->errors['supports'] ?: array();
+$okeys  = $project->okeys['supports'] ?: array();
 $errors = $this['errors'];
 $this['level'] = 3;
 
@@ -25,42 +25,85 @@ $supports = array();
 
 foreach ($project->supports as $support) {
 
+    // a ver si es el que estamos editando o no
+    if (isset($_POST["support-{$support->id}-edit"])) {
+        // a este grupo le ponemos estilo de edicion
     $supports["support-{$support->id}"] = array(
             'type'      => 'group',
-            'class'     => 'support',
+                'class'     => 'support edit',
             'children'  => array(
                 "support-{$support->id}-support" => array(
-                    'title'     => 'Resumen',
+                        'title'     => Text::get('supports-field-support'),
                     'type'      => 'textbox',
+                        'required'  => true,
                     'size'      => 100,
                     'class'     => 'inline',
                     'value'     => $support->support,
+                        'errors'    => !empty($errors["support-{$support->id}-support"]) ? array($errors["support-{$support->id}-support"]) : array(),
+                        'ok'        => !empty($okeys["support-{$support->id}-support"]) ? array($okeys["support-{$support->id}-support"]) : array(),
                     'hint'      => Text::get('tooltip-project-support-support'),
                 ),
                 "support-{$support->id}-type" => array(
-                    'title'     => 'Tipo',
+                        'title'     => Text::get('supports-field-type'),
+                        'required'  => true,
                     'class'     => 'inline support-type',
                     'type'      => 'radios',
                     'options'   => $support_types,
                     'value'     => $support->type,
+                        'errors'    => !empty($errors["support-{$support->id}-type"]) ? array($errors["support-{$support->id}-type"]) : array(),
+                        'ok'        => !empty($okeys["support-{$support->id}-type"]) ? array($okeys["support-{$support->id}-type"]) : array(),
                     'hint'      => Text::get('tooltip-project-support-type'),
                 ),
                 "support-{$support->id}-description" => array(
                     'type'      => 'textarea',
-                    'title'     => 'Descripción',
+                        'required'  => true,
+                        'title'     => Text::get('supports-field-description'),
                     'cols'      => 100,
                     'rows'      => 4,
                     'class'     => 'inline',
-                    'hint'      => Text::get('tooltip-project-support-description'),
-                    'value'     => $support->description
+                        'value'     => $support->description,
+                        'errors'    => !empty($errors["support-{$support->id}-description"]) ? array($errors["support-{$support->id}-description"]) : array(),
+                        'ok'        => !empty($okeys["support-{$support->id}-description"]) ? array($okeys["support-{$support->id}-description"]) : array(),
+                        'hint'      => Text::get('tooltip-project-support-description')
                 ),
                 "support-{$support->id}-remove" => array(
                     'type'  => 'submit',
-                    'label' => 'Quitar',
+                        'label' => Text::get('form-remove-button'),
                     'class' => 'inline remove support-remove'
-                )
+                    ),
+                    "support-{$support->id}-accept" => array(
+                        'type'  => 'submit',
+                        'label' => Text::get('form-accept-button'),
+                        'class' => 'inline accept support-accept'
+                    )
             )
         );
+
+    } else {
+        // a este grupo lo ponemos cerrado, en html y boton para ir a editarlo
+        // ese boton lanza el formulario igual que lo hace el de añadir, quitar o aceptar
+        // a ver si el tipo de registro lo podemos poner con el icono
+        // y el boton de editar en la misma linea
+        $supports["support-{$support->id}"] = array(
+                'type'      => 'group',
+                'class'     => 'support line',
+                'children'  => array(
+                    "support-{$support->id}-support" => array(
+                        'type'      => 'html',
+                        'class'     => 'inline',
+                        'html'      => $this['types'][$support->type] . ': ' . $support->support
+                    ),
+                    "support-{$support->id}-edit" => array(
+                        'type'  => 'submit',
+                        'label' => Text::get('form-edit-button'),
+                        'class' => 'inline edit support-edit'
+                    )
+                )
+            );
+
+    }
+
+
 }
 ?>
 
@@ -89,12 +132,12 @@ foreach ($project->supports as $support) {
         ),
         'suppports' => array(
             'type'      => 'group',
-            'title'     => 'Colaboraciones',
+            'title'     => Text::get('supports-fields-support-title'),
             'hint'      => Text::get('tooltip-project-supports'),
             'children'  => $supports + array(
                 'support-add' => array(
                     'type'  => 'submit',
-                    'label' => 'Añadir',
+                    'label' => Text::get('form-add-button'),
                     'class' => 'add support-add',
                 )
             )
