c21694e8a076b1ec5526825b6ea2615aa1663b61
Julian <onliners-github@doukeshi.org>
2011-07-07 19:43:46 +0200

    Poniendo un boton de upload junto a los input de imagen y video

--- library/superform/view/element/file.html.php
+++ library/superform/view/element/file.html.php
@@ -1,1 +1,1 @@
-<input name="<?php echo htmlspecialchars($this['name']) ?>" type="file" />
+<input name="<?php echo htmlspecialchars($this['name']) ?>" type="file" /> <input type="submit" name="upload" value="Upload" />
