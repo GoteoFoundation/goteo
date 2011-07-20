850a5b33252aa6fe588ff00578e3bb7e3a905e03
Julian <onliners-github@doukeshi.org>
2011-07-20 22:21:30 +0200

    Primer intento de poner las descripciones de los errores * con focus al campo que lo genera

--- view/project/edit/errors.html.php
+++ view/project/edit/errors.html.php
@@ -1,21 +1,39 @@
 <?php
+use Goteo\Library\Text;
+
 $project = $this['project'];
 $step = $this['step'];
 
-$step_errors = 0;
-$total_errors = 0;
-
-foreach ($project->errors as $st => $errors) {
-    if ($st == $step) {
-        $step_errors = count($errors);
-    }
-    $total_errors += count($errors);
-}
+$step_errors = count($project->errors[$step]);
+$total_errors = count($project->errors);
 ?>
 <div>
-    Total: <?php echo $total_errors ?><br />
-    Este paso: <?php echo $step_errors ?><br />
-    
+    <?php if ($step != 'preview') : ?>
+        <?php if (!empty($project->errors[$step])) : ?>
+        <ul class="sf-footer-errors">
+            <?php foreach ($project->errors[$step] as $id => $error) : ?>
+            <li><a href="#<?php echo $id ?>" onclick="document.getElementById('<?php echo $id ?>').focus(); return false;"><?php echo $error ?></a></li>
+            <?php endforeach ?>
+        </ul>
+        <?php endif ?>
+    <?php else : ?>
+        <?php foreach ($project->errors as $st => $errors)  :
+            if (!empty($errors)) : ?>
+            <h4 class="title"><?php echo Text::get('step-'.$st); ?></h4>
+            <ul class="sf-footer-errors">
+            <?php foreach ($errors as $id => $error) : ?>
+                <li><a href="#<?php echo $id ?>" onclick="document.getElementById('<?php echo $id ?>').focus(); return false;"><?php echo $error ?></a></li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endif;
+            endforeach; ?>
+    <?php endif; ?>
+
+    <p>
+    Errores en este Paso: <?php echo $step_errors ?><br />
+    Erores en Total: <?php echo $total_errors ?>
+    </p>
+
 </div>
 
 <script type="text/javascript">
