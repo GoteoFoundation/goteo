fd3dd70def0ca8dd83d9607d7432dbf0981f8d1c
Julian <onliners-github@doukeshi.org>
2011-08-17 18:48:30 +0200

    Lio de  menus por poner los li activables por defecto globalmente

--- view/review/menu.html.php
+++ view/review/menu.html.php
@@ -1,11 +1,11 @@
         <div id="review-menu">
             <ul>
             <?php foreach ($this['menu'] as $section=>$item) : ?>
-                <li class="section<?php if ($section == $this['section']) echo ' active'; ?>">
+                <li class="section<?php if ($section == $this['section']) echo ' current'; ?>">
                     <a href="/review/<?php echo $section; ?>"><?php echo $item['label']; ?></a>
                     <ul>
                     <?php foreach ($item['options'] as $option=>$label) : ?>
-                        <li class="option<?php if ($option == $this['option']) echo ' active'; ?>">
+                        <li class="option<?php if ($option == $this['option']) echo ' current'; ?>">
                             <a href="/review/<?php echo $section; ?>/<?php echo $option; ?>"><?php echo $label; ?></a>
                         </li>
                     <?php endforeach; ?>
