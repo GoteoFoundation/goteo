aae0b842c1bacf35100763ed281018ce708e584d
Julian <onliners-github@doukeshi.org>
2011-11-25 10:53:44 +0100

    Base de feed usuario en dashboard - mi muro

--- view/dashboard/activity/wall.html.php
+++ view/dashboard/activity/wall.html.php
@@ -1,7 +1,36 @@
 <?php
-use Goteo\Core\View;
+use Goteo\Core\View,
+    Goteo\Library\Text;
+
+$items = $this['items'];
 ?>
-<div class="widget">
-    <h2 class="title">Mi muro</h2>
-    <p><a href="/user/<?php echo $_SESSION['user']->id; ?>" target="_blank">Abrir la página pública de tu perfil</a></p>
+<div class="widget feed">
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $('.scroll-pane').jScrollPane({showArrows: true});
+
+            $('.hov').hover(
+              function () {
+                $(this).addClass($(this).attr('rel'));
+              },
+              function () {
+                $(this).removeClass($(this).attr('rel'));
+              }
+            );
+
+        });
+        </script>
+        <h2 class="title"><?php echo Text::get('dashboard-menu-activity-wall'); ?></h2>
+
+        <div class="scroll-pane">
+            <?php foreach ($items as $item) :
+                $odd = !$odd ? true : false;
+                ?>
+            <div class="subitem<?php if ($odd) echo ' odd';?>">
+               <span class="datepub"><?php echo Text::get('feed-timeago', $item->timeago); ?></span>
+               <div class="content-pub"><?php echo $item->html; ?></div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+
 </div>
