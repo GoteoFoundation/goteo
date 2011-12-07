5a4d561679b7e03f2a14a2de8444b703e867ba22
Julian <onliners-github@doukeshi.org>
2011-12-07 09:00:03 +0100

    Nuevo rol para usuarios convocadores

--- db/role/data.sql
+++ db/role/data.sql
@@ -1,5 +1,7 @@
-INSERT INTO `role` (`id`, `name`) VALUES
-('admin', 'Administrador de Nodo'),
-('root', 'Super administrador'),
-('superadmin', 'Super administrador de Goteo'),
-('user', 'Usuario mediocre');
+INSERT INTO `role` VALUES('admin', 'Administrador');
+INSERT INTO `role` VALUES('checker', 'Revisor de proyectos');
+INSERT INTO `role` VALUES('root', 'ROOT');
+INSERT INTO `role` VALUES('superadmin', 'Super administrador');
+INSERT INTO `role` VALUES('translator', 'Traductor de contenidos');
+INSERT INTO `role` VALUES('user', 'Usuario mediocre');
+INSERT INTO `role` VALUES('caller', 'Convocador');
