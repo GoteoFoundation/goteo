8d6ab93ddb22932808ad2ccbfb9fcfd022b9949d
Julian <onliners-github@doukeshi.org>
2011-12-19 14:16:11 +0100

    Estilos splash modalidad uno

--- view/call/splash.html.php
+++ view/call/splash.html.php
@@ -17,76 +17,84 @@ include 'view/call/prologue.html.php';
 			<li class="item" id="description">
 				<img src="<?php echo SRC_URL ?>/image/<?php echo $call->logo ?>" alt="logo" />
                 <h2 class="title"><?php echo Text::get('call-splash-campaign_title') ?><br /><?php echo $call->name ?></h2>
-                <?php /* if ($call->status == 3) : //inscripcion ?>
-                <h2><?php echo Text::get('call-splash-searching_projects') ?></h2>
-                <?php else : //en campaña */ ?>
-				<h2 class="subtitle"><?php echo Text::get('call-splash-invest_explain', $call->user->name) ?></h2>
-                <?php // endif; ?>
+                <?php if ($call->status == 3) : //inscripcion ?>
+                <p class="subtitle red"><?php echo Text::get('call-splash-searching_projects') ?></p>
+                <?php else : //en campaña ?>
+				<p class="subtitle"><?php echo Text::get('call-splash-invest_explain', $call->user->name) ?></p>
+                <?php endif; ?>
 				<p><?php echo $call->description ?></p>
 			</li>
 			<li class="item" id="numbers">
-            <?php /* if ($call->status == 3) : //inscripcion ?>
-				<dl class="block long last">
-					<dt><?php echo Text::get('call-splash-whole_budget-header') ?></dt>
-                    <dd class="money<?php if ($call->status == 4) echo ' light' ?>"><?php echo \amount_format($call->amount) ?> <span class="euro">&euro;</span></dd>
-				</dl>
-				<dl class="block long">
-					<dt><?php echo Text::get('call-splash-valid_until-header') ?></dt>
-					<dd><span>12</span>Nov/2011</dd>
-				</dl>
-				<dl class="block last selected">
-					<dt><?php echo Text::get('call-splash-applied_projects-header') ?></dt>
-					<dd><?php echo count($call->projects) ?></dd>
-				</dl>
-				<dl class="block category">
-					<dt><?php echo Text::get('call-splash-categories-header') ?></dt>
-					<dd><?php echo implode(', ', $call->categories) ?></dd>
-				</dl>
-				<dl class="block return">
-					<dt><?php echo Text::get('call-splash-icons-header') ?></dt>
-					<dd>
-						<ul>
-                            <?php foreach ($call->icons as $iconId=>$iconName) : ?>
-							<li class="<?php echo $iconId ?> activable">
-								<a class="tipsy" title="<?php echo $iconName ?>" ><?php echo $iconName ?></a>
-							</li>
-                            <?php endforeach; ?>
-						</ul>
-					</dd>
-				</dl>
-				<dl class="block long last">
-					<dt><?php echo Text::get('call-splash-more_info-header') ?></dt>
-					<dd>
-                        <a href="<?php echo $call->pdf ?>" target="_blank"><?php echo Text::get('call-splash-dossier-link') ?></a><br />
-                        <a href="<?php echo $call->user->webs[0]->url ?>" target="_blank"><?php echo $call->user->webs[0]->url ?></a><br />
-                        <a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/legal" target="_blank"><?php echo Text::get('call-splash-legal-link') ?></a>
-                    </dd>
-				</dl>
-            <?php else : //en campaña */ ?>
-				<dl class="block long last">
-					<dt><?php echo Text::get('call-splash-whole_budget-header') ?></dt>
-                    <dd class="money light"><?php echo \amount_format($call->amount) ?> <span class="euro">&euro;</span></dd>
-				</dl>
-				<dl class="block long">
-					<dt><?php echo Text::get('call-splash-remain_budget-header') ?></dt>
-					<dd class="money"><?php echo \amount_format($call->rest) ?> <span class="euro">&euro;</span></dd>
-				</dl>
-				<dl class="block last category">
-					<dt><?php echo Text::get('call-splash-categories-header') ?></dt>
-					<dd><?php echo implode(', ', $call->categories) ?></dd>
-				</dl>
-				<dl class="block selected">
-					<dt><?php echo Text::get('call-splash-selected_projects-header') ?></dt>
-					<dd><?php echo count($call->projects) ?></dd>
-				</dl>
-				<dl class="block processing">
-					<dt><?php echo Text::get('call-splash-runing_projects-header') ?></dt>
-					<dd><?php echo $call->runing_projects ?></dd>
-				</dl>
-				<dl class="block success">
-					<dt><?php echo Text::get('call-splash-success_projects-header') ?></dt>
-					<dd><?php echo $call->success_projects ?></dd>
-				</dl>
+            <?php if ($call->status == 3) : //inscripcion ?>
+				<div class="row">
+                    <dl class="block long last">
+                        <dt><?php echo Text::get('call-splash-whole_budget-header') ?></dt>
+                        <dd class="money<?php if ($call->status == 4) echo ' light' ?>"><?php echo \amount_format($call->amount) ?> <span class="euro">&euro;</span></dd>
+                    </dl>
+                    <dl class="block long expires">
+                        <dt><?php echo Text::get('call-splash-valid_until-header') ?></dt>
+						<dd><strong>12</strong> Nov / 2011</dd>
+                    </dl>
+                    <dl class="block last selected">
+                        <dt><?php echo Text::get('call-splash-applied_projects-header') ?></dt>
+                        <dd><?php echo count($call->projects) ?></dd>
+                    </dl>
+				</div>
+				<div class="row">
+
+                    <dl class="block category">
+                        <dt><?php echo Text::get('call-splash-categories-header') ?></dt>
+                        <dd><?php echo implode(', ', $call->categories) ?></dd>
+                    </dl>
+                    <dl class="block return">
+                        <dt><?php echo Text::get('call-splash-icons-header') ?></dt>
+                        <dd>
+                            <ul>
+                                <?php foreach ($call->icons as $iconId=>$iconName) : ?>
+                                <li class="<?php echo $iconId ?> activable">
+                                    <a class="tipsy" title="<?php echo $iconName ?>" ><?php echo $iconName ?></a>
+                                </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </dd>
+                    </dl>
+                    <dl class="block moreinfo last">
+                        <dt><?php echo Text::get('call-splash-more_info-header') ?></dt>
+                        <dd>
+                            <a class="red" href="<?php echo $call->pdf ?>" target="_blank"><?php echo Text::get('call-splash-dossier-link') ?></a>
+                            <a href="<?php echo $call->user->webs[0]->url ?>" target="_blank"><?php echo $call->user->webs[0]->url ?></a>
+                            <a class="aqua" href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/legal" target="_blank"><?php echo Text::get('call-splash-legal-link') ?></a>
+                        </dd>
+                    </dl>
+				</div>
+            <?php else : //en campaña ?>
+				<div class="row">
+                    <dl class="block long last">
+                        <dt><?php echo Text::get('call-splash-whole_budget-header') ?></dt>
+                        <dd class="money light"><?php echo \amount_format($call->amount) ?> <span class="euro">&euro;</span></dd>
+                    </dl>
+                    <dl class="block long">
+                        <dt><?php echo Text::get('call-splash-remain_budget-header') ?></dt>
+                        <dd class="money"><?php echo \amount_format($call->rest) ?> <span class="euro">&euro;</span></dd>
+                    </dl>
+                    <dl class="block last category">
+                        <dt><?php echo Text::get('call-splash-categories-header') ?></dt>
+                        <dd><?php echo implode(', ', $call->categories) ?></dd>
+                    </dl>
+                </div>
+				<div class="row">
+                    <dl class="block selected">
+                        <dt><?php echo Text::get('call-splash-selected_projects-header') ?></dt>
+                        <dd><?php echo count($call->projects) ?></dd>
+                    </dl>
+                    <dl class="block processing">
+                        <dt><?php echo Text::get('call-splash-runing_projects-header') ?></dt>
+                        <dd><?php echo $call->runing_projects ?></dd>
+                    </dl>
+                    <dl class="block success">
+                        <dt><?php echo Text::get('call-splash-success_projects-header') ?></dt>
+                        <dd><?php echo $call->success_projects ?></dd>
+                    </dl>
 				<dl class="block last return">
 					<dt><?php echo Text::get('call-splash-icons-header') ?></dt>
 					<dd>
@@ -99,19 +107,21 @@ include 'view/call/prologue.html.php';
 						</ul>
 					</dd>
 				</dl>
-            <?php // endif; ?>
+                </div>
+            <?php endif; ?>
 
 				<dl class="block location">
 					<dt><?php echo Text::get('call-splash-location-header') ?></dt>
 					<dd><?php echo Text::GmapsLink($call->call_location); ?></dd>
 				</dl>
 
+            <?php if ($call->status == 3) : //inscripcion ?>
+				<a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/info" class="button aqua info long" target="_blank"><?php echo Text::get('call-splash-more_info-button') ?></a>
+				<a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/apply" class="button red join" target="_blank"><?php echo Text::get('call-splash-apply-button') ?></a>
+            <?php else : // ver proyectos ?>
 				<a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/info" class="button aqua info" target="_blank"><?php echo Text::get('call-splash-more_info-button') ?></a>
-            <?php /* if ($call->status == 3) : //inscripcion ?>
-				<a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/apply" class="button red view" target="_blank"><?php echo Text::get('call-splash-apply-button') ?></a>
-            <?php else : // ver proyectos */ ?>
 				<a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>/projects" class="button red view" target="_blank"><?php echo Text::get('call-splash-see_projects-button') ?></a>
-            <?php // endif; ?>
+            <?php endif; ?>
 			</li>
 		</ul>
     </div>
