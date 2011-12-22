ceb7b53d7cd85f171edcfd78c29a639172164385
Julian <onliners-github@doukeshi.org>
2011-12-22 15:58:44 +0100

    arreglos wof (Ivan)

--- controller/wof.php
+++ controller/wof.php
@@ -8,9 +8,9 @@ namespace Goteo\Controller {
 
     class Wof extends \Goteo\Core\Controller {
 
-        public function index($id, $width = 200, $height = 200, $mode = 0) {
-			if($wof = new WallFriends($id)) {
-				echo $wof->html($width,$height,$mode);
+        public function index($id, $type = 1, $all_avatars = 1, $width = 200) {
+			if($wof = new WallFriends($id,$all_avatars)) {
+				echo $wof->html($width,$type);
 			}
 			else {
 				throw new Error(Error::NOT_FOUND);
