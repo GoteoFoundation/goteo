384987561b87695a800bb493925955408ffa9fc3
Julian <onliners-github@doukeshi.org>
2012-01-03 18:51:17 +0100

    Pagina de proyectos

--- view/call/projects.html.php
+++ view/call/projects.html.php
@@ -37,36 +37,26 @@ include 'view/call/header.html.php';
 
                 ?>
 			<li>
+				<a href="<?php echo SITE_URL ?>/project/<?php echo $project->id ?>" class="expand" target="_blank"></a>
                 <div class="image">
                     <?php if (!empty($project->gallery) && (current($project->gallery) instanceof Image)): ?>
                     <a href="<?php echo SITE_URL ?>/project/<?php echo $project->id ?>"><img src="<?php echo current($project->gallery)->getLink(150, 98, true) ?>" alt="<?php echo $project->name ?>"/></a>
                     <?php endif ?>
                     <?php if (!empty($categories)): ?>
-                    <div class="categories">
-                    <?php $sep = ''; foreach ($categories as $key=>$value) :
+                    <div class="categories"><?php $sep = ''; foreach ($categories as $key=>$value) :
                         echo $sep.htmlspecialchars($value);
-                    $sep = ', '; endforeach; ?>
-                    </div>
+                    $sep = ', '; endforeach; ?></div>
                     <?php endif ?>
                 </div>
-
-                <h3 class="title">
-                    <a href="<?php echo SITE_URL ?>/project/<?php echo $project->id ?>"<?php echo $blank; ?>><?php echo htmlspecialchars(Text::recorta($project->name,50)) ?></a>
-                </h3>
-
-                <h4 class="author">
-                    <?php echo Text::get('regular-by')?> <a href="<?php echo SITE_URL ?>/user/profile/<?php echo htmlspecialchars($project->user->id) ?>"<?php echo $blank; ?>><?php echo htmlspecialchars(Text::recorta($project->user->name,40)) ?></a>
-                </h4>
-                <div class="description"><?php echo $project->subtitle; ?></div>
-
-
-                <div class="">
-                    <span class="">Obtenido</span>
-                    <div class=""><?php echo $project->amount ?> <span class="euro">&euro;</span></div>&nbsp;&nbsp;|&nbsp;&nbsp;<div class=""><?php echo $project->per_amount ?> &#37;</div>
-                </div>
-
-                <div class="days">Quedan <?php echo $project->days ?> d&iacute;as</div>
-
+                <h3 class="title"><a href="<?php echo SITE_URL ?>/project/<?php echo $project->id ?>"<?php echo $blank; ?>><?php echo htmlspecialchars(Text::recorta($project->name,50)) ?></a></h3>
+                <div class="description"><?php echo empty($project->subtitle) ? Text::recorta($project->description, 250) : $project->subtitle; ?></div>
+                <h4 class="author"><?php echo Text::get('regular-by')?> <a href="<?php echo SITE_URL ?>/user/profile/<?php echo htmlspecialchars($project->user->id) ?>" target="_blank"><?php echo htmlspecialchars(Text::recorta($project->user->name,40)) ?></a></h4>
+				<span class="obtained"><?php echo Text::get('project-view-metter-got'); ?></span>
+				<div class="obtained">
+		            <strong><?php echo \amount_format($project->amount) ?> <span class="euro">&euro;</span></strong>
+		            <span class="percent"><?php echo $project->per_amount ?> &#37;</span>
+		        </div>
+				<div class="days"><span><?php echo Text::get('project-view-metter-days'); ?></span> <?php echo $project->days ?> <?php echo Text::get('regular-days'); ?></div>
 			</li>
             <?php
             endforeach; ?>
