9e66dabb6f22959e0d14c9d0b3e4eea67057194d
Julian <onliners-github@doukeshi.org>
2012-03-13 09:03:16 +0100

    Comunicaciones, solo mostrar fallisdos si hay alguno

--- view/admin/mailing/send.html.php
+++ view/admin/mailing/send.html.php
@@ -22,15 +22,16 @@ foreach ($users as $usr) {
 $_SESSION['mailing']['receivers'] = $new_receivers;
 ?>
 <div class="widget">
-    <?php if (!empty($this['time']) ) : ?><p>El envio se ha procesado en <?php echo $this['time']; ?> microsegundos</p><?php endif ?>
+    <?php if (!empty($this['time']) ) : ?><p>El envio se ha procesado en <?php echo $this['time']; ?> milisegundos</p><?php endif ?>
     <p><?php echo 'Buscábamos comunicarnos con ' . $_SESSION['mailing']['filters_txt']; ?> </p>
     <p>De los <?php echo count($users) ?> destinatarios, se ha enviado correctamente a <?php echo count($oks) ?> y han fallado <?php echo count($fails) ?> envios.</p>
     <p>El último "si no ves" ha sido: <a href="<?php echo $_SESSION['MAILING_TOKEN'] ?>" target="_blank"><?php echo $_SESSION['MAILING_TOKEN'] ?></a></p>
+    <?php if (!empty($fails) ) : ?>
     <p>Han fallado los siguientes:</p>
         <blockquote><?php foreach ($fails as $usrId) {
                 echo '<strong>' .$receivers[usrId]->name . '</strong> ('.$receivers[usrId]->id.') al mail <strong>' . $receivers[usrId]->email . '</strong><br />';
         } ?></blockquote>
     <p>Puedes cargarlos como nueva lista de destinatarios pulsando <a href="/admin/mailing/edit/?recover=receivers">este enlace</a></p>
-
+    <?php endif; ?>
 </div>
 
