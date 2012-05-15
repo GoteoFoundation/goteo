b5ee9a2df72a55df1d54009de361946c9f196d17
Julian <onliners-github@doukeshi.org>
2012-05-16 00:36:31 +0200

    gestion de paginas institucionales, mejora ckeditor

--- view/js/ckeditor/config.js
+++ view/js/ckeditor/config.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -8,4 +8,5 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.resize_enabled = false;
 };
