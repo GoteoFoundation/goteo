7f3a843c072947acf42162c258375c57047343f7
Julian Portatil <git@doukeshi.org>
2013-01-24 20:58:44 +0900

    algunos arreglos en los widgets de social y title

--- view/call/widget/social.html.php
+++ view/call/widget/social.html.php
@@ -1,7 +1,6 @@
 <?php
 
 use Goteo\Library\Text,
-    Goteo\Library\Buzz,
     Goteo\Core\View;
 
 $call = $this['call'];
@@ -15,7 +14,9 @@ if (LANG != 'es')
 $shate_title = (!empty($social->tweet)) ? $social->tweet : $call->name;
 $facebook_url = 'http://facebook.com/sharer.php?u=' . urlencode($share_url) . '&t=' . urlencode($share_title);
 ?>
-<ul>
+
+<div id="social-logo">
+    <ul>
     <?php if (!empty($call->user->facebook)): ?>
     <li class="facebook"><a href="<?php echo htmlspecialchars($call->user->facebook) ?>"><?php echo Text::get('regular-facebook'); ?></a></li>
     <?php endif ?>
@@ -33,36 +34,36 @@ $facebook_url = 'http://facebook.com/sharer.php?u=' . urlencode($share_url) . '&
     <?php endif ?>
 </ul>
 <a href="<?php echo $URL ?>/service/resources" id="capital" target="_blank"><?php echo Text::get('footer-service-resources') ?></a>
+</div>
 
-<!-- texto "difunde esta iniciativa"
-y espacio donde irán los botones
--->
-<hr />
+<div id="social-actions">
 <span>Difunde esta iniciativa</span>
 
-<a href="https://twitter.com/share" class="twitter-share-button"
-   data-url="<?php echo $share_url; ?>"
-   data-via="<?php echo $social->author ?>"
-   data-text="<?php echo $share_title; ?>"
-   data-lang="<?php echo \LANG; ?>"
-   data-counturl="<?php echo SITE_URL . '/call/' . $call->id; ?>"
-   target="_blank"><?php echo Text::get('regular-twitter'); ?></a>
-<script>!function(d,s,id){
-    var js,fjs=d.getElementsByTagName(s)[0];
-    if(!d.getElementById(id)){
-        js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);
-    }
-}(document,"script","twitter-wjs");</script>
+	<a href="https://twitter.com/share" class="twitter-share-button"
+	   data-url="<?php echo $share_url; ?>"
+	   data-via="<?php echo $social->author ?>"
+	   data-text="<?php echo $share_title; ?>"
+	   data-lang="<?php echo \LANG; ?>"
+	   data-counturl="<?php echo SITE_URL . '/call/' . $call->id; ?>"
+	   target="_blank"><?php echo Text::get('regular-twitter'); ?></a>
+	<script>!function(d,s,id){
+	    var js,fjs=d.getElementsByTagName(s)[0];
+	    if(!d.getElementById(id)){
+	        js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);
+	    }
+	}(document,"script","twitter-wjs");</script>
 
-<?php if (!empty($social->fbappid)) : ?>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/<?php echo \Goteo\Library\Lang::locale(); ?>/all.js#xfbml=1&appId=<?php echo $social->fbappid; ?>";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<?php else: // si no tiene app de facebook ponemos un compartir ?>
-<a target="_blank" href="<?php echo htmlentities($facebook_url) ?>"><?php echo Text::get('regular-facebook'); ?></a>
-<?php endif; ?>
+	<?php if (!empty($social->fbappid)) : ?>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) {return;}
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/<?php echo \Goteo\Library\Lang::locale(); ?>/all.js#xfbml=1&appId=<?php echo $social->fbappid; ?>";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<?php else: // si no tiene app de facebook ponemos un compartir ?>
+	<a target="_blank" href="<?php echo htmlentities($facebook_url) ?>"><?php echo Text::get('regular-facebook'); ?></a>
+	<?php endif; ?>
+			
+</div>
--- view/call/widget/title.html.php
+++ view/call/widget/title.html.php
@@ -5,9 +5,10 @@ use Goteo\Library\Text,
     Goteo\Core\View;
 
 $call = $this['call'];
+$URL = (NODE_ID != GOTEO_NODE) ? NODE_URL : SITE_URL;
 ?>
 
 <div id="title">
-	<a href="<?php echo SITE_URL ?>/call/<?php echo $call->id ?>"><img src="<?php echo $call->logo->getLink(250, 124, true) ?>" alt="<?php echo $call->user->name ?>" class="logo" /></a>
+	<a href="<?php echo $URL ?>/call/<?php echo $call->id ?>"><img src="<?php echo $call->logo->getLink(250, 124, true) ?>" alt="<?php echo $call->user->name ?>" class="logo" /></a>
 	<h2 class="title"><?php echo Text::get('call-splash-campaign_title') ?><br /><?php echo $call->name ?></h2>
 </div>
