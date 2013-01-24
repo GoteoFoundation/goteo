40b517befffd6cb397790df905b32d8d246f27a4
Julian Portatil <git@doukeshi.org>
2013-01-24 19:49:37 +0900

    quitando piñones en la vista de banners

--- view/call/widget/banners.html.php
+++ view/call/widget/banners.html.php
@@ -9,18 +9,12 @@
         });
     });
 </script>
-<div id="call-banners" class="rounded-corners-bottom"<?php if ($bodyClass == 'home') echo ' style="width: 545px; margin: 0px auto;"'; ?>>
-    <div class="call-banners-container rounded-corners-bottom"<?php if ($bodyClass == 'home') echo ' style="width: 545px;"'; ?>>
+<div id="call-banners" class="rounded-corners-bottom">
+    <div class="call-banners-container rounded-corners-bottom">
 
         <?php foreach ($banners as $banner) : ?>
         <div class="call-banner<?php if (!empty($banner->url)) : ?> activable<?php endif; ?>"<?php if ($banner->image instanceof \Goteo\Model\Image) : ?> style="background: url('/data/images/<?php echo $banner->image->name; ?>') no-repeat right bottom;"<?php endif; ?>>
             <?php if (!empty($banner->url)) : ?><a href="<?php echo $banner->url; ?>" class="expand" target="_blank"></a><?php endif; ?>
-
-         <!--
-            <div class="title"><?php echo $banner->title ?></div>
-            <div class="short-desc"><?php echo $banner->description ?></div>
-         -->
-         
         </div>
         <?php endforeach; ?>
 		
