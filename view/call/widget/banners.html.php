7df39a0e0da837df28bb0bc4cd291ad2c39a3290
Julian Portatil <git@doukeshi.org>
2013-01-24 21:06:49 +0900

    [banners] faltaba la capa de datos

--- view/call/widget/banners.html.php
+++ view/call/widget/banners.html.php
@@ -1,3 +1,10 @@
+<?php
+
+use Goteo\Library\Text,
+    Goteo\Core\View;
+
+$call = $this['call'];
+?>
 <script type="text/javascript">
     $(function(){
         $('#call-banners').slides({
@@ -12,12 +19,12 @@
 <div id="call-banners" class="rounded-corners-bottom">
     <div class="call-banners-container rounded-corners-bottom">
 
-        <?php foreach ($banners as $banner) : ?>
-        <div class="call-banner<?php if (!empty($banner->url)) : ?> activable<?php endif; ?>"<?php if ($banner->image instanceof \Goteo\Model\Image) : ?> style="background: url('/data/images/<?php echo $banner->image->name; ?>') no-repeat right bottom;"<?php endif; ?>>
-            <?php if (!empty($banner->url)) : ?><a href="<?php echo $banner->url; ?>" class="expand" target="_blank"></a><?php endif; ?>
-        </div>
+        <?php foreach ($call->banners as $banner) : ?>
+            <div class="call-banner<?php if (!empty($banner->url)) echo ' activable'; ?>"<?php if ($banner->image instanceof \Goteo\Model\Image) : ?> style="background: url('/data/images/<?php echo $banner->image->name; ?>') no-repeat right bottom;"<?php endif; ?>>
+                <?php if (!empty($banner->url)) : ?><a href="<?php echo $banner->url; ?>" class="expand" target="_blank"></a><?php endif; ?>
+            </div>
         <?php endforeach; ?>
-		
+
     </div>
     <div id="call-banners-controler"><ul class="bannerspage"></ul></div>
 </div>
