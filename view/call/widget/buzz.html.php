7ccca5a5f1368988f30734d18b7dc8a0fe720958
Julian Portatil <git@doukeshi.org>
2013-02-02 23:00:09 +0900

    cambiando textos estatico a dinámico (por ahora aun queda maquetación estática)

--- view/call/widget/buzz.html.php
+++ view/call/widget/buzz.html.php
@@ -3,7 +3,7 @@ use Goteo\Library\Text;
 ?>
 <div id="side" class="twitter">
     <h2><?php echo Text::get('call-header-buzz'); ?></h2>
-	
+	<!-- aqui el foreach -->
 	<div class="tweet">
 	    <div class="avatar">
 	        <a href="https://twitter.com/ACS_Arquisocial" target="_blank">
@@ -11,7 +11,7 @@ use Goteo\Library\Text;
 	        </a>
 	    </div>
 	    <div class="text">
-	        <a href="https://twitter.com/ACS_Arquisocial" target="_blank">Arq_CompromisoSocial</a>
+	        <strong><a href="https://twitter.com/ACS_Arquisocial" target="_blank">Arq_CompromisoSocial</a></strong>
 	        <br>
 	        <a href="https://twitter.com/ACS_Arquisocial" target="_blank">@ACS_Arquisocial</a>
 		    <blockquote>RT @platoniq: Una Plaza procomún: cúpula en el Campo de Cebada @campodecebada http://t.co/XtBfhSP1 Riégalo por #goteo http://t.co/u5npJEek</blockquote>
@@ -25,59 +25,37 @@ use Goteo\Library\Text;
 	        </a>
 	    </div>
 	    <div class="text">
-	        <a href="https://twitter.com/ACS_Arquisocial" target="_blank">Arq_CompromisoSocial</a>
+	        <strong><a href="https://twitter.com/ACS_Arquisocial" target="_blank">Arq_CompromisoSocial</a></strong>
 	        <br>
 	        <a href="https://twitter.com/ACS_Arquisocial" target="_blank">@ACS_Arquisocial</a>
 		    <blockquote>MagmaCultura News is out! http://t.co/4l5Irl7r ▸ Top stories today via @hoyesarte_com @platoniq</blockquote>
 	    </div>
 	</div>
-	
-</div>
-
-<!-- pintado estatico para maquetar
-
-<div style="border-bottom: 1px solid grey; display: block;">
-    <div style="float: left; margin-right: 5px;">
-        <a href="https://twitter.com/MagmaCultura" target="_blank">
-            <img src="http://a0.twimg.com/profile_images/2309950436/ACS_logo__normal.jpg" alt="MagmaCultura" title="MagmaCultura">
-        </a>
-    </div>
-    <div>
-        <a href="https://twitter.com/MagmaCultura" target="_blank">MagmaCultura</a>
-        <br>
-        <a href="https://twitter.com/MagmaCultura" target="_blank">@MagmaCultura</a>
-    </div>
-    <blockquote>MagmaCultura News is out! http://t.co/4l5Irl7r ▸ Top stories today via @hoyesarte_com @platoniq</blockquote>
-</div>
-
-
-
-Petición a twitter desconectada en Linea 448 en controller/call.php
-<div id="side">
 <?php
 /*
+// pintado estatico para maquetar
+//
+// Petición a twitter desconectada en Linea 448 en controller/call.php
 # if ($_SESSION['user']->id == 'root' && !empty($social->buzz_debug)) echo '<p>DEBUG:: '. $social->buzz_debug . '</p>';
 # if ($_SESSION['user']->id == 'root' && !empty($social->buzz)) echo \trace($social->buzz);
 
 $social = $this['social'];
 
 foreach ($social->buzz as $item) : ?>
-<div style="border-bottom: 1px solid grey; display: block;">
-    <div style="float: left; margin-right: 5px;">
-        <a href="<?php echo $item->profile ?>" target="_blank">
-            <img src="<?php echo $item->avatar ?>" alt="<?php echo $item->author ?>" title="<?php echo $item->user ?>"/>
-        </a>
+    <div class="tweet">
+        <div class="avatar">
+            <a href="<?php echo $item->profile ?>" target="_blank">
+                <img src="<?php echo $item->avatar ?>" alt="<?php echo $item->author ?>" title="<?php echo $item->user ?>"/>
+            </a>
+        </div>
+        <div class="text">
+            <a href="<?php echo $item->profile ?>" target="_blank"><?php echo $item->user ?></a>
+            <br />
+            <a href="<?php echo 'https://twitter.com/'.$item->twitter_user ?>" target="_blank"><?php echo '@'.$item->twitter_user ?></a>
+                <blockquote><?php echo $item->text ?></blockquote>
+        </div>
     </div>
-    <div>
-        <a href="<?php echo $item->profile ?>" target="_blank"><?php echo $item->user ?></a>
-        <br />
-        <a href="<?php echo 'https://twitter.com/'.$item->twitter_user ?>" target="_blank"><?php echo '@'.$item->twitter_user ?></a>
-    </div>
-    <br clear="both" />
-    <blockquote><?php echo $item->text ?></blockquote>
-</div>
 <?php endforeach;
- *
- */ ?>
+*/ ?>
 </div>
--->
+
--- view/call/widget/social.html.php
+++ view/call/widget/social.html.php
@@ -37,14 +37,14 @@ $facebook_url = 'http://facebook.com/sharer.php?u=' . urlencode($share_url) . '&
 	</div>
 
 	<div id="social-actions">
-	<h2>Difunde esta iniciativa</h2>
+	<h2><?php echo Text::get('call-header-social_spread'); ?></h2>
 
 		<a href="https://twitter.com/share" class="twitter-share-button"
 		   data-url="<?php echo $share_url; ?>"
 		   data-via="<?php echo $social->author ?>"
 		   data-text="<?php echo $share_title; ?>"
 		   data-lang="<?php echo \LANG; ?>"
-		   data-counturl="<?php echo SITE_URL . '/call/' . $call->id; ?>"
+		   data-count="none"
 		   target="_blank"><?php echo Text::get('regular-twitter'); ?></a>
 		<script>!function(d,s,id){
 		    var js,fjs=d.getElementsByTagName(s)[0];
@@ -53,17 +53,18 @@ $facebook_url = 'http://facebook.com/sharer.php?u=' . urlencode($share_url) . '&
 		    }
 		}(document,"script","twitter-wjs");</script>
 
-	<div data-show-faces="false" data-width="450" data-layout="button_count" data-send="false" data-href="http://www.facebook.com/pages/Goteo/268491113192109" class="fb-like fb_edge_widget_with_comment fb_iframe_widget" fb-xfbml-state="rendered"><span style="height: 20px; width: 118px;"><iframe scrolling="no" id="f3d78948875001e" name="f357c6a66af3e0c" style="border: medium none; overflow: hidden; height: 20px; width: 118px;" title="Like this content on Facebook." class="fb_ltr" src="http://www.facebook.com/plugins/like.php?api_key=189133314484241&amp;locale=es_ES&amp;sdk=joey&amp;channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D18%23cb%3Df3953b18d391ebc%26origin%3Dhttp%253A%252F%252Fgoteo.org%252Ff29a354eb36053%26domain%3Dgoteo.org%26relation%3Dparent.parent&amp;href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FGoteo%2F268491113192109&amp;node_type=link&amp;width=450&amp;layout=button_count&amp;colorscheme=light&amp;show_faces=false&amp;send=false&amp;extended_social_context=false"></iframe></span></div>
+	<div data-show-faces="false" data-width="450" data-layout="standard" data-send="false" data-href="http://www.facebook.com/pages/Goteo/268491113192109" class="fb-like fb_edge_widget_with_comment fb_iframe_widget" fb-xfbml-state="rendered"><span style="height: 20px; width: 118px;"><iframe scrolling="no" id="f3d78948875001e" name="f357c6a66af3e0c" style="border: medium none; overflow: hidden; height: 20px; width: 118px;" title="Like this content on Facebook." class="fb_ltr" src="http://www.facebook.com/plugins/like.php?api_key=189133314484241&amp;locale=es_ES&amp;sdk=joey&amp;channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D18%23cb%3Df3953b18d391ebc%26origin%3Dhttp%253A%252F%252Fgoteo.org%252Ff29a354eb36053%26domain%3Dgoteo.org%26relation%3Dparent.parent&amp;href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FGoteo%2F268491113192109&amp;node_type=link&amp;width=450&amp;layout=button_count&amp;colorscheme=light&amp;show_faces=false&amp;send=false&amp;extended_social_context=false"></iframe></span></div>
 
 		<?php if (!empty($social->fbappid)) : ?>
-		<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) {return;}
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/<?php echo \Goteo\Library\Lang::locale(); ?>/all.js#xfbml=1&appId=<?php echo $social->fbappid; ?>";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
+		<div class="fb-like" data-href="<?php echo $share_url; ?>" data-layout="standard" data-send="false" data-width="450" data-show-faces="false"></div>
+                <div id="fb-root"></div>
+                <script>(function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/<?php echo \Goteo\Library\Lang::locale(); ?>/all.js#xfbml=1&appId=<?php echo $social->fbappid; ?>";
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
 		<?php else: // si no tiene app de facebook ponemos un compartir ?>
 		<!--a target="_blank" href="<?php echo htmlentities($facebook_url) ?>"><?php echo Text::get('regular-facebook'); ?></a-->
 		<?php endif; ?>
