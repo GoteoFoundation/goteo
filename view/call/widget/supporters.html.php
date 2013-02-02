e8519f5358047231a4ec415bd939c84b2e0fc085
Julian Portatil <git@doukeshi.org>
2013-02-03 00:07:06 +0900

    que el avatar del convocador no salga en las caritas

--- view/call/widget/supporters.html.php
+++ view/call/widget/supporters.html.php
@@ -14,7 +14,8 @@ $supporters = $call->getSupporters();
 <ul>
 <?php foreach ($supporters as $item) :
     if (!$item->avatar instanceof Image) continue;  // quitamos los que tengan la imagen rota
-//    if ($item->avatar->id == 1) continue;  // por ahora no quitamos las gotas
+    if ($item->avatar->id == 1) continue;  // por ahora no quitamos las gotas
+    if ($item->id == $call->owner) continue;  // quitamos también al convocador
     ?>
     <li><a href="<?php echo '/user/profile/'.$item->id ?>" target="_blank"><img src="<?php echo $item->avatar->getLink(32, 32, true); ?>" alt="[G]" title="<?php echo $item->name; ?>" /></a></li>
 <?php endforeach; ?>
