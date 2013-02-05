2fdbb8d886f3e2bc534830f27fa61609c67cfb1b
Julian Portatil <git@doukeshi.org>
2013-02-06 01:20:42 +0900

    [caritas] que el contador corresponda con las caritas que se pintan (al quitar los sin imagen)

--- view/call/widget/supporters.html.php
+++ view/call/widget/supporters.html.php
@@ -6,17 +6,22 @@ use Goteo\Library\Text,
 
 $call = $this['call'];
 
-$cuantos = $call->getSupporters(true);
+//$cuantos = $call->getSupporters(true); // esto contaria independientemente del filtro y no seria igual que las caritas
 $supporters = $call->getSupporters();
+
+// para quitar los que no tengan imagen, los que tengan gotas o al convocador
+foreach ($supporters as $kay=>$item) {
+    if (!$item->avatar instanceof Image || $item->avatar->id == 1 || $item->id == $call->owner) unset($supporters[$kay]);
+}
+
+// así contamos tal cual las caritas que se van a pintar
+$cuantos = count($supporters);
+
 ?>
 <div id="supporters">
 	<h8 class="title"><?php echo Text::get('call-header-supporters', $cuantos) ?></h8>
 <ul>
-<?php foreach ($supporters as $item) :
-    if (!$item->avatar instanceof Image) continue;  // quitamos los que tengan la imagen rota
-    if ($item->avatar->id == 1) continue;  // por ahora no quitamos las gotas
-    if ($item->id == $call->owner) continue;  // quitamos también al convocador
-    ?>
+<?php foreach ($supporters as $item) : ?>
     <li><a href="<?php echo '/user/profile/'.$item->id ?>" target="_blank"><img src="<?php echo $item->avatar->getLink(32, 32, true); ?>" alt="[G]" title="<?php echo $item->name; ?>" /></a></li>
 <?php endforeach; ?>
 </ul>
