7aea8c78a1711343942b58f187c8d74fdcee7309
Julian Portatil <git@doukeshi.org>
2013-02-06 01:21:00 +0900

    [buzz] dinámico para testeo

--- view/call/widget/buzz.html.php
+++ view/call/widget/buzz.html.php
@@ -3,7 +3,30 @@ use Goteo\Library\Text;
 ?>
 <div id="side" class="twitter">
     <h2><?php echo Text::get('call-header-buzz'); ?></h2>
-	<!-- aqui el foreach -->
+<?php
+// Petición a twitter se desconecta en la Linea 448 en controller/call.php
+# if ($_SESSION['user']->id == 'root' && !empty($social->buzz_debug)) echo '<p>DEBUG:: '. $social->buzz_debug . '</p>';
+# if ($_SESSION['user']->id == 'root' && !empty($social->buzz)) echo \trace($social->buzz);
+
+$social = $this['social'];
+
+foreach ($social->buzz as $item) : ?>
+    <div class="tweet">
+        <div class="avatar">
+            <a href="<?php echo $item->profile ?>" target="_blank">
+                <img src="<?php echo $item->avatar ?>" alt="<?php echo $item->author ?>" title="<?php echo $item->user ?>"/>
+            </a>
+        </div>
+        <div class="text">
+            <strong><a href="<?php echo $item->profile ?>" target="_blank"><?php echo $item->user ?></a></strong>
+            <br />
+            <a href="<?php echo 'https://twitter.com/'.$item->twitter_user ?>" target="_blank"><?php echo '@'.$item->twitter_user ?></a>
+                <blockquote><?php echo $item->text ?></blockquote>
+        </div>
+    </div>
+<?php endforeach;
+?>
+	<!-- estaticos para maquetar
 	<div class="tweet">
 	    <div class="avatar">
 	        <a href="https://twitter.com/ACS_Arquisocial" target="_blank">
@@ -31,31 +54,6 @@ use Goteo\Library\Text;
 		    <blockquote>MagmaCultura News is out! http://t.co/4l5Irl7r ▸ Top stories today via @hoyesarte_com @platoniq</blockquote>
 	    </div>
 	</div>
-<?php
-/*
-// pintado estatico para maquetar
-//
-// Petición a twitter desconectada en Linea 448 en controller/call.php
-# if ($_SESSION['user']->id == 'root' && !empty($social->buzz_debug)) echo '<p>DEBUG:: '. $social->buzz_debug . '</p>';
-# if ($_SESSION['user']->id == 'root' && !empty($social->buzz)) echo \trace($social->buzz);
-
-$social = $this['social'];
-
-foreach ($social->buzz as $item) : ?>
-    <div class="tweet">
-        <div class="avatar">
-            <a href="<?php echo $item->profile ?>" target="_blank">
-                <img src="<?php echo $item->avatar ?>" alt="<?php echo $item->author ?>" title="<?php echo $item->user ?>"/>
-            </a>
-        </div>
-        <div class="text">
-            <strong><a href="<?php echo $item->profile ?>" target="_blank"><?php echo $item->user ?></a></strong>
-            <br />
-            <a href="<?php echo 'https://twitter.com/'.$item->twitter_user ?>" target="_blank"><?php echo '@'.$item->twitter_user ?></a>
-                <blockquote><?php echo $item->text ?></blockquote>
-        </div>
-    </div>
-<?php endforeach;
-*/ ?>
+        -->
 </div>
 
