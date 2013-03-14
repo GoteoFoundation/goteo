9b4652c3572ccf14f1da23a87bf25e4f6231779f
Julian Portatil <git@doukeshi.org>
2013-03-14 23:53:03 +0900

    [estructura] creacion de tablas y modelo

--- model/location.php
+++ model/location.php
@@ -8,25 +8,130 @@ namespace Goteo\Model {
     
     class Location extends \Goteo\Core\Model {
     
+        public
+            $id,
+            $name,
+            $location,
+            $region,
+            $country = 'España',
+            $lon,
+            $lat,
+            $valid = 1;
+
+
         /**
-         * Obtener datos de una localizacion (longitud, latitud, nombbre completo )
+         * Obtener datos de una localizacion (longitud, latitud, nombre completo(montado), poblacion, provincia, pais y si está validada )
          * @param   type mixed  $id     Identificador
-         * @return  type object         Objeto
+         * @return  type object         Instancia de geolocalización
          */
         static public function get ($id) {
-            return new stdClass();
+            try {
+                $query = static::query("SELECT *, CONCAT(location, region, country) as name FROM location WHERE id = ?", array($id));
+                $item = $query->fetchObject(__CLASS__);
+
+                return $item;
+            } catch(\PDOException $e) {
+                throw new \Goteo\Core\Exception($e->getMessage());
+            }
         }
 
         /**
-		 * Guardar.
+         * Lista de tareas
+         *
+         * @param  bool $visible    true|false
+         * @return mixed            Array de objetos de tareas
+         */
+        public static function getAll ($filters = array()) {
+
+            $values = array();
+
+            $list = array();
+
+            $sqlFilter = "";
+            $and = " WHERE";
+            if (isset($filters['valid']) && $filters['valid'] != 'all') {
+                if ($filters['valid']) {
+                    $sqlFilter .= "$and valid = 1";
+                    $and = " AND";
+                } else {
+                    $sqlFilter .= "$and valid = 0 OR valid IS NULL";
+                    $and = " AND";
+                }
+            }
+            if (!empty($filters['location'])) {
+                $sqlFilter .= "$and location LIKE :location";
+                $values[':location'] = $filters['location'];
+                $and = " AND";
+            }
+            if (!empty($filters['region'])) {
+                $sqlFilter .= "$and region LIKE :region";
+                $values[':region'] = $filters['region'];
+                $and = " AND";
+            }
+            if (!empty($filters['country'])) {
+                $sqlFilter .= "$and country LIKE :country";
+                $values[':country'] = $filters['country'];
+                $and = " AND";
+            }
+
+            if (!empty($filters['name'])) {
+                $sqlFilter .= "$and (location LIKE :name OR region LIKE :name OR country LIKE :name)";
+                $values[':name'] = "%".$filters['name']."%";
+                $and = " AND";
+            }
+
+            $sql = "SELECT *, CONCAT(location, region, country) as name
+                    FROM location
+                    $sqlFilter
+                    ORDER BY name ASC
+                    ";
+
+            $query = self::query($sql, $values);
+            foreach ($query->fetchAll(\PDO::FETCH_CLASS, __CLASS__) as $item) {
+                $list[] = $item;
+            }
+            return $list;
+        }
+
+        /**
+         * Guardar.
          * @param   type array  $errors     Errores devueltos pasados por referencia.
          * @return  type bool   true|false
          */
          public function save (&$errors = array()) {
-             if (!$this->validate()) return false;
+             if (!$this->validate())
+                return false;
 
-             return true;
-         }
+            $fields = array(
+                'id',
+                'location',
+                'region',
+                'country',
+                'lon',
+                'lat',
+                'valid'
+                );
+
+            $set = '';
+            $values = array();
+
+            foreach ($fields as $field) {
+                if ($set != '') $set .= ", ";
+                $set .= "`$field` = :$field ";
+                $values[":$field"] = $this->$field;
+            }
+
+            try {
+                $sql = "REPLACE INTO location SET " . $set;
+                self::query($sql, $values);
+                if (empty($this->id)) $this->id = self::insertId();
+
+                return true;
+            } catch(\PDOException $e) {
+                $errors[] = "HA FALLADO!!! " . $e->getMessage();
+                return false;
+            }
+        }
 
         /**
          * Validar.
@@ -34,8 +139,84 @@ namespace Goteo\Model {
          * @return  type bool   true|false
          */
         public function validate (&$errors = array()) {
-            return true;
+            if (empty($this->country))
+                $errors[] = 'Falta pais';
+
+            if (empty($this->lon))
+                $errors[] = 'Falta longitud';
+
+            if (empty($this->lat))
+                $errors[] = 'Falta latitud';
+
+            if (empty($errors))
+                return true;
+            else
+                return false;
         }
+
+
+        /*
+         * Lista simple de una columna filtrada por otra
+         */
+        public static function getList ($type = 'country', $filter = array('type'=>'', 'value'=>'')) {
+
+            $list = array();
+            $values = array();
+
+            if (!empty($filter['type']) && !empty($filter['value'])) {
+                $sqlFilter = " WHERE {$filter['type']} LIKE :fVal";
+                $values[':fVal'] = $filter['value'];
+            }
+
+            $sql = static::query("
+                SELECT
+                    DISTINCT({$type}) as name
+                FROM  location
+                $sqlFilter
+                ORDER BY name ASC
+                ", $values);
+
+            foreach ($sql->fetchAll(\PDO::FETCH_OBJ) as $item) {
+                $list[] = $item->name;
+            }
+
+            return $list;
+        }
+
+
+
+        /*
+         * Lista de elementos a checkear
+         */
+        public static function getCheck ($type = 'user', $limit) {
+
+            $list = array();
+
+            return $list;
+
+            $values = array();
+
+            if (!empty($filter['type']) && !empty($filter['value'])) {
+                $sqlFilter = " WHERE {$filter['type']} LIKE :fVal";
+                $values[':fVal'] = $filter['value'];
+            }
+
+            $sql = static::query("
+                SELECT
+                    DISTINCT({$type}) as name
+                FROM  location
+                $sqlFilter
+                ORDER BY name ASC
+                ", $values);
+
+            foreach ($sql->fetchAll(\PDO::FETCH_OBJ) as $item) {
+                $list[] = $item->name;
+            }
+
+            return $list;
+        }
+
+
 
     }
 }
