aa101db4231c3960edcbada713c206f658773163
Julian Akuma <jcanaves@doukeshi.org>
2013-04-28 16:48:39 +0200

    [Libreria] GeoLocation en vez de Location

---
+++ library/geoloc.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Goteo\Library {
+
+    use Goteo\Core\Model;
+
+	/*
+	 * Clase para cosas de localizaciones
+	 * Para sacar las existentes, las coincidencias proyecto-usuario, las de gmaps, y geo-localizacion
+	 */
+    class Geoloc {
+
+        /**
+         * Obtiene los datos de la dirección IP
+         * @param type $ip Dirección IP a buscar
+         */
+        public static function getIpData($ip) {
+            
+        }
+        
+	}
+
+}
