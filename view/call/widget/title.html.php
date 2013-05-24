e4496a4482dce8f103a5bdaf7cfdb85346b43d6b
Julian Akuma <jcanaves@doukeshi.org>
2013-05-24 19:00:10 +0200

    [convocatoria] cambios en la cabecera

--- view/call/widget/title.html.php
+++ view/call/widget/title.html.php
@@ -10,5 +10,5 @@ $URL = (NODE_ID != GOTEO_NODE) ? NODE_URL : SITE_URL;
 
 <div id="title">
 	<a href="<?php echo $URL ?>/call/<?php echo $call->id ?>"><img src="/data/images/<?php echo $call->logo->name /*$call->logo->getLink(250, 124, true)*/ ?>" alt="<?php echo $call->user->name ?>" class="logo" /></a>
-	<h2 class="title"><?php echo Text::get('call-splash-campaign_title') ?><br /><?php echo $call->name ?></h2>
+	<h2 class="title"><?php echo $call->name ?></h2>
 </div>
