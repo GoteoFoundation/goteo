2fc2b0c191f014beeff64207ce5172d2c1bc4afe
Julian Akuma <jcanaves@doukeshi.org>
2013-05-28 10:31:37 +0200

    [form convocatoria] editor enriquecido para enunciado

--- view/call/edit/overview.html.php
+++ view/call/edit/overview.html.php
@@ -55,53 +55,6 @@ $maxp_modes = array(
     )
 );
 
-/*
-<script type="text/javascript" src="/view/js/ckeditor/ckeditor.js"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-	// wysiwyg para la descripción de la convocatoria
-	CKEDITOR.replace('description_editor', {
-		toolbar: 'Full',
-		toolbar_Full: [
-				['Source','-'],
-				['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
-				['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-				'/',
-				['Bold','Italic','Underline','Strike'],
-				['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-				['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-				['Link','Unlink','Anchor'],
-                ['Image','Format','FontSize'],
-			  ],
-		skin: 'kama',
-		language: 'es',
-		height: '300px',
-		width: '660px'
-	});
-
-	// wysiwyg para los terminos y condiciones
-	CKEDITOR.replace('legal_editor', {
-		toolbar: 'Full',
-		toolbar_Full: [
-				['Source','-'],
-				['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
-				['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-				'/',
-				['Bold','Italic','Underline','Strike'],
-				['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-				['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-				['Link','Unlink','Anchor'],
-                ['Image','Format','FontSize'],
-			  ],
-		skin: 'kama',
-		language: 'es',
-		height: '300px',
-		width: '640px'
-	});
-});
-</script>
-*/
-
 $superform = array(
     'level'         => $this['level'],
     'action'        => '',
@@ -432,5 +385,32 @@ foreach ($superform['elements'] as $id => &$element) {
     }
     
 }
+?>
 
+<script type="text/javascript" src="/view/js/ckeditor/ckeditor.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	// wysiwyg para la descripción de la convocatoria
+	CKEDITOR.replace('description_editor', {
+		toolbar: 'Full',
+		toolbar_Full: [
+				['Source','-'],
+				['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+				['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+				'/',
+				['Bold','Italic','Underline','Strike'],
+				['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+				['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+				['Link','Unlink','Anchor'],
+                ['Image','Format','FontSize'],
+			  ],
+		skin: 'kama',
+		language: 'es',
+		height: '300px',
+		width: '660px'
+	});
+});
+</script>
+
+<?php
 echo new SuperForm($superform);
