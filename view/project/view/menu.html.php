a7cdc52593ea1ab44dd148a8e8b49bb99b73ac30
Julian Akuma <jcanaves@doukeshi.org>
2013-07-15 12:47:01 +0200

    [menu proyecto] tipsy

--- view/project/view/menu.html.php
+++ view/project/view/menu.html.php
@@ -1,16 +1,41 @@
-<?php use Goteo\Library\Text; ?>
+<?php use Goteo\Library\Text; 
+
+$items = array(
+    (object) array(
+        'id' => 'home',
+        'show' => Text::get('project-menu-home'),
+        'title' => Text::get('project-menu_title-home')
+    ),
+    (object) array(
+        'id' => 'needs',
+        'show' => Text::get('project-menu-needs'),
+        'title' => Text::get('project-menu_title-needs')
+    ),
+    (object) array(
+        'id' => 'supporters',
+        'show' => Text::get('project-menu-supporters').' <span class="digits">'.$this['supporters'].'</span>',
+        'title' => Text::get('project-menu_title-supporters')
+    ),
+    (object) array(
+        'id' => 'messages',
+        'show' => Text::get('project-menu-messages').' <span class="digits">'.$this['messages'].'</span>',
+        'title' => Text::get('project-menu_title-messages')
+    ),
+    (object) array(
+        'id' => 'updates',
+        'show' => Text::get('project-menu-updates').' <span class="digits">'.$this['updates'].'</span>',
+        'title' => Text::get('project-menu_title-updates')
+    )
+);
+
+
+?>
 <div class="project-menu">
     <ul>
         <?php
-        foreach (array(
-            'home'        => Text::get('project-menu-home'),
-            'needs'       => Text::get('project-menu-needs'),
-            'supporters'  => Text::get('project-menu-supporters').' <span class="digits">'.$this['supporters'].'</span>',
-            'messages'    => Text::get('project-menu-messages').' <span class="digits">'.$this['messages'].'</span>',
-            'updates'     => Text::get('project-menu-updates').' <span class="digits">'.$this['updates'].'</span>'
-        ) as $id => $show): ?>        
-        <li class="<?php echo $id ?><?php if ($this['show'] == $id) echo ' show' ?>">
-        	<a href="/project/<?php echo htmlspecialchars($this['project']->id) ?>/<?php echo $id ?>"><?php echo $show ?></a>
+        foreach ($items as $item): ?>        
+        <li class="<?php echo $item->id ?><?php if ($this['show'] == $item->id) echo ' show' ?>">
+        	<a href="/project/<?php echo htmlspecialchars($this['project']->id) ?>/<?php echo $item->id ?>" class="tipsy" title="<?php echo $item->title ?>"><?php echo $item->show ?></a>
         </li>
         <?php endforeach ?>        
     </ul>
