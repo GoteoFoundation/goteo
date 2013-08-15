8c51deefbc0410917517b87f78a4fd3eec49a73c
Julian Akuma <jcanaves@doukeshi.org>
2013-08-15 20:49:54 +0200

    Flags de control de proceso

--- db/contract_status/structure.sql
+++ db/contract_status/structure.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `contract_status` (
-`contract`  bigint(20) unsigned NOT NULL,
+`contract`  VARCHAR( 50 ) NOT NULL COMMENT 'Id del proyecto',
 `owner` int(1) NOT NULL DEFAULT '0' COMMENT 'El impulsor ha dado por rellenados los datos',
-`admin` int(1) NOT NULL DEFAULT '0' COMMENT 'El admin ha dado por válidos los datos',
+`admin` int(1) NOT NULL DEFAULT '0' COMMENT 'El admin ha comenzado a revisar los datos',
+`ready` int(1) NOT NULL DEFAULT '0' COMMENT 'Datos verificados y correctos',
+`recieved` int(1) NOT NULL DEFAULT '0' COMMENT 'Se ha recibido el contrato firmado',
+`payed` int(1) NOT NULL DEFAULT '0' COMMENT 'Se ha realizado el pago al proyecto',
 `pdf` varchar(255) DEFAULT NULL,
 PRIMARY KEY ( `contract` )
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT = 'Seguimiento de estado de contrato';
 
--- el id de contrato es el id de proyecto
-ALTER TABLE `contract_status` CHANGE `contract` `contract` VARCHAR( 50 ) NOT NULL COMMENT 'Id del proyecto';
