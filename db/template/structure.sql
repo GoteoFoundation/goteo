da62d64ec5e7370aada829ec4ab413921c302571
Julian Akuma <jcanaves@doukeshi.org>
2013-09-25 04:14:48 +0200

    Estructura y modificación de registros para filtro de agrupación * nuevas plantillas

--- db/template/structure.sql
+++ db/template/structure.sql
@@ -4,4 +4,8 @@ CREATE TABLE `template` (
 `purpose` TINYTEXT NOT NULL ,
 `title` TINYTEXT NOT NULL ,
 `text` TEXT NOT NULL
-) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT = 'Plantillas emails automáticos'
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT = 'Plantillas emails automáticos';
+
+
+-- Agrupacion de usos de las plantillas
+ALTER TABLE `template` ADD `group` VARCHAR( 50 ) NOT NULL DEFAULT 'general' COMMENT 'Agrupación de uso' AFTER `name`;
