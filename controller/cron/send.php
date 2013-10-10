141d1490bfa2c9a1b672c28304858c7045aae6f9
Julian Akuma <jcanaves@doukeshi.org>
2013-10-10 15:12:33 +0200

    Monitoring: Mercè en bcc

--- controller/cron/send.php
+++ controller/cron/send.php
@@ -178,8 +178,8 @@ namespace Goteo\Controller\Cron {
                 $mailHandler->to = $project->user->email;
                 $mailHandler->toName = $project->user->name;
                 // monitorización solo para 'quien-manda'
-                if ($project->id == 'quien-manda') 
-                    $mailHandler->bcc = array('enric@goteo.org', 'maria@goteo.org', 'olivier@goteo.org', 'jcanaves@doukeshi.org');
+                if ($project->id == 'quien-manda' || $project->id == 'despliegue-de-guifi-net-en-extremadura') 
+                    $mailHandler->bcc = array('enric@goteo.org', 'maria@goteo.org', 'olivier@goteo.org', 'jcanaves@doukeshi.org', 'info@goteo.org');
                 
                 // si es un proyecto de nodo: reply al mail del nodo
                 // si es de centra: reply a MAIL_GOTEO
