dae3a7efa3df195e8220671c9ded2f1e64896893
Julian Akuma <jcanaves@doukeshi.org>
2013-11-30 11:19:40 +0100

    Actualización del código UrlLinker

--- library/text.php
+++ library/text.php
@@ -588,9 +588,9 @@ namespace Goteo\Library {
          * -------------------------------------------------------------------------------
          *  UrlLinker - facilitates turning plaintext URLs into HTML links.
          *
-         *  Author: SÃ¸ren LÃ¸vborg
+         *  Author: Søren Løvborg
          *
-         *  To the extent possible under law, SÃ¸ren LÃ¸vborg has waived all copyright
+         *  To the extent possible under law, Søren Løvborg has waived all copyright
          *  and related or neighboring rights to UrlLinker.
          *  http://creativecommons.org/publicdomain/zero/1.0/
          * -------------------------------------------------------------------------------
@@ -600,13 +600,21 @@ namespace Goteo\Library {
             /*
              *  Regular expression bits used by htmlEscapeAndLinkUrls() to match URLs.
              */
-            $rexProtocol = '(https?://)?';
-            $rexDomain   = '((?:[-a-zA-Z0-9]{1,63}\.)+[-a-zA-Z0-9]{2,63}|(?:[0-9]{1,3}\.){3}[0-9]{1,3})';
-            $rexPort     = '(:[0-9]{1,5})?';
-            $rexPath     = '(/[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]*?)?';
-            $rexQuery    = '(\?[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
-            $rexFragment = '(#[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
-            $rexUrlLinker = "{\\b$rexProtocol$rexDomain$rexPort$rexPath$rexQuery$rexFragment(?=[?.!,;:\)\"]?(,|\s|\)|$))}";
+            $rexScheme    = 'https?://';
+            // $rexScheme    = "$rexScheme|ftp://"; // Uncomment this line to allow FTP addresses.
+            $rexDomain    = '(?:[-a-zA-Z0-9]{1,63}\.)+[a-zA-Z][-a-zA-Z0-9]{1,62}';
+            $rexIp        = '(?:[1-9][0-9]{0,2}\.|0\.){3}(?:[1-9][0-9]{0,2}|0)';
+            $rexPort      = '(:[0-9]{1,5})?';
+            $rexPath      = '(/[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]*?)?';
+            $rexQuery     = '(\?[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
+            $rexFragment  = '(#[!$-/0-9?:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
+            $rexUsername  = '[^]\\\\\x00-\x20\"(),:-<>[\x7f-\xff]{1,64}';
+            $rexPassword  = $rexUsername; // allow the same characters as in the username
+            $rexUrl       = "($rexScheme)?(?:($rexUsername)(:$rexPassword)?@)?($rexDomain|$rexIp)($rexPort$rexPath$rexQuery$rexFragment)";
+            $rexTrailPunct= "[)'?.!,;:]"; // valid URL characters which are not part of the URL if they appear at the very end
+            $rexNonUrl    = "[^-_#$+.!*'(),;/?:@=&a-zA-Z0-9]"; // characters that should never appear in a URL
+            $rexUrlLinker = "{\\b$rexUrl(?=$rexTrailPunct*($rexNonUrl|$))}";
+            // $rexUrlLinker .= 'i'; // Uncomment this line to allow uppercase URL schemes (e.g. "HTTP://google.com").
 
             /**
              *  $validTlds is an associative array mapping valid TLDs to the value true.
@@ -614,15 +622,16 @@ namespace Goteo\Library {
              *  from time to time.
              *
              *  List source:  http://data.iana.org/TLD/tlds-alpha-by-domain.txt
-             *  Last updated: 2010-09-04
+             *  Last updated: 2012-09-06
              */
-            $validTlds = array_fill_keys(explode(" ", ".ac .ad .ae .aero .af .ag .ai .al .am .an .ao .aq .ar .arpa .as .asia .at .au .aw .ax .az .ba .bb .bd .be .bf .bg .bh .bi .biz .bj .bm .bn .bo .br .bs .bt .bv .bw .by .bz .ca .cat .cc .cd .cf .cg .ch .ci .ck .cl .cm .cn .co .com .coop .cr .cu .cv .cx .cy .cz .de .dj .dk .dm .do .dz .ec .edu .ee .eg .er .es .et .eu .fi .fj .fk .fm .fo .fr .ga .gb .gd .ge .gf .gg .gh .gi .gl .gm .gn .gov .gp .gq .gr .gs .gt .gu .gw .gy .hk .hm .hn .hr .ht .hu .id .ie .il .im .in .info .int .io .iq .ir .is .it .je .jm .jo .jobs .jp .ke .kg .kh .ki .km .kn .kp .kr .kw .ky .kz .la .lb .lc .li .lk .lr .ls .lt .lu .lv .ly .ma .mc .md .me .mg .mh .mil .mk .ml .mm .mn .mo .mobi .mp .mq .mr .ms .mt .mu .museum .mv .mw .mx .my .mz .na .name .nc .ne .net .nf .ng .ni .nl .no .np .nr .nu .nz .om .org .pa .pe .pf .pg .ph .pk .pl .pm .pn .pr .pro .ps .pt .pw .py .qa .re .ro .rs .ru .rw .sa .sb .sc .sd .se .sg .sh .si .sj .sk .sl .sm .sn .so .sr .st .su .sv .sy .sz .tc .td .tel .tf .tg .th .tj .tk .tl .tm .tn .to .tp .tr .travel .tt .tv .tw .tz .ua .ug .uk .us .uy .uz .va .vc .ve .vg .vi .vn .vu .wf .ws .xn--0zwm56d .xn--11b5bs3a9aj6g .xn--80akhbyknj4f .xn--9t4b11yi5a .xn--deba0ad .xn--fiqs8s .xn--fiqz9s .xn--fzc2c9e2c .xn--g6w251d .xn--hgbk6aj7f53bba .xn--hlcj6aya9esc7a .xn--j6w193g .xn--jxalpdlp .xn--kgbechtv .xn--kprw13d .xn--kpry57d .xn--mgbaam7a8h .xn--mgbayh7gpa .xn--mgberp4a5d4ar .xn--o3cw4h .xn--p1ai .xn--pgbs0dh .xn--wgbh1c .xn--xkc2al3hye2a .xn--ygbi2ammx .xn--zckzah .ye .yt .za .zm .zw"), true);
+            $validTlds = array_fill_keys(explode(" ", ".ac .ad .ae .aero .af .ag .ai .al .am .an .ao .aq .ar .arpa .as .asia .at .au .aw .ax .az .ba .bb .bd .be .bf .bg .bh .bi .biz .bj .bm .bn .bo .br .bs .bt .bv .bw .by .bz .ca .cat .cc .cd .cf .cg .ch .ci .ck .cl .cm .cn .co .com .coop .cr .cu .cv .cw .cx .cy .cz .de .dj .dk .dm .do .dz .ec .edu .ee .eg .er .es .et .eu .fi .fj .fk .fm .fo .fr .ga .gb .gd .ge .gf .gg .gh .gi .gl .gm .gn .gov .gp .gq .gr .gs .gt .gu .gw .gy .hk .hm .hn .hr .ht .hu .id .ie .il .im .in .info .int .io .iq .ir .is .it .je .jm .jo .jobs .jp .ke .kg .kh .ki .km .kn .kp .kr .kw .ky .kz .la .lb .lc .li .lk .lr .ls .lt .lu .lv .ly .ma .mc .md .me .mg .mh .mil .mk .ml .mm .mn .mo .mobi .mp .mq .mr .ms .mt .mu .museum .mv .mw .mx .my .mz .na .name .nc .ne .net .nf .ng .ni .nl .no .np .nr .nu .nz .om .org .pa .pe .pf .pg .ph .pk .pl .pm .pn .post .pr .pro .ps .pt .pw .py .qa .re .ro .rs .ru .rw .sa .sb .sc .sd .se .sg .sh .si .sj .sk .sl .sm .sn .so .sr .st .su .sv .sx .sy .sz .tc .td .tel .tf .tg .th .tj .tk .tl .tm .tn .to .tp .tr .travel .tt .tv .tw .tz .ua .ug .uk .us .uy .uz .va .vc .ve .vg .vi .vn .vu .wf .ws .xn--0zwm56d .xn--11b5bs3a9aj6g .xn--3e0b707e .xn--45brj9c .xn--80akhbyknj4f .xn--80ao21a .xn--90a3ac .xn--9t4b11yi5a .xn--clchc0ea0b2g2a9gcd .xn--deba0ad .xn--fiqs8s .xn--fiqz9s .xn--fpcrj9c3d .xn--fzc2c9e2c .xn--g6w251d .xn--gecrj9c .xn--h2brj9c .xn--hgbk6aj7f53bba .xn--hlcj6aya9esc7a .xn--j6w193g .xn--jxalpdlp .xn--kgbechtv .xn--kprw13d .xn--kpry57d .xn--lgbbat1ad8j .xn--mgb9awbf .xn--mgbaam7a8h .xn--mgbayh7gpa .xn--mgbbh1a71e .xn--mgbc0a9azcg .xn--mgberp4a5d4ar .xn--o3cw4h .xn--ogbpf8fl .xn--p1ai .xn--pgbs0dh .xn--s9brj9c .xn--wgbh1c .xn--wgbl6a .xn--xkc2al3hye2a .xn--xkc2dl3a5ee0h .xn--yfro4i67o .xn--ygbi2ammx .xn--zckzah .xxx .ye .yt .za .zm .zw"), true);
+
 
             /**
              *  Transforms plain text into valid HTML, escaping special characters and
              *  turning URLs into links.
              */
-            $result = "";
+            $html = '';
 
             $position = 0;
             while (preg_match($rexUrlLinker, $text, $match, PREG_OFFSET_CAPTURE, $position))
@@ -630,30 +639,58 @@ namespace Goteo\Library {
                 list($url, $urlPosition) = $match[0];
 
                 // Add the text leading up to the URL.
-                $result .= htmlspecialchars(substr($text, $position, $urlPosition - $position));
+                $html .= htmlspecialchars(substr($text, $position, $urlPosition - $position));
 
-                $domain = $match[2][0];
-                $port   = $match[3][0];
-                $path   = $match[4][0];
+                $scheme      = $match[1][0];
+                $username    = $match[2][0];
+                $password    = $match[3][0];
+                $domain      = $match[4][0];
+                $afterDomain = $match[5][0]; // everything following the domain
+                $port        = $match[6][0];
+                $path        = $match[7][0];
 
-                // Check that the TLD is valid (or that $domain is an IP address).
-                // Quitamos esto para que no confunda los millares con ips
-                //preg_match('{^\.[0-9]{1,3}$}', $tld) ||
+                // Check that the TLD is valid or that $domain is an IP address.
                 $tld = strtolower(strrchr($domain, '.'));
-                if (isset($validTlds[$tld]))
+                if (preg_match('{^\.[0-9]{1,3}$}', $tld) || isset($validTlds[$tld]))
                 {
-                    // Prepend http:// if no protocol specified
-                    $completeUrl = $match[1][0] ? $url : "http://$url";
+                    // Do not permit implicit scheme if a password is specified, as
+                    // this causes too many errors (e.g. "my email:foo@example.org").
+                    if (!$scheme && $password)
+                    {
+                        $html .= htmlspecialchars($username);
+
+                        // Continue text parsing at the ':' following the "username".
+                        $position = $urlPosition + strlen($username);
+                        continue;
+                    }
+
+                    if (!$scheme && $username && !$password && !$afterDomain)
+                    {
+                        // Looks like an email address.
+                        $completeUrl = "mailto:$url";
+                        $linkText = $url;
+                    }
+                    else
+                    {
+                        // Prepend http:// if no scheme is specified
+                        $completeUrl = $scheme ? $url : "http://$url";
+                        $linkText = "$domain$port$path";
+                    }
+
+                    $linkHtml = '<a href="' . htmlspecialchars($completeUrl) . '">'
+                        . htmlspecialchars($linkText)
+                        . '</a>';
+
+                    // Cheap e-mail obfuscation to trick the dumbest mail harvesters.
+                    $linkHtml = str_replace('@', '&#64;', $linkHtml);
 
                     // Add the hyperlink.
-                    $result .= '<a href="' . htmlspecialchars($completeUrl) . '" target="_blank">'
-                        . htmlspecialchars("$domain$port$path")
-                        . '</a>';
+                    $html .= $linkHtml;
                 }
                 else
                 {
                     // Not a valid URL.
-                    $result .= htmlspecialchars($url);
+                    $html .= htmlspecialchars($url);
                 }
 
                 // Continue text parsing from after the URL.
@@ -661,9 +698,8 @@ namespace Goteo\Library {
             }
 
             // Add the remainder of the text.
-            $result .= htmlspecialchars(substr($text, $position));
-            return $result;
-
+            $html .= htmlspecialchars(substr($text, $position));
+            return $html;
 
 		}
 
