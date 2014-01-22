eb55bad6846e53fa2b43f755a724d5cdd4c1358f
Ivan Vergés <ivan@microstudi.net>
2014-01-23 00:17:01 +0100

    mejorado el cron-sender.sh, ahora comprime los logs de envios antiguos

--- cron-sender.sh
+++ cron-sender.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-echo "cron-sender: "`date` > logs/cli-sender.log
+echo "comprimiendo archivos antiguos..."
+tar cfz "logs/sender-$(date +%Y-%m-%dT%H:%I).tar.gz" logs/cli-send*.log
+echo "procesando envios..."
+rm logs/cli-send*.log
+echo "cron-sender:$(date)" > logs/cli-sender.log
 /usr/bin/php cli-sender.php >> logs/cli-sender.log 2>&1
 
