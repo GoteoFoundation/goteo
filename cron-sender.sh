d5fe0273c3ce506a4a97fd0dcd476fcc869a1d2c
Tracy <tracy@localhost>
2014-01-23 13:41:47 +0100

    test

--- cron-sender.sh
+++ cron-sender.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-echo "comprimiendo archivos antiguos..."
+echo "Comprimiendo archivos antiguos..."
 tar cfz "logs/sender-$(date +%Y-%m-%dT%H:%I).tar.gz" logs/cli-send*.log
-echo "procesando envios..."
+echo "Procesando envios..."
 rm logs/cli-send*.log
 echo "cron-sender:$(date)" > logs/cli-sender.log
 /usr/bin/php cli-sender.php >> logs/cli-sender.log 2>&1
