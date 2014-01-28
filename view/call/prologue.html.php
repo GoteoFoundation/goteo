2b6d2cd730120de0885263e694b45e4ccbb93df8
Javier Carrillo <javicarrillo83@gmail.com>
2014-01-28 19:33:32 +0100

    Creadas nuevas clases adaptadas por tipo dispositivo a call. HMTL5 + meta etiquetas. Probando prototipos.

--- view/call/prologue.html.php
+++ view/call/prologue.html.php
@@ -21,8 +21,8 @@ if ($call->image instanceof Goteo\Model\Image) {
 
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE HTML>
+<html>
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -33,6 +33,7 @@ if ($call->image instanceof Goteo\Model\Image) {
         <meta name="author" content="<?php echo GOTEO_META_AUTHOR ?>" />
         <meta name="copyright" content="<?php echo GOTEO_META_COPYRIGHT ?>" />
         <meta name="robots" content="all" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
         <meta property="og:title" content="<?php echo $call->name; ?>" />
         <meta property="og:description" content="<?php echo $call->subtitle; ?>" />
@@ -90,6 +91,11 @@ if ($call->image instanceof Goteo\Model\Image) {
                }
     </style>
    
+    <link rel="stylesheet" href="<?php echo SRC_URL ?>/view/css/call/minimobile.css" media="only screen and (max-width:340px)">
+        <link rel="stylesheet" href="<?php echo SRC_URL ?>/view/css/call/mobile.css" media="only screen and (min-width:340px) and (max-width:750px)">
+        <link rel="stylesheet" href="<?php echo SRC_URL ?>/view/css/call/tablet.css" media="only screen and (min-width:750px) and (max-width:1023px)">
+        <link rel="stylesheet" href="<?php echo SRC_URL ?>/view/css/call/pc.css" media="only screen and (min-width:1024px) and (max-width:1400px)">
+        <link rel="stylesheet" href="<?php echo SRC_URL ?>/view/css/call/bigpc.css" media="only screen and (min-width:1400px)">
     <?php endif; ?>
 
       <!--[if IE]>
