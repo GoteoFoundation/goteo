e22e22b440e0fb1582f15c3d4af8021a0a79392e
Javier Carrillo <javicarrillo83@gmail.com>
2014-02-13 19:53:31 +0100

    Obtener imagen sección play-video en vista media. Colocar botón ver vídeo en el centro de la imagen.

--- view/project/widget/media.html.php
+++ view/project/widget/media.html.php
@@ -1,6 +1,15 @@
-<?php if (!empty($this['project']->media)): ?>
+<?php 
+
+$img_url=$this['project']->secGallery['play-video'][0]->imageData->getLink(620, 380);
+
+if (!empty($this['project']->media)): ?>
 <div class="widget project-media" <?php if ($this['project']->media_usubs) : ?>style="height:412px;"<?php endif; ?>>
-    <?php echo $this['project']->media->getEmbedCode($this['project']->media_usubs); ?>
-    <?php //echo putImages($this['project']->secGallery['play-video']); ?>
+	<div style="position:relative;">
+		<img src="<?php echo $img_url; ?>" width="620" height="380"/>  
+		<div style="position:absolute;top:150px;left:272px;">
+		<a href="#" class="button" target="_blank">Ver video</a>
+		</div>
+	</div>
+
 </div>
 <?php endif ?>
