54c530f52b30c952f0c3f2ce3ea71059d0131e4d
Javier Carrillo <javicarrillo83@gmail.com>
2014-02-13 14:11:59 +0100

    Sección Dentro Vídeo agregada.

--- view/project/widget/media.html.php
+++ view/project/widget/media.html.php
@@ -1,5 +1,6 @@
 <?php if (!empty($this['project']->media)): ?>
 <div class="widget project-media" <?php if ($this['project']->media_usubs) : ?>style="height:412px;"<?php endif; ?>>
     <?php echo $this['project']->media->getEmbedCode($this['project']->media_usubs); ?>
+    <?php //echo putImages($this['project']->secGallery['play-video']); ?>
 </div>
 <?php endif ?>
