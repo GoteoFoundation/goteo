940bb3a3595c06439a02139abef9fab2e52b7206
Javier Carrillo <javicarrillo83@gmail.com>
2014-02-14 11:17:27 +0100

    Carga de vídeo por javascript funcionando provisionalmente. Agregado play al botón.

--- view/project/widget/media.html.php
+++ view/project/widget/media.html.php
@@ -1,14 +1,25 @@
+<script>
+
+function loadVideo()
+
+{
+
+    var vid = document.getElementById('video_holder');
+
+    vid.innerHTML = '<iframe width="640" height="360" src="//www.youtube.com/embed/F-N7vv0IdbQ" frameborder="0" allowfullscreen></iframe>';
+   
+}
+</script>
+
 <?php 
 
 $img_url=$this['project']->secGallery['play-video'][0]->imageData->getLink(620, 380);
 
 if (!empty($this['project']->media)): ?>
 <div class="widget project-media" <?php if ($this['project']->media_usubs) : ?>style="height:412px;"<?php endif; ?>>
-	<div style="position:relative;">
+	<div style="position:relative;" id="video_holder">
 		<img src="<?php echo $img_url; ?>" width="620" height="380"/>  
-		<div style="position:absolute;top:150px;left:272px;">
-		<a href="#" class="button" target="_blank">Ver video</a>
-		</div>
+		<div onclick="loadVideo()" class="video_button" style="position:absolute;left:236.5px;top:169px; font-size:18px;"><img src="/view/css/project/widget/play.png" width="6"style="margin-right:12px;"/><strong>Ver video</strong></div>
 	</div>
 
 </div>
