e84470584ccd9a2af15d98e8f4739488d1d0d01c
Javier Carrillo <javicarrillo83@gmail.com>
2014-02-14 13:14:19 +0100

    Consideración no existe imagen preview.

--- view/project/widget/media.html.php
+++ view/project/widget/media.html.php
@@ -6,21 +6,29 @@ function loadVideo()
 
     var vid = document.getElementById('video_holder');
 
-    vid.innerHTML = '<iframe width="640" height="360" src="//www.youtube.com/embed/F-N7vv0IdbQ" frameborder="0" allowfullscreen></iframe>';
+    vid.innerHTML = '<?php if(!empty($this['project']->media))  echo $this['project']->media->getEmbedCode($this['project']->media_usubs); ?>';
    
 }
 </script>
 
 <?php 
 
-$img_url=$this['project']->secGallery['play-video'][0]->imageData->getLink(620, 380);
+if (!empty($this['project']->media)) {
 
-if (!empty($this['project']->media)): ?>
-<div class="widget project-media" <?php if ($this['project']->media_usubs) : ?>style="height:412px;"<?php endif; ?>>
-	<div style="position:relative;" id="video_holder">
+	if(!empty($this['project']->secGallery['play-video'][0])) 
+	{
+
+	$img_url=$this['project']->secGallery['play-video'][0]->imageData->getLink(620, 380);
+
+	?>
+	<div class="widget project-media" <?php if ($this['project']->media_usubs) : ?>style="height:412px;"<?php endif; ?> style="position:relative;" id="video_holder">	
 		<img src="<?php echo $img_url; ?>" width="620" height="380"/>  
-		<div onclick="loadVideo()" class="video_button" style="position:absolute;left:236.5px;top:169px; font-size:18px;"><img src="/view/css/project/widget/play.png" width="6"style="margin-right:12px;"/><strong>Ver video</strong></div>
+		<div onclick="loadVideo()" class="video_button"><img src="/view/css/project/widget/play.png" width="6"style="margin-right:12px;"/>Ver video</div>
 	</div>
-
-</div>
-<?php endif ?>
+<?php }
+	else { ?>
+	
+	<?php 
+	}
+}
+?>
