73997e91039cb6227cd53379da2e3c7170933b0e
Javier Carrillo <javicarrillo83@gmail.com>
2014-03-04 19:57:51 +0100

    Ajustes cajas maquetación stories.

--- view/home/stories.html.php
+++ view/home/stories.html.php
@@ -30,16 +30,16 @@ if (count($stories) > 6) {
         <?php foreach ($stories as $story) : ?>
             <div class="stories-banner<?php if (!empty($story->url)) echo ' activable'; ?>"<?php if ($story->image instanceof \Goteo\Model\Image) : ?> style="background: url('/data/images/<?php echo $story->image->name; ?>');"<?php endif; ?>>
                 <?php if (!empty($story->url)) : ?><a href="<?php echo $story->url; ?>" class="expand" target="_blank"></a><?php endif; ?>
-                <h2 class="title" style="margin-top:50px; margin-left:50px; color:#ee4843; background:none;"><?php echo Text::get('home-stories-header'); ?></h2>
+                <div class="title_story"><?php echo Text::get('home-stories-header'); ?></div>
                 <div class="info">
                     <div><?php echo htmlspecialchars($story->title); ?></div>
                     <div style="margin-top:5px;"><?php echo htmlspecialchars($story->description); ?></div>
                     <div style="margin-top:5px;"><?php echo htmlspecialchars($story->name); ?></div>
                     <div><?php echo htmlspecialchars($story->node); ?></div>
                 </div>
-                <div class="info_right">
-                    <div>15 cofinanciadores</div>
-                    <div style="margin-top:5px;">3000 € conseguidos</div>
+                <div class="info_extra">
+                    <span style="color:#313B96; font-size:15px;">COFINANCIADORES <strong style="font-size:18px;">80</strong></span>
+                    <span style="color:#96238F; margin-left:10px;">OBTENIDO <strong>2.500</strong></span>
                 </div>
             </div>
         <?php endforeach; ?>
