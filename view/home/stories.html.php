c24a39622241084b7a4d2cfc0e2e10c81735d0f7
Javier Carrillo <javicarrillo83@gmail.com>
2014-03-05 19:31:11 +0100

    Ajustes en fuentes y margins stories.

--- view/home/stories.html.php
+++ view/home/stories.html.php
@@ -30,17 +30,16 @@ if (count($stories) > 6) {
         <?php foreach ($stories as $story) : ?>
             <div class="stories-banner<?php if (!empty($story->url)) echo ' activable'; ?>"<?php if ($story->image instanceof \Goteo\Model\Image) : ?> style="background: url('/data/images/<?php echo $story->image->name; ?>');"<?php endif; ?>>
                 <?php if (!empty($story->url)) : ?><a href="<?php echo $story->url; ?>" class="expand" target="_blank"></a><?php endif; ?>
-                <div class="title_story"><?php echo Text::get('home-stories-header'); ?></div>
+                <div class="title_story"><strong><?php echo Text::get('home-stories-header').": "?></strong><span style="text-decoration:underline">Open Data</span></div>
                 <div class="info">
-                    <div><?php echo htmlspecialchars($story->title); ?></div>
-                    <div style="margin-top:5px;"><?php echo htmlspecialchars($story->description); ?>Descripción de Architecture in your hand. Architecture in your hand. Architecture in your hand.
-ARCHINHAND | Architecture in your</div>
-                    <div style="margin-top:5px;"><?php echo htmlspecialchars($story->name); ?></div>
-                    <div><?php echo htmlspecialchars($story->node); ?></div>
+                    <div style="font-size:18px;"><strong><?php echo htmlspecialchars($story->title); ?></strong></div>
+                    <div style="margin-top:5px; color:#BCE8E8; font-size:15px;"><?php echo htmlspecialchars($story->name); ?></div>
+                    <div style="border-bottom-style:solid; border-bottom-width:1px; width:15px; margin-top:7px; margin-bottom:7px;"></div>
+                    <div style="color:#58595B"><?php echo htmlspecialchars($story->description); ?></div>
                 </div>
                 <div class="info_extra">
-                    <span style="color:#313B96; font-size:12px;">COFINANCIADORES <strong style="font-size:20px;">80</strong></span>
-                    <span style="color:#96238F; font-size:12px; margin-left:10px;">OBTENIDO <strong style="font-size:20px;">2.500</strong><img style="vertical-align: middle; margin-left:5px;" src="/view/css/euro/violet/xl.png" width="20"/></span>
+                    <span style="color:#313B96; font-size:11px;"><span style="position:absolute; bottom:16px;">COFINANCIADORES</span> <strong style="font-size:20px; margin-left:110px;">1000</strong></span>
+                    <span style="color:#96238F; font-size:11px; margin-left:40px;"><span style="position:absolute; bottom:16px;">OBTENIDO</span><strong style="font-size:20px; margin-left:63px;">2.500</strong><img style=" vertical-align: text-bottom; margin-left:5px;" src="/view/css/euro/violet/xl.png" width="20"/></span>
                 </div>
             </div>
         <?php endforeach; ?>
