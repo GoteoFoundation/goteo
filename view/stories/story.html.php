3a9d294105065f6575dfec0e1ac64eefb3f66c43
Javier Carrillo <javicarrillo83@gmail.com>
2014-04-12 12:54:06 +0200

    Creación vista unificada story individual.

---
+++ view/stories/story.html.php
@@ -0,0 +1,35 @@
+<?php
+
+use Goteo\Core\View,
+    Goteo\Library\Text;
+
+$story = $this['story'];
+
+if(!empty($story->post))
+    $link_title="/blog/".$story->post;
+else if(!empty($story->url))
+    $link_title=$story->url;
+else
+    $link_title='/project/'.$story->project->id;
+if(empty($story->url))
+    $link_background='/project/'.$story->project->id;
+else
+    $link_background=$story->url;
+
+?>
+<div class="stories-banner<?php if (!empty($story->project)) echo ' activable'; ?>"<?php if ($story->image instanceof \Goteo\Model\Image) : ?> style="background: url('<?php echo $story->image->getLink(940, 385, true); ?>');"<?php endif; ?>>
+    <a href="<?php echo $link_background;?>" class="expand" target="_blank"></a>
+    <div class="title_story"><strong><?php echo Text::get('home-stories-header').': '?></strong><span style="text-decoration:underline"><?php print_r($story->project->open_tags); ?></span></div>
+        <div class="info">
+            <a href="<?php echo $link_title;?>" target="_blank">
+                <div id="info_title"><?php echo htmlspecialchars($story->title); ?></div>
+                <div id="review"><?php echo htmlspecialchars($story->review); ?></div>
+            </a>
+            <div id="line"></div>
+            <div id="description"><blockquote><?php echo htmlspecialchars($story->description).'</blockquote>. '.'<a href="/user/profile/'.$story->project->user->id.'" target="_blank">'.htmlspecialchars($story->project->user->name).'</a>, del proyecto: '.'<a href="/project/'.$story->project->id.'" target="_blank">'.htmlspecialchars($story->project->name).'</a>'; ?></div>
+        </div>
+        <div class="info_extra">
+            <span id="cofinanciadores"><?php echo  mb_strtoupper(Text::get('project-view-metter-investors'));?></span> <strong id="ncofinanciadores"><?php echo $story->project->num_investors;?></strong>
+            <span id="obtenido"><span><?php echo  mb_strtoupper(Text::get('project-view-metter-got'));?></span><strong><?php echo $story->project->amount;?></strong><img src="/view/css/euro/violet/xl.png" width="20"/></span>
+        </div>            
+    </div>                                   
