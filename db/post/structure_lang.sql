aa91d741bbca6774093bbfa988a9a95b36e978e7
Julian Canaves <julian.canaves@gmail.com>
2014-08-04 19:25:06 +0200

    Campo blog en post_lang

--- db/post/structure_lang.sql
+++ db/post/structure_lang.sql
@@ -12,3 +12,10 @@ ALTER TABLE `post_lang` ADD `media` TINYTEXT NULL ;
 
 -- pendiente de traducir
 ALTER TABLE `post_lang` ADD `pending` INT( 1 ) NULL DEFAULT '0' COMMENT 'Debe revisarse la traducción';
+
+-- indice blog
+ALTER TABLE `post_lang` ADD `blog` INT( 20 ) NOT NULL AFTER `id` ,
+ADD INDEX ( `blog` );
+
+-- rellenar este campo
+UPDATE post_lang SET post_lang.blog = (SELECT post.blog FROM post WHERE post.id = post_lang.id);
