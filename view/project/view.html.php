6b23e9c7973c1aa8f34a1b583d7b763bd4d0314a
Pablo Castellano <pablo@anche.no>
2014-08-13 11:21:23 +0200

    Aplica htmlspecialchars() en og:title

--- view/project/view.html.php
+++ view/project/view.html.php
@@ -42,7 +42,7 @@ $bodyClass = 'project-show';
 
 // metas og: para que al compartir en facebook coja bien el nombre y la imagen (todas las de proyecto y las novedades
 $ogmeta = array(
-    'title' => $project->name,
+    'title' => htmlspecialchars($project->name, ENT_QUOTES),
     'description' => Text::get('regular-by').' '.$project->user->name,
     'url' => $URL . '/project/'.$project->id
 );
