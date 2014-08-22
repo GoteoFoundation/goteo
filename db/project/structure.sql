2074ca7121e75cfae27c62bc9d9a1a9847ea8f1e
Javier Carrillo <javicarrillo83@gmail.com>
2014-08-22 19:20:27 +0200

    Nuevos campos precalculados en project para usar en convocatoria.

--- db/project/structure.sql
+++ db/project/structure.sql
@@ -119,3 +119,15 @@ ALTER TABLE `project` ADD COLUMN `num_messengers` INT UNSIGNED DEFAULT 0 NOT NUL
 
 -- Añadido numero de posts
 ALTER TABLE `project` ADD COLUMN `num_posts` INT UNSIGNED DEFAULT 0 NOT NULL COMMENT 'Número de post' AFTER `num_messengers`;
+
+-- Recaudación que proviene de los usuarios
+ALTER TABLE `project` ADD COLUMN `amount_users` INT UNSIGNED DEFAULT 0 NOT NULL COMMENT 'Recaudación proveniente de los usuarios';
+
+-- Recaudación proveniente de la convocatoria
+ALTER TABLE `project` ADD COLUMN `amount_call` INT UNSIGNED DEFAULT 0 NOT NULL COMMENT 'Recaudación proveniente de la convocatoria';
+
+-- Convocatoria en la que está
+ALTER TABLE `project` ADD COLUMN `called` VARCHAR(50) COMMENT 'Convocatoria en la que está';
+
+-- Máximo dinero que puede conseguir un proyecto de la convocatoria
+ALTER TABLE `project` ADD COLUMN `maxproj` INT(5) 'Dinero que puede conseguir un proyecto de la convocatoria';
