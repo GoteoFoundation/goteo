f00df9be108d7975e0f8f3ee661bf076ca7b5695
Pablo Castellano <pablo@anche.no>
2014-08-28 10:43:06 +0200

    Revert "/mail/ ahora te redirige a amazon"
    This reverts commit 279d3dd781b0cfc812b00c92f0537a80a5d5b357.

--- controller/mail.php
+++ controller/mail.php
@@ -3,7 +3,8 @@
 namespace Goteo\Controller {
 
 	use Goteo\Core\Redirection,
-		Goteo\Library\Mail as Mailer;
+		Goteo\Core\Model,
+        Goteo\Core\View;
 
 	class Mail extends \Goteo\Core\Controller {
 
@@ -12,12 +13,33 @@ namespace Goteo\Controller {
 	     */
 		public function index ($token) {
 
-            if (!empty($token)) {
+            if (!empty($token) && !empty($_GET['email'])) {
                 $token = \mybase64_decode($token);
                 $parts = explode('¬', $token);
+                if(count($parts) > 2 && ($_GET['email'] == $parts[1] || $parts[1] == 'any' ) && !empty($parts[2])) {
 
-                $link = Mailer::getSinovesLink($parts[2]);
-                throw new Redirection($link);
+                    // cogemos el md5 del archivo a cargar del campo 'content' de la tabla 'mail'
+                    // montamos url (segun newsletter)  y hacemos get_content
+                    // pasamos el contenido a la vista
+
+                    // cogemos el contenido de la bbdd y lo pintamos aqui tal cual
+                    if ($query = Model::query('SELECT html FROM mail WHERE email = ? AND id = ?', array($parts[1], $parts[2]))) {
+                        $content = $query->fetchColumn();
+
+
+                        if ($parts[1] == 'any') {
+                            return new View ('view/email/newsletter.html.php', array('content'=>$content, 'baja' => ''));
+                        } else {
+                            $baja = SEC_URL . '/user/leave/?email=' . $parts[1];
+
+                            if (NODE_ID != \GOTEO_NODE && \file_exists('nodesys/'.NODE_ID.'/view/email/default.html.php')) {
+                                return new View ('nodesys/'.NODE_ID.'/view/email/default.html.php', array('content'=>$content, 'baja' => $baja));
+                            } else {
+                                return new View ('view/email/goteo.html.php', array('content'=>$content, 'baja' => $baja));
+                            }
+                        }
+                    }
+                }
             }
 
             throw new Redirection('/');
