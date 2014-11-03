229ea7b1b2d1452ae34d4e8c7de27f37c5b74f3b
Javier Carrillo <javicarrillo83@gmail.com>
2014-11-03 12:26:21 +0100

    Eliminamos número del button.

--- view/project/edit/steps.html.php
+++ view/project/edit/steps.html.php
@@ -14,12 +14,12 @@ $errors = $this['errors'] ?>
                     
                     <span class="step first-off off<?php if ($this['step'] === 'userProfile') echo ' active'; else echo ' activable'; ?>">
                         <button type="button" name="view-step-userProfile" value="userProfile"><?php echo Text::get('step-1'); ?>
-                        <strong class="number">1</strong></button>
+                        <strong class="number"></strong></button>
                     </span>
                     
                     <span class="step off-off off<?php if ($this['step'] === 'userPersonal') echo ' active'; else echo ' activable'; ?>">
                         <button type="button" name="view-step-userPersonal" value="userPersonal"><?php echo Text::get('step-2'); ?>
-                        <strong class="number">2</strong></button>
+                        <strong class="number"></strong></button>
                     </span>
                     
                     <fieldset style="display: inline">
@@ -28,34 +28,34 @@ $errors = $this['errors'] ?>
                         
                         <span class="step off-on<?php if ($this['step'] === 'overview') echo ' active'; else echo ' activable'; ?>">
                             <button type="button" name="view-step-overview" value="overview"><?php echo Text::get('step-3'); ?>
-                            <strong class="number">3</strong></button>                            
+                            <strong class="number"></strong></button>                            
                         </span>
 
                         <span class="step on-on<?php if ($this['step'] === 'images') echo ' active'; else echo ' activable'; ?>">
                             <button type="button" name="view-step-images" value="images"><?php echo Text::get('step-3b'); ?>
-                            <strong class="number">3'</strong></button>
+                            <strong class="number"></strong></button>
                         </span>
 
                         <span class="step on-on<?php if ($this['step'] === 'costs') echo ' active'; else echo ' activable'; ?>">
                             <button type="button" name="view-step-costs" value="costs"><?php echo Text::get('step-4'); ?>
-                            <strong class="number">4</strong></button>
+                            <strong class="number"></strong></button>
                         </span>
 
                         <span class="step on-on<?php if ($this['step'] === 'rewards') echo ' active'; else echo ' activable'; ?>">
                             <button type="button" name="view-step-rewards" value="rewards>"><?php echo Text::get('step-5'); ?>
-                            <strong class="number">5</strong></button>                            
+                            <strong class="number"></strong></button>                            
                         </span>
 
                         <span class="step on-off<?php if ($this['step'] === 'supports') echo ' active'; else echo ' activable'; ?>">
                             <button type="button" name="view-step-supports" value="supports"><?php echo Text::get('step-6'); ?>
-                            <strong class="number">6</strong></button>                            
+                            <strong class="number"></strong></button>                            
                         </span>
                         
                     </fieldset>
                     
                     <span class="step off-last off<?php if ($this['step'] === 'preview') echo ' active'; else echo ' activable'; ?>">
                         <button type="button" name="view-step-preview" value="preview"><?php echo Text::get('step-7'); ?>
-                        <strong class="number">7</strong></button>                        
+                        <strong class="number"></strong></button>                        
                     </span>
 
                 </div>
