e9a591e156c3c34850617bcd614e8800dd4a4869
Ivan Vergés <ivan@microstudi.net>
2014-11-12 12:21:47 +0100

    Configuracion nodo testnode

--- app/nodesys/testnode/config.php
+++ app/nodesys/testnode/config.php
@@ -1,13 +1,13 @@
 <?php
 // Configuraciones específicas para nodos
 // Metadata
-define('NODE_META_TITLE', 'TestNode');
-define('NODE_META_DESCRIPTION', utf8_encode('Red social de financiación colectiva'));
-define('NODE_META_KEYWORDS', utf8_encode('crowdfunding, procomún, commons, social, network, financiacion colectiva, cultural, creative commons, proyectos abiertos, open source, free software, licencias libres'));
+define('NODE_META_TITLE', 'TestNode en beta');
+define('NODE_META_DESCRIPTION', 'Red social de financiación colectiva');
+define('NODE_META_KEYWORDS', 'crowdfunding, procomún, commons, social, network, financiacion colectiva, cultural, creative commons, proyectos abiertos, open source, free software, licencias libres');
 define('NODE_META_AUTHOR', 'Fundación Fuentes Abiertas');
 define('NODE_META_COPYRIGHT', 'Platoniq');
 define('NODE_DEFAULT_LANG', 'es');
-define('NODE_URL', 'http://barcelona.goteo.org');
+define('NODE_URL', '//testnode.goteo.org');
 define('NODE_NAME', 'NodoTesteo');
 define('NODE_MAIL', 'goteo_testnode@doukeshi.org');
 define('NODE_ANALYTICS_TRACKER', '');
