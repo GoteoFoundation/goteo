cfb6bdbcc7e05b4124151ed8750db4d309551859
Ivan Vergés <ivan@microstudi.net>
2014-11-15 00:27:53 +0100

    archivos servidor

---
+++ var/php/router.php
@@ -0,0 +1,19 @@
+<?php
+//router.php
+// Default index file
+define("DIRECTORY_INDEX", "index.php");
+
+if (call_user_func(function() {
+    $f = $_SERVER['SCRIPT_FILENAME'];
+    $root = $_SERVER["DOCUMENT_ROOT"];
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+    // return if requested file exists
+    if ($f != __FILE__ && file_exists($root . $path)) {
+
+        return true;
+    }
+    $_SERVER['SCRIPT_NAME'] = "/" . DIRECTORY_INDEX; // fix SCRIPT_NAME that change when REQUEST_URI contains dot
+})) return false;
+
+require_once DIRECTORY_INDEX;
