74d4cf41bf4a9ef3d6a82555788da0360875adc0
Julian Canaves <julian.canaves@gmail.com>
2014-12-06 06:31:01 +0100

    Circulitos dinámicos según array de pasos

--- app/view/project/edit/steps.html.php
+++ app/view/project/edit/steps.html.php
@@ -2,7 +2,34 @@
 
 use Goteo\Library\Text;
 
-$errors = $this['errors'] ?>
+$errors = $this['errors'];
+$steps = $this['steps'];
+
+/**
+ *  Estilos
+ * ---------
+ *
+ * first: no tiene linea antes del círculo
+ * off: linea/circulo gris
+ * on: linea/circulo aqua
+ * last: no tiene linea después del circulo
+ *
+ * - Fieldset: contiene pasos en una linea roja, reza 'proyecto nuevo'.
+ */
+$line = array(
+    'userProfile' => ' first-off off',
+    'userPersonal' => ' off-off off',
+    'overview' => ' off-on',
+    'images' => ' on-on',
+    'costs' => ' on-on',
+    'rewards' => ' on-on',
+    'supports' => ' on-off',
+    'preview' => ' off-last off'
+);
+
+// URL
+$url = "/project/edit/{$this['id_project']}/";
+?>
 
 <div id="project-steps">
             
@@ -11,21 +38,40 @@ $errors = $this['errors'] ?>
                 <legend><h3><?php echo Text::get('form-navigation_bar-header'); ?></h3></legend>
 
                 <div class="steps">
-                    
-                    <a href="/project/edit/<?php echo $this['id_project']; ?>/userProfile">
+
+                    <?php foreach ($steps as $stepId => $stepData) {
+
+                        // fieldset start/end
+                        if ($stepId == 'overview') echo '<fieldset style="display: inline"><legend>'.Text::get('regular-new_project').'</legend>';
+                        if ($stepId == 'preview') echo '</fieldset>';
+
+                        // circulito
+                        $active = ($this['step'] == $stepId) ? ' active' : ' activable';
+
+                        echo '<a href="' . $url . $stepId . '" title="' . $stepData['title'] . '">
+                            <span class="step' . $line[$stepId] . $active . '">
+                                <button type="button" name="view-step-' . $stepId . '" value="' . $stepId . '">' . $stepData['name'] . '</button>
+                            </span>
+                        </a>
+                        ';
+
+                    } ?>
+
+                    <?php /*********************************************************************
+                    <a href="/project/edit/<?php echo $this['id_project']; ?>/userProfile" title="">
                     <span class="step first-off off<?php if ($this['step'] === 'userProfile') echo ' active'; else echo ' activable'; ?>">
                         <button type="button" name="view-step-userProfile" value="userProfile"><?php echo Text::get('step-1'); ?>
                         <strong class="number"></strong></button>
                     </span>
                     </a>
-                    
+
                     <a href="/project/edit/<?php echo $this['id_project']; ?>/userPersonal">
                     <span class="step off-off off<?php if ($this['step'] === 'userPersonal') echo ' active'; else echo ' activable'; ?>">
                         <button type="button" name="view-step-userPersonal" value="userPersonal"><?php echo Text::get('step-2'); ?>
                         <strong class="number"></strong></button>
                     </span>
                     </a>
-                    
+
                     <fieldset style="display: inline">
                         
                         <legend><?php echo Text::get('regular-new_project'); ?></legend>
@@ -73,6 +119,7 @@ $errors = $this['errors'] ?>
                         <strong class="number"></strong></button>                        
                     </span>
                     </a>
+                    *********************************************************************/ ?>
 
                 </div>
 
