59291193929912ab3ad786b07901b76ac6c06159
Julian Canaves <julian.canaves@gmail.com>
2014-12-09 11:10:46 +0100

    Co0nfiguración y primer test (que falla...)

---
+++ tests/Goteo/Library/Paypal.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Goteo\Library\Tests;
+
+use \Paypal\Service as PPService;
+
+class PaypalTest extends \PHPUnit_Framework_TestCase {
+
+    // Aunque no es obligatorio que exista este archivo de configuración para compilar grunt
+    /*
+    public function testConfig() {
+
+        $configFile = PP_CONFIG_PATH . 'sdk_config.ini';
+//        echo $configFile;
+//        var_dump(is_file($configFile));
+        $config = file_get_contents($configFile);
+//        var_dump($config);
+
+        $this->contains('acct1.UserName');
+    }
+    */
+
+    public function testInstance() {
+
+        $service  = new PPService\AdaptivePaymentsService;
+        var_dump($service);
+        die;
+
+        $this->assertInstanceOf('\Paypal\Service\AdaptivePaymentsService', $service);
+
+        return $service;
+    }
+
+}
