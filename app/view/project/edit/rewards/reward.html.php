4ef1d2cc1f8bdf7b2cbebe23a6fde94cbdb19259
Julian Canaves <julian.canaves@gmail.com>
2015-01-02 19:05:18 +0100

    Poner el importe de la recompensa delante del título (en el formulario, cuando está plegada) * solo para recompensas con importe

--- app/view/project/edit/rewards/reward.html.php
+++ app/view/project/edit/rewards/reward.html.php
@@ -8,7 +8,9 @@ $types = $this['data']['types'];
 
 <div class="reward <?php echo $reward->icon ?>">
     
-    <div class="title"><strong><?php echo "{$reward->amount} &euro;<br />".$types[$reward->icon]->name . ': ' . htmlspecialchars($reward->reward) ?></strong></div>
+    <div class="title"><strong><?php
+            if (!empty($reward->amount)) echo "{$reward->amount} &euro;<br />";
+            echo $types[$reward->icon]->name . ': ' . htmlspecialchars($reward->reward) ?></strong></div>
     
     <div class="description">
         <p><?php echo htmlspecialchars($reward->description) ?></p>
