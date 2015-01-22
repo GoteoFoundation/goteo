18ec07f749336703fb10a5bf758099cd8d35da5a
Ivan Vergés <ivan@microstudi.net>
2015-01-22 13:53:08 +0100

    superform changes (adding data-* html5 attributes)

--- src/Goteo/Library/SuperForm/view/superform/element/textarea.html.php
+++ src/Goteo/Library/SuperForm/view/superform/element/textarea.html.php
@@ -1,7 +1,15 @@
-<textarea <?php if ($this['cols'] > 0) echo ' cols="' . ((int) $this['cols']) . '"' ?><?php if ($this['rows'] > 0) echo ' rows="' . ((int) $this['rows']) . '"' ?> name="<?php echo htmlspecialchars($this['name']) ?>" id="<?php echo htmlspecialchars($this['name']) ?>_editor"<?php if (isset($this['class'])) echo ' class="' . htmlspecialchars($this['class']) . '"'?>><?php if (isset($this['value'])) echo $this['value'] ?></textarea>
-<?php
-/* ya no hace falta
-<script type="text/javascript">
-<?php include __DIR__ . '/textarea.js.src.php' ?>
-</script>
-*/
+<textarea name="<?php echo htmlspecialchars($this['name']) ?>" id="<?php echo htmlspecialchars($this['name']) ?>_editor" <?php
+
+if ($this['cols'] > 0) echo ' cols="' . ((int) $this['cols']) . '"';
+
+if ($this['rows'] > 0) echo ' rows="' . ((int) $this['rows']) . '"';
+
+if (isset($this['class'])) echo ' class="' . htmlspecialchars($this['class']) . '"';
+
+if($this['data'] && is_array($this['data'])) {
+    foreach($this['data'] as $key => $val) {
+        echo ' data-' . $key . '="' . htmlspecialchars($val) . '"';
+    }
+}
+
+?>><?php if (isset($this['value'])) echo $this['value'] ?></textarea>
--- src/Goteo/Library/SuperForm/view/superform/element/textbox.html.php
+++ src/Goteo/Library/SuperForm/view/superform/element/textbox.html.php
@@ -1,8 +1,17 @@
-<input id="<?php echo htmlspecialchars($this['id']) ?>" name="<?php echo htmlspecialchars($this['name']) ?>" type="text"<?php if (isset($this['class'])) echo ' class="' . htmlspecialchars($this['class']) . '"'?>  value="<?php if (isset($this['value'])) echo htmlspecialchars($this['value']) ?>"<?php if (isset($this['size'])) echo ' size="' . ((int) $this['size']) . '"' ?><?php if ($this['maxlength'] > 0) echo ' maxlength="' . ((int) $this['maxlength']) . '"' ?> />
+<input id="<?php echo htmlspecialchars($this['id']) ?>" name="<?php echo htmlspecialchars($this['name']) ?>" type="text" value="<?php if (isset($this['value'])) echo htmlspecialchars($this['value']) ?>"<?php
+
+if (isset($this['size'])) echo ' size="' . ((int) $this['size']) . '"';
+
+if ($this['maxlength'] > 0) echo ' maxlength="' . ((int) $this['maxlength']) . '"';
+
+if (isset($this['class'])) echo ' class="' . htmlspecialchars($this['class']) . '"';
+
+if($this['data'] && is_array($this['data'])) {
+    foreach($this['data'] as $key => $val) {
+        echo ' data-' . $key . '="' . htmlspecialchars($val) . '"';
+    }
+}
+
+ ?> />
 <?php
 if (isset($this['symbol'])) echo '<span class="symbol">'.$this['symbol'].'</span>';
-/* ya no hace falta
-<script type="text/javascript">
-<?php include __DIR__ . '/textbox.js.src.php' ?>
-</script>
-*/
