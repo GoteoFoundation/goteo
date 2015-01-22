12fff6ddd5773f8733349d356f51452b0007620c
Ivan Vergés <ivan@microstudi.net>
2015-01-22 10:34:55 +0100

    map allways shown in map.html view

--- app/view/widget/map.html.php
+++ app/view/widget/map.html.php
@@ -1,11 +1,7 @@
 <?php
 // vista para poner un mapa de google
 // El codigo javascript esta en js/geolocation.js
-// recibimos las coordenadas
-if ($this['latitude'] && $this['longitude']) :
 
 ?>
-<div id="map-canvas" class="geomap" data-map-latitude="<?php echo htmlspecialchars($this['latitude']); ?>" data-map-longitude="<?php echo htmlspecialchars($this['longitude']); ?>" data-map-content="<?php echo htmlspecialchars($this['name']); ?>" style="border: 2px solid grey; height: 500px;"></div>
-<?php
+<div id="map-canvas" class="geomap" data-map-latitude="<?php echo htmlspecialchars($this['latitude']); ?>"<?php if ($this['latitude'] && $this['longitude']) : ?> data-map-longitude="<?php echo htmlspecialchars($this['longitude']); ?>" data-map-content="<?php echo htmlspecialchars($this['name']); ?>" <?php endif; ?> style="border: 2px solid grey; height: 500px;"></div>
 
-endif;
