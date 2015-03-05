e1206b4e51a8bcb3c1f14769e375cb5f31850f9c
Ivan Vergés <ivan@microstudi.net>
2015-03-05 16:35:26 +0100

    sql changes

--- db/invest_node/structure.sql
+++ db/invest_node/structure.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS invest_node (
   `invest_node` varchar(50) NOT NULL COMMENT 'Nodo en el que se hace el aporte',
   UNIQUE KEY `invest` (`invest_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Aportes por usuario/nodo a proyecto/nodo';
+
+-- Optimizations:
+ALTER TABLE `invest_node`
+    ADD KEY `invest_id`(`invest_id`) ,
+    ADD KEY `invest_node`(`invest_node`) ,
+    ADD KEY `project_id`(`project_id`) ,
+    ADD KEY `project_node`(`project_node`) ,
+    ADD KEY `user_id`(`user_id`) ;
