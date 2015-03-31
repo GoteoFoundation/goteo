5350c6228886b7659ea9d851ad24df56fff4e7b8
Javier Carrillo <javicarrillo83@gmail.com>
2015-03-31 16:18:21 +0200

    Cambio meta author en nodos.

--- app/nodesys/andalucia/config.php
+++ app/nodesys/andalucia/config.php
@@ -4,7 +4,7 @@
 define('NODE_META_TITLE', 'Goteo Andalucía');
 define('NODE_META_DESCRIPTION', utf8_encode('Red social de financiación colectiva'));
 define('NODE_META_KEYWORDS', utf8_encode('crowdfunding, procomún, commons, social, network, financiacion colectiva, cultural, creative commons, proyectos abiertos, open source, free software, licencias libres'));
-define('NODE_META_AUTHOR', 'Fundación Fuentes Abiertas');
+define('NODE_META_AUTHOR', 'Fundación Goteo');
 define('NODE_META_COPYRIGHT', 'Platoniq');
 define('NODE_DEFAULT_LANG', 'es');
 define('NODE_URL', 'http://andalucia.goteo.org');
