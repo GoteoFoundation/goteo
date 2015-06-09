78481031f21a7a486fce097d572d07e25495a231
Javier Carrillo <javicarrillo83@gmail.com>
2015-06-09 17:41:44 +0200

    Obtenemos datos de convocatoria con get en lugar de getMini. Bug de configuración económica.

--- extend/goteo/src/Goteo/Controller/Admin/Calls.php
+++ extend/goteo/src/Goteo/Controller/Admin/Calls.php
@@ -27,7 +27,8 @@ namespace Goteo\Controller\Admin {
              * redirect
              *
              */
-            $call = Model\Call::getMini($id);
+            $call = Model\Call::get($id);
+
 
             // si es admin (no superadmin) si no la tiene asignada no puede hacer otra cosa que no sea listar.
             if ($action != 'list'
