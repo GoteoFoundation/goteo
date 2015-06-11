d9335f3e664d217d033055f89a732ff257e5cf29
Javier Carrillo <javicarrillo83@gmail.com>
2015-06-11 14:19:08 +0200

    Widget personalizado para channel.

--- templates/default/project/widget/horizontal_project.php
+++ templates/default/project/widget/horizontal_project.php
@@ -34,14 +34,14 @@ if ($project->status == 3 && $project->noinvest) {
 }
 ?>
 
-<div class="widget project activable<?php if (isset($this->balloon)) echo ' balloon' ?>">
+<div class="widget project horizontal">
 	<a href="<?php echo $url ?>/project/<?php echo $project->id ?>" class="expand"<?php echo $blank; ?>></a>
-    <?php if (isset($this->balloon)): ?>
-    <div class="balloon"><?php echo $this->balloon ?></div>
-    <?php endif ?>
+    
+    <h<?php echo $level ?> class="title horizontal"><a href="<?php echo $url ?>/project/<?php echo $project->id ?>"<?php echo $blank; ?>><?php echo htmlspecialchars($project->name,150) ?></a></h<?php echo $level ?>>
+    <h<?php echo $level + 1 ?> class="author"><?php echo $this->text('regular-by')?> <a href="<?php echo $url ?>/user/profile/<?php echo htmlspecialchars($project->user->id) ?>"<?php echo $blank; ?>><?php echo htmlspecialchars($this->text_recorta($project->user->name,37)) ?></a></h<?php echo $level + 1?>>
 
     <div class="image">
-        <?php switch ($project->tagmark) {
+        <?php /*switch ($project->tagmark) {
             case 'oneround': // "ronda única"
                 break;
             case 'onrun': // "en marcha"
@@ -62,14 +62,10 @@ if ($project->status == 3 && $project->noinvest) {
             case 'fail': // "caducado"
                 echo '<div class="tagmark grey">' . $this->text('regular-fail_mark') . '</div>';
                 break;
-        } ?>
-
-        <?php if (isset($this->investor)) : ?>
-            <div class="investor"><img src="<?php echo $investor->avatar->getLink(34, 34, 1) ?>" alt="<?php echo $investor->name ?>" /><div class="invest"><?php echo $this->text('proj-widget-my_invest'); ?><br /><span class="amount"><?php echo \amount_format($invest->total); ?></span></div></div>
-        <?php endif; ?>
+        }*/ ?>
 
         <?php if ($project->image instanceof Image): ?>
-        <a href="<?php echo $url ?>/project/<?php echo $project->id ?>"<?php echo $blank; ?>><img alt="<?php echo $project->id ?>" src="<?php echo $project->image->getLink(226, 130, true) ?>" /></a>
+        <a href="<?php echo $url ?>/project/<?php echo $project->id ?>"<?php echo $blank; ?>><img alt="<?php echo $project->id ?>" src="<?php echo $project->image->getLink(672, 250, true) ?>" /></a>
         <?php endif ?>
         <?php if (!empty($project->cat_names)): ?>
         <div class="categories">
@@ -80,27 +76,10 @@ if ($project->status == 3 && $project->noinvest) {
         <?php endif ?>
     </div>
 
-    <h<?php echo $level ?> class="title"><a href="<?php echo $url ?>/project/<?php echo $project->id ?>"<?php echo $blank; ?>><?php echo htmlspecialchars($this->text_recorta($project->name,50)) ?></a></h<?php echo $level ?>>
 
-    <h<?php echo $level + 1 ?> class="author"><?php echo $this->text('regular-by')?> <a href="<?php echo $url ?>/user/profile/<?php echo htmlspecialchars($project->user->id) ?>"<?php echo $blank; ?>><?php echo htmlspecialchars($this->text_recorta($project->user->name,37)) ?></a></h<?php echo $level + 1?>>
+    <div class="description"><?php echo $this->text_recorta($project->description, 300); ?></div>
 
-    <div class="description"><?php echo $this->text_recorta($project->description, 100); ?></div>
-
-    <?php echo View::get('project/meter_hor.html.php', array('project' => $project)) ?>
-
-    <div class="rewards">
-        <h<?php echo $level + 1 ?>><?php echo $this->text('project-rewards-header'); ?></h<?php echo $level + 1?>>
-
-        <ul>
-           <?php foreach ($project->rewards as $reward): ?>
-            <li class="<?php echo $reward->icon ?> activable">
-                <?php $link_param= ($reward->type == 'individual') ? "/invest?amount=".\amount_format($reward->amount, 0, true) : "/rewards#social-rewards"; ?>
-                <a href="<?php echo $url ?>/project/<?php echo $project->id.$link_param ?>" title="<?php echo htmlspecialchars("{$reward->icon_name}: {$reward->reward}"); if ($reward->type == 'individual') echo ' '.\amount_format($reward->amount); ?>" class="tipsy"<?php echo $blank; ?>><?php echo htmlspecialchars($reward->reward) ?></a>
-            </li>
-           <?php endforeach ?>
-        </ul>
-
-
+    <div class="meter-container">
+    <?php echo View::get('project/meter_hor_big.html.php', array('project' => $project)) ?>
     </div>
-
 </div>
