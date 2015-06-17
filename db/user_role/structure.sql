8a54e2b2d4b408c98dec2941c6bbdb91ccf524d2
Ivan Vergés <ivan@microstudi.net>
2015-06-17 20:28:53 +0200

    sql changes

--- db/user_role/structure.sql
+++ db/user_role/structure.sql
@@ -7,3 +7,15 @@ CREATE TABLE IF NOT EXISTS `user_role` (
   KEY `role_FK` (`role_id`),
   KEY `node_FK` (`node_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- roles for nodes
+ALTER TABLE `user_role`
+    ADD COLUMN `node_id` varchar(50)  COLLATE utf8_general_ci NOT NULL after `role_id` ,
+    CHANGE `datetime` `datetime` timestamp   NULL DEFAULT CURRENT_TIMESTAMP after `node_id` ,
+    ADD KEY `node_FK`(`node_id`) ,
+    ADD KEY `role_FK`(`role_id`) ,
+    DROP KEY `role_id` ,
+    ADD KEY `user_FK`(`user_id`) ,
+    DROP FOREIGN KEY `user_role_ibfk_1`  ,
+    DROP FOREIGN KEY `user_role_ibfk_2`  ;
