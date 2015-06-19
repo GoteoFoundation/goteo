5dbe07077fb77fcd8291c2dcf1885cc19b803708
Javier Carrillo <javicarrillo83@gmail.com>
2015-06-19 16:35:15 +0200

    Link de vuelta hacia home de channel. Link hacia páginas de categorias/tipos sin enlace ancla. Módulo de convocatorias en side funcionando ok.

--- templates/default/channel/partials/side/searcher.php
+++ templates/default/channel/partials/side/searcher.php
@@ -1,7 +1,7 @@
 <div class="side_widget">
     <?php foreach ($this->types as $type) : ?>
     <div class="line button rounded-corners<?php if ($type == $this->type) echo ' current' ?>">
-        <p><a href="<?= $URL.'/channel/'.$this->channel->id.'/'.$type.'#content' ?>" class="show_cat"><?= $this->text('node-side-searcher-'.$type) ?></a></p>
+        <p><a href="<?= $URL.'/channel/'.$this->channel->id.'/'.$type ?>" class="show_cat"><?= $this->text('node-side-searcher-'.$type) ?></a></p>
     </div>
     <?php endforeach; ?>
 
