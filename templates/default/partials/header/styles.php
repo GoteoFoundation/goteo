ae5d5053e38bfbdda725bb3a56ad66c4e0a58bb3
Ivan Vergés <ivan@microstudi.net>
2015-06-30 18:33:25 +0200

    ie 8

--- templates/default/partials/header/styles.php
+++ templates/default/partials/header/styles.php
@@ -5,3 +5,7 @@
     <!--[if IE]>
     <link href="<?= SRC_URL ?>/view/css/ie.css" media="screen" rel="stylesheet" type="text/css" />
     <![endif]-->
+
+    <!--[if lt IE 9]>
+    <script>alert('Your browser is outdated and will not work on this site! Please upgrade it!')</script>
+    <![endif]-->
