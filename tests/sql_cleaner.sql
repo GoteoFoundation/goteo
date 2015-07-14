c42c78ddb08b6dd5c8435d04517cd017bcb7d70e
Ivan Vergés <ivan@microstudi.net>
2015-07-14 14:47:55 +0200

    fix blog creation

---
+++ tests/sql_cleaner.sql
@@ -0,0 +1,11 @@
+DELETE FROM `node` WHERE id='testnode';
+DELETE FROM `project` WHERE node='testnode';
+DELETE FROM `user` WHERE node='testnode';
+DELETE FROM `user_personal` WHERE `user` NOT IN (SELECT id FROM `user`);
+DELETE FROM  `project_category` WHERE `project` NOT IN (SELECT id FROM project);
+DELETE FROM `post_lang` WHERE id NOT IN (SELECT id FROM post);
+DELETE FROM `post_lang` WHERE id NOT IN (SELECT id FROM post);
+DELETE FROM `user_web` WHERE `user` NOT IN (SELECT id FROM `user`);
+DELETE FROM  `project_account` WHERE `project` NOT IN (SELECT id FROM project);
+DELETE FROM `message` WHERE `user` NOT IN (SELECT id FROM `user`);
+DELETE FROM  `cost` WHERE `project` NOT IN (SELECT id FROM project);
