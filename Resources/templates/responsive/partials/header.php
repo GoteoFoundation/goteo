56a33bdc2f079fe8d477d4185912049faa1f2737
Ivan Vergés <ivan@microstudi.net>
2015-10-09 18:31:07 +0200

    fix en avatar header

--- Resources/templates/responsive/partials/header.php
+++ Resources/templates/responsive/partials/header.php
@@ -2,26 +2,26 @@
     <nav class="navbar main-color-background">
       <div class="container">
        <div class="pull-left">
-          <a href="/"><img src="<?= SRC_URL ?>/assets/img/logo.png" class="img-responsive logo" alt="Goteo"></a> 
+          <a href="/"><img src="<?= SRC_URL ?>/assets/img/logo.png" class="img-responsive logo" alt="Goteo"></a>
        </div>
-       <?php if (is_null($this->get_user())) : ?>
+       <?php if (!$this->get_user()) : ?>
       	<div class="navbar-right">
 	      <ul class="nav navbar-nav">
-	        
+
 	          <li>
 	            <a class="navbar-element" href="/signup">Registrarse</a>
 	          </li>
-	        
+
 	          <li>
 	            <a class="navbar-element" href="/login">Iniciar sesión</a>
 	          </li>
-	        
+
 	      </ul>
     	</div>
     	<?php else: ?>
     	<div class="pull-right user-container">
     		<a href="/dashboard"><span></span><img class="avatar-radius" src="<?= $this->get_user()->avatar->getLink(35, 35, true); ?>" /></a>
-    	<?php endif; ?>
+    	<?php endif ?>
       </div>
     </nav>
 
