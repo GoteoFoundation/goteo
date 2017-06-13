d55362063b7e7fd931943758b5a1f1e4332c4aa1
Ivan Vergés <ivan@microstudi.net>
2017-06-13 17:47:09 +0200

    Fixes TAB usability on create new project

--- Resources/templates/responsive/project/create.php
+++ Resources/templates/responsive/project/create.php
@@ -24,14 +24,14 @@ $terms=$this->terms;
             <div class="col-md-6 col-md-offset-3">
                 <div class="form-group col-sm-12 active" id="name-group">
                     <label><?= $this->text('overview-field-name') ?></label>
-                    <input class="form-control" name="name" value="" type="text" required>
+                    <input tabindex="0" class="form-control" name="name" value="" type="text" required>
                 </div>
                 <div class="form-group col-sm-12" id="subtitle-group">
                     <div class="alert alert-success">
                         <?= $this->text('project-create-subtitle-alert') ?>
                     </div>
                     <label><?= $this->text('overview-field-subtitle') ?></label>
-                    <input class="form-control" name="subtitle" value="" type="text" required>
+                    <input tabindex="0" class="form-control" name="subtitle" value="" type="text" required>
                 </div>
                 <div class="form-group col-sm-12" id="minimum-group">
                     <div class="alert alert-success">
@@ -45,7 +45,7 @@ $terms=$this->terms;
                     <div class="row">
                         <div class="col-sm-4">
                             <label><?= $this->text('project-create-minimum-field') ?></label>
-                            <input type="text" class="form-control minimum" name="minimum" id="minimum" value="" required>
+                            <input tabindex="0" type="text" class="form-control minimum" name="minimum" id="minimum" value="" required>
                         </div>
                         <div class="col-sm-4">
                             <button id="calculate-invest" type="submit" class="btn btn-block pink"><?= $this->text('project-create-minimum-calculate-button') ?></button>
@@ -58,9 +58,9 @@ $terms=$this->terms;
                         </div>
                     </div>
                 </div>
-                <div class="form-group col-sm-12" id="calendar-group">
+                <div tabindex="0" class="form-group col-sm-12" id="calendar-group" style="outline:none">
                     <label><?= $this->text('project-create-publishing-date-field') ?></label>
-                    <div id="calendar" >
+                    <div id="calendar">
                     </div>
                     <input type="hidden" name="publishing_date" id="publishing-date" value="" required>
                 </div>
@@ -71,19 +71,19 @@ $terms=$this->terms;
                     </div>
                     <label><?= $this->text('project-create-social-select-field') ?></label>
                     <div class="row">
-                        <div class="col-xs-6 col-sm-4 pay-method text-left-important">
+                        <div class="col-xs-6 col-sm-4 social-commitment-option text-left-important">
                             <label class="category" for="no-social">
-                                <input class="social-category" id="no-social" name="social" value="0" type="radio" required>
+                                <input tabindex="-1" class="social-category" id="no-social" name="social" value="0" type="radio" required>
                                 <img class="img-responsive img-method align-center-margin" alt="<?= $category->name ?>" title="none" src="/assets/img/project/create/none.png">
                                 <span class="method-text">
                                 <?= $this->text('project-create-no-social-commitment') ?>
                                 </span>
                             </label>
                         </div>
-                    <?php foreach($this->social_commitments as $key => $social): ?>
-                        <div class="col-xs-6 col-sm-4 pay-method text-left-important <?= ($key+1)%3==0 ? 'clear-both-md clear-both-sm' : '' ?> <?= ($key+1)%2==0 ? 'clear-both-xs' : '' ?>" >
+                        <?php foreach($this->social_commitments as $key => $social): ?>
+                        <div class="col-xs-6 col-sm-4 social-commitment-option text-left-important<?= ($key+1)%3==0 ? ' clear-both-md clear-both-sm' : '' ?><?= ($key+1)%2==0 ? ' clear-both-xs' : '' ?>">
                             <label class="category" for="<?= $key ?>-social">
-                                <input class="social-category" name="social" id="<?= $key ?>-social" value="<?= $social->id ?>" type="radio" required>
+                                <input tabindex="<?= $key == 0 ? '0' : '-1' ?>" class="social-category" name="social" id="<?= $key ?>-social" value="<?= $social->id ?>" type="radio" required>
                                 <img class="img-responsive img-method align-center-margin" alt="<?= $social->name ?>" title="<?= $social->description ?>" src="<?= $social->image->getLink(60, 60, false) ?>">
                                  <span class="method-text">
                                 <?= $social->name ?>
@@ -98,11 +98,11 @@ $terms=$this->terms;
                         <?= $this->text('project-create-social-text-alert') ?>
                     </div>
                     <label><?= $this->text('project-create-social-text-field') ?></label>
-                    <input class="form-control" name="social-description" value="" required>
+                    <input tabindex="0" class="form-control" name="social-description" value="" required>
                 </div>
                 <div class="form-group col-sm-12" id="accept-group" >
                     <label class="terms">
-                        <input class="" type="checkbox" name="create-accept" id="create-accept" value="1">
+                        <input tabindex="0" class="" type="checkbox" name="create-accept" id="create-accept" value="1">
                             <?= $this->text('project-create-terms-field') ?>
                     </label>
                 </div>
@@ -110,7 +110,7 @@ $terms=$this->terms;
         </div>
         <div class="row">
             <div class="col-md-3 col-md-offset-3 col-sm-6 spacer">
-                <button id="create-continue" disabled="disabled" type="submit" class="btn btn-block green"><?= $this->text('project-create-save-button') ?></button>
+                <button tabindex="0" id="create-continue" disabled="disabled" type="submit" class="btn btn-block green"><?= $this->text('project-create-save-button') ?></button>
             </div>
         </div>
         <div class="row" id="submit-alert">
@@ -164,6 +164,28 @@ $terms=$this->terms;
 
     $(function(){
 
+        $('#calendar').datetimepicker({
+            inline: true,
+            format: 'MM/DD/YYYY',
+            locale: '<?= $this->lang_current() ?>',
+            keyBinds: {
+                up: null,
+                down: function (widget) {
+                    if (!widget) {
+                        this.show();
+                        return;
+                    }
+                },
+                left: null,
+                right: null,
+                t : null,
+                'delete' : null
+            }
+            }).on('dp.change', function (e) {
+                _activate_calendar();
+                $('#publishing-date').val(e.date.format('YYYY/MM/DD'));
+        });
+
         var _get_ajax_invest_result = function() {
             var minimum=$("#minimum").val();
 
@@ -178,14 +200,21 @@ $terms=$this->terms;
 
         };
 
+        $('#project-form input[type="text"]:first').focus();
+
         $('#create-accept').change(function() {
-            var social_selected=$("input[name=social]:checked").val();
+            var social_selected = $('input[name="social"]:checked').val();
 
             //If none selected in social radio button, continue button is always disabled
             if(social_selected==0)
                 $('#create-continue').attr('disabled', true);
             else
                 $('#create-continue').attr('disabled', !this.checked);
+
+        });
+
+        $('#noSocialModal').on('hidden.bs.modal', function (e) {
+            $('input[name="social"]:first').focus();
 
         });
 
@@ -216,13 +245,72 @@ $terms=$this->terms;
 
         });
 
+        $("#calendar-group").on('keypress', function(e) {
+
+            var calendar = $('#calendar').data("DateTimePicker");
+            var first = $('#calendar').find(".datepicker-days tr td:first");
+            var last = $('#calendar').find(".datepicker-days tr td:last");
+            var new_date = null;
+            switch(e.keyCode) {
+                case 37:
+                    // left
+                    new_date = calendar.date().clone().subtract(1, 'd');
+                    break;
+                case 38:
+                    // up
+                    new_date = calendar.date().clone().subtract(7, 'd');
+                    break;
+                case 39:
+                    // right
+                    new_date = calendar.date().clone().add(1, 'd');
+                    break;
+                case 40:
+                    // Down
+                    new_date = calendar.date().clone().add(7, 'd');
+                    break;
+                case 10:
+                case 13:
+                case 9:
+                    // Goto next
+                    $(this).blur();
+                    $(this).next('div.form-group').click();
+
+                    break;
+            }
+            if(new_date) {
+                e.preventDefault();
+                calendar.date(new_date);
+                calendar.hide().show();
+            }
+        });
+
+        var _activate_calendar = function() {
+            if(!$('#calendar-group').hasClass('active')) {
+                $('#calendar-group').addClass('active');
+                $('#calendar-group').focus();
+            }
+
+        };
+        var _deactivate_calendar = function() {
+            if($('#calendar-group').hasClass('active')) {
+                $(".form-group").removeClass("active");
+            }
+        };
+
         $("#project-form").on('click', 'div.form-group', function(){
+
+            var item_id = $(this).attr('id');
+
+            if(item_id === 'calendar-group')
+                _activate_calendar();
+            else
+                _deactivate_calendar();
+
             $(".form-group").removeClass("active");
             $(this).addClass("active");
 
-            var item_id=$(this).attr('id');
-
-            $("#"+item_id+" input").focus();
+            if($("#"+item_id+' input:not([type="radio"])').length)
+                $("#"+item_id+" input:first").focus();
 
             $("#"+item_id+" .alert").fadeIn(1000);
 
@@ -232,28 +320,37 @@ $terms=$this->terms;
 
         });
 
-        $("#project-form").on('keypress', "div.form-group input", function (e) {
-            console.log(e.keyCode);
+        $("#project-form").on('keypress', 'div.form-group input', function (e) {
             if (e.keyCode == 10 || e.keyCode == 13 || e.keyCode == 9) {
                 e.preventDefault();
-                var item_id=$(this).closest("div.form-group").attr('id');
-                var next=$('#'+item_id).next("div.form-group");
-                var next_id=$(next).attr('id');
-                console.log('next is', next_id);
-                $('#'+next_id).click();
-
-                return false;
+                $(this).blur();
+                $(this).closest("div.form-group").next('div.form-group').click();
+            }
+            if($(this).is('[type="radio"]')) {
+                var el = null;
+                switch(e.keyCode) {
+                    case 37: // left
+                    case 38: // up
+                        el = $(this).closest('.social-commitment-option').prev('.social-commitment-option').find('input[type="radio"]');
+                        if(!el.length) {
+                            el = $('input[name="social"]:last');
+                        }
+                        break;
+                    case 39: // right
+                    case 40: // Down
+                        el = $(this).closest('.social-commitment-option').next('.social-commitment-option').find('input[type="radio"]');
+                        if(!el.length) {
+                            el = $('input[name="social"]:first');
+                        }
+                        break;
+                }
+                if(el) {
+                    e.preventDefault();
+                    el.focus();
+                }
             }
         });
 
-        $('#calendar').datetimepicker({
-            inline: true,
-            format: 'MM/DD/YYYY',
-            locale: '<?= $this->lang_current() ?>'
-            }).on('dp.change', function (e) {
-                $('#publishing-date').val(e.date.format('YYYY/MM/DD'));
-                $('#calendar-group').addClass("active");
-        });
 
         $("form").submit(function(e){
             var form = this;
