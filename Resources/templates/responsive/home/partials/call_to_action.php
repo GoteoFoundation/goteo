04d2ed94e6603810a1a82bf414e2f0a5cfee00bb
Javier Carrillo <javicarrillo83@gmail.com>
2017-08-10 10:28:08 +0200

    Texts changes.

--- Resources/templates/responsive/home/partials/call_to_action.php
+++ Resources/templates/responsive/home/partials/call_to_action.php
@@ -1,12 +1,12 @@
-<div class="section call-action">
+<div class="section call-action" style="background-color: #F04C27">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-7">
 				<div class="title">
-				Lorem ipsum dolor
+				Por unos alquileres justos
 				</div>
 				<div class="description">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					Sindicat de Llogaters i Llogateres
 				</div>
 			</div>
 			<div class="col-md-3 col-md-offset-1 col-button">
