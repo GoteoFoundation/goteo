00dd6d1cd452451e3f10de6d793f1798ba45172a
Ivan Vergés <ivan@microstudi.net>
2017-09-19 16:46:26 +0200

    pool integrated into dashboard

--- Resources/templates/responsive/pool/share.php
+++ Resources/templates/responsive/pool/share.php
@@ -1,20 +1,20 @@
 <?php
 
-$this->layout('layout', [
-    'bodyClass' => '',
+$this->layout('dashboard/layout', [
+    'bodyClass' => 'dashboard',
     'title' => $this->text('meta-title-pool-share'),
     'meta_description' => $this->text('meta-description-discover')
     ]);
 
-$this->section('content');
+$this->section('dashboard-content');
 
 ?>
 
 <?= $this->insert('pool/partials/steps_bar') ?>
 
-<div class="container">
+<div class="dashboard-content cyan">
 
-	<div class="row row-form">
+	<div class="row">
 			<div class="panel panel-default invest-container">
 				<div class="panel-body">
 					<h2 class="col-sm-offset-1 padding-bottom-2"><?= $this->text('pool-share-title') ?></h2>
@@ -42,7 +42,7 @@ $this->section('content');
                     <div class="row">
 
 						<h3 class="col-md-offset-1 col-md-10 clearfix padding-bottom-6"><?= $this->text('pool-invest-spread-header') ?></h3>
-					
+
 					</div>
 					<div class="row">
 						<div class="col-sm-5 col-md-offset-1 margin-2">
@@ -55,20 +55,20 @@ $this->section('content');
 				    			<i class="fa fa-twitter"></i> <?= $this->text('spread-twitter') ?>
 				  			</a>
 				  		</div>
-				  		
+
 			  		</div>
 
 					<hr class="share hidden-xs">
 
 					<div class="row">
 
-						
+
 
 				  	</div>
 				</div>
 			</div>
 	</div>
-	
+
 </div>
 
 <?php $this->replace() ?>
