4dfe1ce0de801ab899bfe3e99e0008a94bb5ffa4
Ivan Vergés <ivan@microstudi.net>
2017-09-22 20:50:47 +0200

    fixed sidebar behaviour

--- Resources/templates/responsive/partials/sidebar.php
+++ Resources/templates/responsive/partials/sidebar.php
@@ -1,28 +1,30 @@
 <?php if($this->sidebarMenu || $this->sidebarBottom): ?>
   <nav id="sidebar-menu">
-    <button class="toggle-sidebar btn btn-link visible-xs" title="Close"><i class="fa fa-close"></i></button>
+    <div class="sidebar-wrap">
+        <!-- <button class="toggle-sidebar btn btn-link" title="Close"><i class="fa fa-close"></i></button> -->
 
-    <div class="sidebar-header">
-      <?= $this->supply('sidebar-header') ?>
+        <div class="sidebar-header">
+          <?= $this->supply('sidebar-header') ?>
+        </div>
+
+        <ul class="nav sidebar-nav">
+          <?php foreach($this->raw('sidebarMenu') as $link => $item): ?>
+            <?= $this->insert('partials/header/menu_item', ['link' => $link, 'item' => $item, 'active' => $this->zone]) ?>
+          <?php endforeach ?>
+        </ul>
+
+      <?php if($this->sidebarBottom): ?>
+        <ul class="nav sidebar-nav bottom">
+          <?php foreach($this->raw('sidebarBottom') as $link => $item): ?>
+            <?= $this->insert('partials/header/menu_item', ['link' => $link, 'item' => $item, 'active' => $this->zone]) ?>
+          <?php endforeach ?>
+        </ul>
+      <?php endif ?>
     </div>
-
-    <ul class="nav sidebar-nav">
-      <?php foreach($this->raw('sidebarMenu') as $link => $item): ?>
-        <?= $this->insert('partials/header/menu_item', ['link' => $link, 'item' => $item, 'active' => $this->zone]) ?>
-      <?php endforeach ?>
-    </ul>
-
-  <?php if($this->sidebarBottom): ?>
-    <ul class="nav sidebar-nav bottom">
-      <?php foreach($this->raw('sidebarBottom') as $link => $item): ?>
-        <?= $this->insert('partials/header/menu_item', ['link' => $link, 'item' => $item, 'active' => $this->zone]) ?>
-      <?php endforeach ?>
-    </ul>
-  <?php endif ?>
-
   </nav>
 
   <div id="sidebar-menu-toggle" class="toggle-sidebar visible-xs" title="Open menu">
     <i class="fa fa-angle-double-right"></i>
+    <i class="fa fa-angle-double-left"></i>
   </div>
 <?php endif ?>
