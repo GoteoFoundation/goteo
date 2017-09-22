99fa47f1beaca26b71c3ccefaec6b5d788e48377
Javier <javicarrillo83@gmail.com>
2017-09-22 17:26:17 +0200

    Search function in home.

--- Resources/templates/responsive/home/layout.php
+++ Resources/templates/responsive/home/layout.php
@@ -154,8 +154,19 @@ $this->section('content');
             ]
           });*/
 
-
-
+          $('a[href="#search"]').on('click', function(event) {
+              event.preventDefault();
+              $('#search').addClass('open');
+              $('#search > form > input[type="search"]').focus();
+          });
+          
+          $('#search, #search button.close').on('click keyup', function(event) {
+              if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+                  $(this).removeClass('open');
+              }
+          });
+          
+          
         });
     </script>
 
