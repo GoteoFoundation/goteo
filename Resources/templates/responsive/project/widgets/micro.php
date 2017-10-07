750c524f0aea2286c25c879224efcb07fb23ed1a
Ivan Vergés <ivan@microstudi.net>
2017-10-07 13:12:31 +0200

    impersonate user on project sidebar for admins

--- Resources/templates/responsive/project/widgets/micro.php
+++ Resources/templates/responsive/project/widgets/micro.php
@@ -5,6 +5,12 @@
     <div class="content">
         <?php if($this->admin): ?>
             <span class="label label-danger"><?= $this->project->getTextStatus() ?></span>
+            <?php if($this->is_admin()): ?>
+                <form method="post" action="/admin/users/impersonate/<?= $this->project->owner ?>" class="pull-right">
+                    <input type="hidden" name="id" value="<?= $this->project->owner ?>">
+                <button type="submit" class="btn btn-xs btn-orange" title="<?= $this->text('admin-impersonate-user', $this->project->getOwner()->name) ?>"><i class="fa fa-user-md"></i></button>
+                </form>
+            <?php endif ?>
         <?php endif ?>
         <div class="title"><a href="/project/<?= $this->project->id ?>"><?= $this->text_truncate($this->project->name, 80); ?></a></div>
     </div>
