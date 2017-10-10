de0c6326d57b3869f0a7a61bffc4a4f744127612
Ivan Vergés <ivan@microstudi.net>
2017-10-10 12:23:32 +0200

    Copies

--- Resources/templates/responsive/dashboard/project/overview.php
+++ Resources/templates/responsive/dashboard/project/overview.php
@@ -8,7 +8,7 @@
     <h1><?= $this->text('overview-main-header') ?></h1>
     <div class="auto-hide">
         <div class="inner"><?= $this->text('guide-project-description') ?></div>
-        <div class="more"><i class="fa fa-info-circle"></i> <?= $this->text('regular-more_info') ?></div>
+        <div class="more"><i class="fa fa-info-circle"></i> <?= $this->text('regular-help') ?></div>
     </div>
 
     <?= $this->supply('dashboard-content-form', function() {return $this->form_form($this->raw('form'));}) ?>
