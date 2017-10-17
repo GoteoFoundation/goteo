b16af2b85e7234ef24863080076c4685f5e29671
Ivan Vergés <ivan@microstudi.net>
2017-10-17 23:14:25 +0200

    Vendor css minification

--- public/templates/responsive/partials/header/styles.php
+++ public/templates/responsive/partials/header/styles.php
@@ -1,11 +1,11 @@
 <!-- Bootstrap core CSS -->
+<!-- build:css assets/css/vendor.css -->
 <link href="<?= SRC_URL ?>/assets/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?= SRC_URL ?>/assets/vendor/bootstrap-xxs/bootstrap-xxs.min.css" rel="stylesheet">
 <link href="<?= SRC_URL ?>/assets/vendor/bootstrap-social/bootstrap-social.css" rel="stylesheet">
 <link href="<?= SRC_URL ?>/assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-<link href="<?= SRC_URL ?>/assets/vendor/footable/compiled/footable.bootstrap.min.css" rel="stylesheet">
-<link href="<?= SRC_URL ?>/assets/vendor/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 <link href="<?= SRC_URL ?>/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+<!-- endbuild -->
 
 <!-- POST PROCESSING THIS CSS BY GRUNT -->
 
