5708383ec12f764503743b97a8224437fb78938b
Ivan Vergés <ivan@microstudi.net>
2017-10-17 21:25:25 +0200

    Dasboard javascript minification

--- Resources/templates/responsive/dashboard/layout.php
+++ Resources/templates/responsive/dashboard/layout.php
@@ -39,15 +39,6 @@ $this->section('content');
 
 <?php $this->section('footer') ?>
 
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/vendor/Sortable/Sortable.min.js"></script>
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/vendor/dropzone/dist/min/dropzone.min.js"></script>
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/js/forms.js"></script>
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/js/dashboard/ajax-utils.js"></script>
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/vendor/simplemde/dist/simplemde.min.js"></script>
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
-    <script type="text/javascript" src="<?= SRC_URL ?>/assets/vendor/typeahead.js/dist/typeahead.bundle.min.js"></script>
-    <script type="text/javascript">
-        // Disable dropzone auto discover
-        Dropzone.autoDiscover = false;
-    </script>
+    <?= $this->insert('dashboard/partials/javascript') ?>
+
 <?php $this->append() ?>
