ad33c6541bf86948812175648b777d1388f22621
Ivan Vergés <ivan@microstudi.net>
2017-10-23 12:50:06 +0200

    Non renew sessions (experimental)

--- src/Goteo/Application/Session.php
+++ src/Goteo/Application/Session.php
@@ -111,7 +111,7 @@ class Session {
                 self::getSession()->start();
             }
             // Fixes session cookie time life
-            self::getSession()->migrate(false, self::getSessionExpires());
+            // self::getSession()->migrate(false, self::getSessionExpires());
         } catch(\RuntimeException $e) {
             throw new Config\ConfigException($e->getMessage());
         }
@@ -124,14 +124,14 @@ class Session {
         if($session_time) {
             self::setSessionExpires($session_time);
         }
-        if( self::getStartTime() > self::get('init_time') + self::getSessionExpires() ) {
-            // expires session
-            self::destroy(false);
-            $callback = self::$triggers['session_expires'];
-            if(is_callable($callback)) {
-                $callback();
-            }
-        }
+        // if( self::getStartTime() > self::get('init_time') + self::getSessionExpires() ) {
+        //     // expires session
+        //     self::destroy(false);
+        //     $callback = self::$triggers['session_expires'];
+        //     if(is_callable($callback)) {
+        //         $callback();
+        //     }
+        // }
     }
 
     static public function getId() {
