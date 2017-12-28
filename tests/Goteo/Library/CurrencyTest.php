1646b373fc69a8ccb0c81df3e6636b8cc5fa9a90
Ivan Vergés <ivan@microstudi.net>
2017-12-28 12:06:52 +0100

    currency test fix

--- tests/Goteo/Library/CurrencyTest.php
+++ tests/Goteo/Library/CurrencyTest.php
@@ -4,6 +4,7 @@
 namespace Goteo\Library\Tests;
 
 use Goteo\Library\Currency;
+use Goteo\Application\Session;
 
 class CurrencyTest extends \PHPUnit_Framework_TestCase {
 
@@ -16,75 +17,39 @@ class CurrencyTest extends \PHPUnit_Framework_TestCase {
         return $cur;
     }
 
-    /**
-     * Test the currency constant setting
-     */
-    public function testSetCurrency() {
-
-        // testing currency
-        $ccy0 = 'eur';
-        $ccy1 = 'usd';
-        $ccy2 = 'gbp';
-
-        // set default
-        $res_default = Currency::set();
-        $this->assertEquals($res_default, Currency::DEFAULT_CURRENCY); // EUR
-
-
-        // set from force
-        $res_force = Currency::set($ccy1); // USD
-        $this->assertEquals($res_force, strtoupper($ccy1));
-
-        // set from COOKIE
-        $_COOKIE['currency'] = $ccy2;
-        $res_cookie = Currency::set();
-        $this->assertEquals($res_cookie, strtoupper($ccy2));
-
-        // set from SESSION
-        $_SESSION['currency'] = $ccy0;
-        $res_session = Currency::set();
-        $this->assertEquals($res_session, strtoupper($ccy0));
-
-        // set from GET
-        $_GET['currency'] = $ccy1;
-        $res_get = Currency::set();
-        $this->assertEquals($res_get, strtoupper($ccy1));
-
-        return true;
-    }
-
-
     public function testAmountFormatCurrency() {
 
         $amount = 100000;
 
         // to euro
-        $_SESSION['currency'] = 'eur';
+        Session::store('currency', 'EUR');
         $format = Currency::amount_format($amount);
         // echo $format."\n";
         // format must have . for miliar
-        $this->assertRegExp('/\d?.{1}\d?/', $format, $format);
-
+        $this->assertRegExp('/\d?\.{1}\d?/', $format, $format);
+        $this->assertContains('€', $format);
 
         // to dollar
-        $_SESSION['currency'] = 'usd';
+        Session::store('currency', 'USD');
         $format = Currency::amount_format($amount);
         // echo $format."\n";
         // format must have , for miliar
         $this->assertRegExp('/\d?\,{1}\d?/', $format, $format);
+        $this->assertContains('$', $format);
 
         // to pound
-        $_SESSION['currency'] = 'gbp';
+        Session::store('currency', 'GBP');
         $format = Currency::amount_format($amount);
-        // echo $format."\n";
+        echo $format."\n";
         // format must have , for miliar
         $this->assertRegExp('/\d?\,{1}\d?/', $format, $format);
+        $this->assertContains('£', $format);
 
         return true;
     }
 
     public function testRate() {
-        $_SESSION['currency'] = 'usd';
+        Session::store('currency', 'USD');
         $rate = Currency::rate();
 
         $this->assertNotNull($rate);
