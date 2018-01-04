473a1296bb803e943bf97a986d51b20692ac1e12
Ivan Vergés <ivan@microstudi.net>
2018-01-04 13:40:00 +0100

    matcher basic widget

--- Resources/templates/responsive/matcher/widgets/normal.php
+++ Resources/templates/responsive/matcher/widgets/normal.php
@@ -1,3 +1,35 @@
 <?php
 
-var_dump($this->matcher);
+$link = $this->link ? $this->link : '/matcher/' . $this->matcher->id;
+
+?>
+
+<div class="flip-widget call-widget normal active" id="call-<?= $this->matcher->id ?>">
+    <div class="status">
+        <?= $this->text('call-tagmark-active') ?>
+    </div>
+
+    <div class="sphere">
+        <div class="text-center name"><?= $this->matcher->name ?></div>
+    </div>
+
+    <a class="floating flip" href="#backflip-<?= $this->matcher->id ?>" title="<?= $this->text('regular-more_info') ?>">+</a>
+
+    <div class="content">
+        <div class="amount-label">
+            Presupuesto de riego
+        </div>
+        <div class="amount">
+           <?= amount_format($this->matcher->amount) ?>
+        </div>
+        <div class="bottom" >
+            <div class="pull-left">
+                <img src="<?= $this->matcher->getOwner()->avatar->getLink(70) ?>">
+            </div>
+        </div>
+    </div>
+
+<?= $this->insert('matcher/widgets/partials/backside_normal') ?>
+
+</div>
+
