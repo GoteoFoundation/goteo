0b8cafabc96681b01b553294be4c6f4a3dbea44c
Ivan Vergés <ivan@microstudi.net>
2018-01-09 16:23:16 +0100

    loading container classes

--- Resources/templates/responsive/home/partials/projects.php
+++ Resources/templates/responsive/home/partials/projects.php
@@ -1,21 +1,23 @@
 <div class="section projects auto-update-projects" >
-    <h2 class="title text-center">
-        <?= $this->text('home-projects-title') ?>
-    </h2>
-    <ul class="filters list-inline center-block text-center">
-        <li data-status="promoted" class="active">
-            <?= $this->text('home-projects-team-favourites') ?>
-        </li>
-        <li data-status="outdated">
-            <?= $this->text('home-projects-outdate') ?>
-        </li>
-        <li data-status="near">
-            <?= $this->text('home-projects-near') ?>
-        </li>
-        <li data-status="matchfunding" class="matchfunding">
-            <?= $this->text('home-projects-matchfunding') ?>
-        </li>
-    </ul>
+    <div id="projects-container-title" class="container">
+        <h2 class="title text-center">
+            <?= $this->text('home-projects-title') ?>
+        </h2>
+        <ul class="filters list-inline center-block text-center">
+            <li data-status="promoted" class="active">
+                <?= $this->text('home-projects-team-favourites') ?>
+            </li>
+            <li data-status="outdated">
+                <?= $this->text('home-projects-outdate') ?>
+            </li>
+            <li data-status="near">
+                <?= $this->text('home-projects-near') ?>
+            </li>
+            <li data-status="matchfunding" class="matchfunding">
+                <?= $this->text('home-projects-matchfunding') ?>
+            </li>
+        </ul>
+    </div>
     <div class="container" id="projects-container">
         <?php if($this->projects): ?>
             <?= $this->insert('home/partials/projects_list', [
