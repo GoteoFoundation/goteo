592d93437dc6e310e7aef17aebd64148d808c9ee
Ivan Vergés <ivan@microstudi.net>
2018-02-12 16:46:00 +0100

    Admin wip scafolding

---
+++ Resources/templates/responsive/admin/index.php
@@ -0,0 +1,13 @@
+<?php $this->layout('admin/layout') ?>
+
+
+<?php $this->section('admin-content') ?>
+
+<div class="admin-content">
+  <div class="inner-container">
+        <h2><?= $this->text('admin-home-title') ?></h2>
+    </div>
+</div>
+
+<?php $this->replace() ?>
+
