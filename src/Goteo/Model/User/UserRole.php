17ac8d9a2a294b662be166a4f7edc8e0d3530189
Ivan Vergés <ivan@microstudi.net>
2018-02-15 17:50:20 +0100

    Add userrole tests

--- src/Goteo/Model/User/UserRole.php
+++ src/Goteo/Model/User/UserRole.php
@@ -11,6 +11,8 @@
 namespace Goteo\Model\User;
 
 use Goteo\Application\Role;
+use Goteo\Application\Exception\RoleException;
+use Goteo\Model\User;
 
 /**
  * Class UserRole
@@ -19,20 +21,85 @@ use Goteo\Application\Role;
  * Class for handeling user permissions
  *
  */
-class UserRole extends \Goteo\Core\Model
+class UserRole extends \ArrayObject
 {
+    public $user_id;
+
     protected $permissions = array();
-    protected $role_id;
 
     public function __construct() {
+        $args = func_get_args();
+        call_user_func_array(array('parent', '__construct'), $args);
+        if(!$this->hasRole('user')) {
+            $this->addRole('user');
+        }
     }
 
-    public function get($role_id) {
+    /**
+     * Gets all roles for a user
+     * @param  User   $user [description]
+     * @return [type]       [description]
+     */
+    public static function getRolesForUser($user_id) {
+        if($user_id instanceOf User) $user_id = $user_id->id;
 
+        $sql = "SELECT * FROM user_role WHERE user_id = ?";
+
+        $roles = [];
+        if($query = User::query($sql, $user_id)) {
+            if($result = $query->fetchAll(\PDO::FETCH_OBJ)) {
+                foreach($result as $ob) {
+                    if(Role::roleExists($ob->role_id)) $roles[$ob->role_id] = Role::getRolePerms($ob->role_id);
+                }
+            }
+        }
+        return new UserRole($roles);
+    }
+
+    /**
+     * Adds a role to the user
+     */
+    public function addRole($role) {
+        if(Role::roleExists($role)) {
+            $this->offsetSet($role, Role::getRolePerms($role));
+        }
+        return $this;
+    }
+
+    /**
+     * Removes a role from the user
+     * @param  [type] $role [description]
+     * @return [type]       [description]
+     */
+    public function removeRole($role) {
+        if($this->hasRole($role)) {
+            if($role === 'user') throw new RoleException("Role [user] cannot be removed");
+
+            $this->offsetUnset($role);
+        }
+        return $this;
+    }
+
+    /**
+     * Persists the current object to the database
+     */
+    public function save(&$errors = []) {
+        // TODO
+    }
+
+    /**
+     * Check if some role is set
+     * @return boolean [description]
+     */
+    public function hasRole($role) {
+        return $this->offsetExists($role);
     }
 
     // check if a permission is set
-    public function hasPerm($permission) {
-        return isset($this->permissions[$permission]);
+    public function hasPerm($perm) {
+        foreach($this as $role => $permissions) {
+            if(in_array($perm, $permissions)) return true;
+        }
+        return false;
     }
 }
