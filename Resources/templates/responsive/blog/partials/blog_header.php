374690121a654755ce43337e9c7af31c5e9c3727
Javier <javicarrillo83@gmail.com>
2018-03-06 16:56:58 +0100

    Header default

--- Resources/templates/responsive/blog/partials/blog_header.php
+++ Resources/templates/responsive/blog/partials/blog_header.php
@@ -2,12 +2,19 @@
 
 <div class="section post-header">
 	<div class="image">
-		<?php if($this->post->image): ?>
-			<img src="<?= $this->post->image->getLink(1920, 600, true) ?>" class="display-none-important img-responsive  hidden-xs visible-up-1400">
-			<img src="<?= $this->post->image->getLink(1400, 550, true) ?>" class="display-none-important img-responsive  hidden-xs visible-1051-1400">
-			<img src="<?= $this->post->image->getLink(1051, 550, true) ?>" class="display-none-important img-responsive  hidden-xs visible-768-1050">
-			<img src="<?= $this->post->image->getLink(750, 460, true) ?>" class="img-responsive visible-xs">
+		<?php if($this->post->header_image): ?>
+			<img src="<?= $this->post->header_image->getLink(1920, 600, true) ?>" class="display-none-important img-responsive  hidden-xs visible-up-1400">
+			<img src="<?= $this->post->header_image->getLink(1400, 600, true) ?>" class="display-none-important img-responsive  hidden-xs visible-1051-1400">
+			<img src="<?= $this->post->header_image->getLink(1051, 600, true) ?>" class="display-none-important img-responsive  hidden-xs visible-768-1050">
+			<img src="<?= $this->post->header_image->getLink(750, 450, true) ?>" class="img-responsive visible-xs">
+
+		<?php else: ?>
+			<img src="/assets/img/blog/header_default.jpg" width="1920" class="display-none-important header-default img-responsive  hidden-xs visible-up-1400">
+			<img src="/assets/img/blog/header_default.jpg" width="1400" class="display-none-important header-default img-responsive  hidden-xs visible-1051-1400">
+			<img src="/assets/img/blog/header_default.jpg" width="1051" class="display-none-important header-default img-responsive  hidden-xs visible-768-1050">
+			<img src="/assets/img/blog/header_default.jpg" width="750"  class="img-responsive header-default visible-xs">
 		<?php endif; ?>
+
 	</div>
 	<div class="info">
 		<div class="container">
