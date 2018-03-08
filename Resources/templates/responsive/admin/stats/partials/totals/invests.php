3f7a4a02093df033887b5fc8c0154cd25809507c
Ivan Vergés <ivan@microstudi.net>
2018-03-08 17:55:53 +0100

    layout fixes

--- Resources/templates/responsive/admin/stats/partials/totals/invests.php
+++ Resources/templates/responsive/admin/stats/partials/totals/invests.php
@@ -1,4 +1,4 @@
-<div class=" -d3-chart discrete-values" data-source="/api/charts/totals/invests?<?= $this->query ?>" data-title="<?= $this->text('admin-invests-stats') ?>" data-interval="15">
+<div class=" -d3-chart discrete-values" data-source="/api/charts/totals/invests?<?= $this->query ?>" data-interval="15">
     <ul class="row list-unstyled">
         <li class="col-xs-4 col-sm-2" data-property="created.today" data-title="<?= $this->text('admin-invests-created-today') ?>"></li>
     </ul>
