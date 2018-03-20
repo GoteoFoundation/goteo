d1711119cfaf7427ba4f866444f446e2cad607c3
Ivan Vergés <ivan@microstudi.net>
2018-03-20 14:14:32 +0100

    Add amount stats

--- src/Routes/api_charts_routes.php
+++ src/Routes/api_charts_routes.php
@@ -48,7 +48,7 @@ $api->add('api-chart-project-device', new Route(
 // any project/call origins stats
 $api->add('api-charts-referers', new Route(
     '/charts/referers/{type}',
-    array('_controller' => 'Goteo\Controller\Api\ChartsApiController::originStatsAction',
+    array('_controller' => 'Goteo\Controller\Api\AdminChartsApiController::originStatsAction',
         'id' => null,
         'type' => 'project',
         'group' => 'referer'
@@ -58,7 +58,7 @@ $api->add('api-charts-referers', new Route(
 // any project/call origins stats (origin)
 $api->add('api-chart-devices', new Route(
     '/charts/devices/{type}',
-    array('_controller' => 'Goteo\Controller\Api\ChartsApiController::originStatsAction',
+    array('_controller' => 'Goteo\Controller\Api\AdminChartsApiController::originStatsAction',
         'type' => 'project',
         'group' => 'ua'
         )
@@ -67,7 +67,7 @@ $api->add('api-chart-devices', new Route(
 // aggregate time metric series
 $api->add('api-chart-aggregates', new Route(
     '/charts/aggregates/{type}',
-    array('_controller' => 'Goteo\Controller\Api\ChartsApiController::aggregatesAction',
+    array('_controller' => 'Goteo\Controller\Api\AdminChartsApiController::aggregatesAction',
         'type' => 'invests'
         )
 ));
@@ -75,7 +75,7 @@ $api->add('api-chart-aggregates', new Route(
 // Totals for invests (today, yesterday, this week, etc)
 $api->add('api-chart-totals-invests', new Route(
     '/charts/totals/invests/{target}/{method}',
-    array('_controller' => 'Goteo\Controller\Api\ChartsApiController::totalInvestsAction',
+    array('_controller' => 'Goteo\Controller\Api\AdminChartsApiController::totalInvestsAction',
           'target' => '',
           'method' => 'global'
         )
@@ -83,7 +83,7 @@ $api->add('api-chart-totals-invests', new Route(
 // Totals for projects
 $api->add('api-chart-totals-projects', new Route(
     '/charts/totals/projects/{part}',
-    array('_controller' => 'Goteo\Controller\Api\ChartsApiController::totalProjectsAction',
+    array('_controller' => 'Goteo\Controller\Api\AdminChartsApiController::totalProjectsAction',
           'part' => null
         )
 ));
