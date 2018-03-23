077bcbc897c63cb469fd18010e9a55d88c6674f1
Javier Carrillo <javicarrillo83@gmail.com>
2018-03-23 18:47:12 +0100

    Team module.

---
+++ public/templates/responsive/blog/partials/javascript.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="<?= SRC_URL ?>/assets/vendor/typeahead.js/dist/typeahead.bundle.min.js"></script>
+<script src="<?= SRC_URL ?>/assets/vendor/slick-carousel/slick/slick.min.js"></script>
+<script type="text/javascript" src="/assets/vendor/selection-sharer/selection-sharer.js" ></script>
+
+
+
