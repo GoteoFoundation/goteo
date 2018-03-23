40f3c3e97a5171cb7e67ab81ed48048034ab294a
Javier Carrillo <javicarrillo83@gmail.com>
2018-03-23 18:47:12 +0100

    Team module.

--- Resources/templates/responsive/blog/partials/related_posts.php
+++ Resources/templates/responsive/blog/partials/related_posts.php
@@ -4,6 +4,7 @@
 			<div class="row">
 				<?php foreach($this->related_posts as $related_post): ?>
 					<?php $image= $related_post->header_image ? $related_post->header_image : $related_post->image; ?>
+                    <?php $related_author=$related_post->getAuthor(); ?>
 
 					<div class="col-md-4 related-post">
                         <a class="img-link" href="<?= '/blog/'.$related_post->id ?>">
@@ -16,6 +17,10 @@
                                 <a class="a-unstyled" href="<?= '/blog/'.$related_post->id ?>">
                                 	<?= $this->text_truncate($related_post->title, 120) ?>
                                 </a>
+                            </div>
+                            <div class="related-author">
+                                <img src="<?= $related_author->avatar->getLink(40, 40, true); ?>" ?>
+                                <?= $this->text('regular-by').' '?><?= $related_author->name ?>
                             </div>
                         </div>
                     </div>
