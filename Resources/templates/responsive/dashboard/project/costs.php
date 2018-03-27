d3a309155ea09d266bc03c9989b9522776216d25
Ivan Vergés <ivan@microstudi.net>
2018-03-27 18:10:37 +0200

    fix message listing

--- Resources/templates/responsive/dashboard/project/costs.php
+++ Resources/templates/responsive/dashboard/project/costs.php
@@ -47,3 +47,19 @@
 
 <?php $this->replace() ?>
 
+
+
+<?php $this->section('footer') ?>
+
+<script type="text/javascript">
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt
+
+$(function(){
+
+    $('.autoform').on('change', '.cost-item .type select', function() {
+        $(this).closest('.type').find('img').attr('src', '<?= $this->ee($this->asset('img/project/needs/'), 'js') ?>' + $(this).val() + '.png');
+    });
+
+// @license-end
+</script>
+<?php $this->append() ?>
