371e1e0210685f13a09e6ff829468ab1d644a7ff
Javier <javicarrillo83@gmail.com>
2018-04-11 17:40:01 +0200

    New style navbar blog.

--- Resources/templates/responsive/blog/partials/list_posts.php
+++ Resources/templates/responsive/blog/partials/list_posts.php
@@ -1,13 +1,20 @@
+<ul class="filters list-inline center-block text-center">
+	<?php foreach ($this->blog_sections as $key => $section) : ?>
+		<?php $icon= $key=='matchfunding' ? 'icon-call' : 'icon-'.$key ?>
+		<?php $description= $section.'-description'; ?>
+	    <a href="<?= '/blog/section/' . $key ?>" >
+	        <li class="<?php if ($section == $key||$key=='mission') echo 'active' ?>">
+	        	<span class="block icon icon-3x <?= $icon ?>"></span>
+	        	<br>
+	            <span><?= $this->text($section) ?></span>
+	        </li>
+	    </a>
+	<?php endforeach; ?>
+</ul>
  <div class="section list-posts container">
- 	<ul class="filters list-inline center-block text-center">
-	    <?php foreach ($this->blog_sections as $key => $section) : ?>
-	        <a href="<?= '/blog/section/' . $key ?>" >
-	            <li class="<?php if ($section == $key||$key=='mission') echo 'active' ?>">
-	                <?= $this->text($section) ?>
-	            </li>
-	        </a>
-	    <?php endforeach; ?>
-	</ul>
+ 	<div class="description">
+ 		<?= $this->text($description) ?>
+ 	</div>
  	<div class="row">
 	 	<?php foreach($this->list_posts as $post): ?>
 	 	<div class="col-md-4">
