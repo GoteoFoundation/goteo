03a3e0a54c6ce7335ed12d698e677d63b88d6279
Javier <javicarrillo83@gmail.com>
2018-04-13 17:25:42 +0200

    Filter by section

--- Resources/templates/responsive/blog/partials/list_sticky.php
+++ Resources/templates/responsive/blog/partials/list_sticky.php
@@ -1,18 +1,16 @@
 <!-- sticky menu -->
 
-<div class="sticky-menu" data-offset-top="600" data-spy="affix">
-	<div class="container-fluid">
-  	<ul class="filters list-inline center-block text-center">
-        <?php foreach ($this->blog_sections as $key => $section) : ?>
-            <?php $icon= $key=='matchfunding' ? 'icon-call' : 'icon-'.$key ?>
-            <a href="<?= '/blog/section/' . $key ?>" >
-                <li class="<?php if ($section == $key||$key=='mission') echo 'active' ?>">
-                    <span class="block icon icon-3x <?= $icon ?>"></span>
-                    <br>
-                    <span><?= $this->text($section) ?></span>
-                </li>
-            </a>
-        <?php endforeach; ?>
-    </ul>
-	</div>
+<div class="sticky-menu sticky-list" data-offset-top="600" data-spy="affix">
+      	<ul class="filters list-inline center-block text-center">
+            <?php foreach ($this->blog_sections as $key => $section) : ?>
+                <?php $icon= $key=='matchfunding' ? 'icon-call' : 'icon-'.$key ?>
+                <a href="<?= '/blog/section/' . $key ?>" >
+                    <li class="<?php if ($section == $key||$key=='mission') echo 'active' ?>">
+                        <span class="block icon icon-3x <?= $icon ?>"></span>
+                        <br>
+                        <span><?= $this->text($section) ?></span>
+                    </li>
+                </a>
+            <?php endforeach; ?>
+        </ul>
 </div>
