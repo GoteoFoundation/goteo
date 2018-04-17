9cb76b68645ccb5b36e913d593cd7e6d8c90a34d
Javier <javicarrillo83@gmail.com>
2018-04-17 16:46:35 +0200

    Add tags

--- Resources/templates/responsive/blog/partials/list_posts.php
+++ Resources/templates/responsive/blog/partials/list_posts.php
@@ -1,22 +1,28 @@
-<ul class="filters list-inline center-block text-center">
-	<?php foreach ($this->blog_sections as $key => $section_value) : ?>
-		<?php $icon= $key=='matchfunding' ? 'icon-call' : 'icon-'.$key ?>
-		<?php if($this->section == $key): ?>
-			<?php $description= $section_value.'-description'; ?>
-		<?php endif; ?>
-	    <a href="<?= '/blog-section/' . $key ?>" >
-	        <li class="<?php if ($this->section == $key) echo 'active' ?>">
-	        	<span class="block icon icon-3x <?= $icon ?>"></span>
-	        	<br>
-	            <span><?= $this->text($section_value) ?></span>
-	        </li>
-	    </a>
-	<?php endforeach; ?>
-</ul>
+<?php if(!$this->tag->name): ?>
+	<ul class="filters list-inline center-block text-center">
+		<?php foreach ($this->blog_sections as $key => $section_value) : ?>
+			<?php $icon= $key=='matchfunding' ? 'icon-call' : 'icon-'.$key ?>
+			<?php if($this->section == $key): ?>
+				<?php $description= $section_value.'-description'; ?>
+			<?php endif; ?>
+		    <a href="<?= '/blog-section/' . $key ?>" >
+		        <li class="<?php if ($this->section == $key) echo 'active' ?>">
+		        	<span class="block icon icon-3x <?= $icon ?>"></span>
+		        	<br>
+		            <span><?= $this->text($section_value) ?></span>
+		        </li>
+		    </a>
+		<?php endforeach; ?>
+	</ul>
+<?php else: ?>
+	<h2 class="tag-title"><?= ucfirst($this->tag->name) ?></h2>
+<?php endif; ?>
  <div class="section list-posts container">
- 	<div class="description">
- 		<?= $this->text($description) ?>
- 	</div>
+ 	<?php if(!$this->tag->name): ?>
+	 	<div class="description">
+	 		<?= $this->text($description) ?>
+	 	</div>
+ 	<?php endif; ?>
  	<div class="row">
 	 	<?php foreach($this->list_posts as $post): ?>
 	 	<div class="col-md-4">
