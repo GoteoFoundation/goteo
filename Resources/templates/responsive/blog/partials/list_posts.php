6d1cdda7d0863db9d6c51d108d725f01e3ce0839
Javier <javicarrillo83@gmail.com>
2018-04-20 17:14:39 +0200

    Sticky anchor.

--- Resources/templates/responsive/blog/partials/list_posts.php
+++ Resources/templates/responsive/blog/partials/list_posts.php
@@ -1,11 +1,11 @@
 <?php if(!$this->tag->name): ?>
-	<ul class="filters list-inline center-block text-center">
+	<ul id="filters" class="filters list-inline center-block text-center">
 		<?php foreach ($this->blog_sections as $key => $section_value) : ?>
 			<?php $icon= $key=='matchfunding' ? 'icon-call' : 'icon-'.$key ?>
 			<?php if($this->section == $key): ?>
 				<?php $description= $section_value.'-description'; ?>
 			<?php endif; ?>
-		    <a href="<?= '/blog-section/' . $key ?>" >
+		    <a href="<?= '/blog-section/' . $key.'#filters' ?>" >
 		        <li class="<?php if ($this->section == $key) echo 'active' ?>">
 		        	<span class="block icon icon-3x <?= $icon ?>"></span>
 		        	<br>
