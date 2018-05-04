f773c3caf459a50d8f623a1ca581d7c457969f22
Ivan Vergés <ivan@platoniq.net>
2018-05-04 18:01:17 +0200

    fix css minification

--- public/templates/default/partials/header/styles.php
+++ public/templates/default/partials/header/styles.php
@@ -1,16 +1,19 @@
-    <!-- build:css view/css/all.css -->
-    <!-- POST PROCESSING THIS CSS BY GRUNT -->
-    <link rel="stylesheet" type="text/css" href="<?= SRC_URL ?>/view/css/goteo.css" />
-    <!-- endbuild -->
+<!-- POST PROCESSING THIS CSS BY GRUNT -->
 
-    <link href="<?= SRC_URL ?>/assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="<?= SRC_URL ?>/assets/vendor/animate.css/animate.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="<?= SRC_URL ?>/assets/css/legacy_compat.css" />
+<!-- build:css assets/css/vendor.css -->
+<link href="<?= SRC_URL ?>/assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="<?= SRC_URL ?>/assets/vendor/animate.css/animate.css" rel="stylesheet">
+<!-- endbuild -->
 
-    <!--[if IE]>
-    <link href="<?= SRC_URL ?>/view/css/ie.css" media="screen" rel="stylesheet" type="text/css" />
-    <![endif]-->
+<!-- build:css view/css/all.css -->
+<link rel="stylesheet" type="text/css" href="<?= SRC_URL ?>/view/css/goteo.css" />
+<link rel="stylesheet" type="text/css" href="<?= SRC_URL ?>/assets/css/legacy_compat.css" />
+<!-- endbuild -->
 
-    <!--[if lt IE 9]>
-    <script>alert('Your browser is outdated and will not work on this site! Please upgrade it!')</script>
-    <![endif]-->
+<!--[if IE]>
+<link href="<?= SRC_URL ?>/view/css/ie.css" media="screen" rel="stylesheet" type="text/css" />
+<![endif]-->
+
+<!--[if lt IE 9]>
+<script>alert('Your browser is outdated and will not work on this site! Please upgrade it!')</script>
+<![endif]-->
