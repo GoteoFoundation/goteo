0312cd05b47521821ca725487a351da2626aec30
Javier <javicarrillo83@gmail.com>
2018-07-18 15:38:43 +0200

    Migration fix

--- db/migrations/20180717095827_goteo_stories_match.php
+++ db/migrations/20180717095827_goteo_stories_match.php
@@ -35,7 +35,7 @@ class GoteoStoriesMatch
       ALTER TABLE `stories` ADD COLUMN `type` TINYTEXT NULL;
       ALTER TABLE `stories` ADD COLUMN `landing_match` TINYINT(1) NOT NULL DEFAULT 0;
       ALTER TABLE `stories` ADD COLUMN `landing_pitch` TINYINT(1) NOT NULL DEFAULT 0;
-      ALTER TABLE `goteo`.`stories` ADD COLUMN `sphere` BIGINT(20) UNSIGNED NULL;
+      ALTER TABLE `stories` ADD COLUMN `sphere` BIGINT(20) UNSIGNED NULL;
       ALTER TABLE `stories` ADD CONSTRAINT `sphere` FOREIGN KEY (`sphere`) REFERENCES `sphere`(`id`) ON UPDATE CASCADE ON DELETE CASCADE; 
     ";
   }
