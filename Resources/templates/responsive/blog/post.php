2d4c65259c1ab913ca8a139099a5988d79311a94
Ivan Vergés <ivan@platoniq.net>
2018-07-25 17:58:41 +0200

    fix optional partial view for blog

--- Resources/templates/responsive/blog/post.php
+++ Resources/templates/responsive/blog/post.php
@@ -13,7 +13,9 @@ $this->section('blog-content');
 	<?= $this->insert('blog/partials/blog_header') ?>
 	<?= $this->insert('blog/partials/content') ?>
 	<?= $this->insert('blog/partials/tags') ?>
-    <?= $this->insert('foundation/donor') ?>
+
+    <?= $this->insertif('foundation/donor') ?>
+
 	<?= $this->insert('blog/partials/related_posts') ?>
 	<?= $this->insert('blog/partials/sticky') ?>
 
