8a4ecbc972f05cb0ec0776588303c409d5ca422f
Ivan Vergés <ivan@platoniq.net>
2018-10-02 11:36:34 +0200

    show image instead of text in admin footprints

--- Resources/templates/responsive/admin/partials/objects/footprints.php
+++ Resources/templates/responsive/admin/partials/objects/footprints.php
@@ -1,6 +1,6 @@
 <?php
-foreach($this->raw('value') as $sdg) {
-    // echo $this->insert('admin/partials/objects/text', ['value' => '<img class="" src="' . $sdg->getIcon()->getLink(32,32) . '" alt="' . $sdg->name . '" title="' . $sdg->name . '">', 'class' => 'avatar']);
-    echo $this->insert('admin/partials/objects/text', ['value' => $sdg->name, 'class' => 'avatar'])."<br>\n";
+foreach($this->raw('value') as $foot) {
+    echo $this->insert('admin/partials/objects/text', ['value' => '<img class="" src="' . $foot->getIcon()->getLink(32,32) . '" alt="' . $foot->name . '" title="' . $foot->name . '">', 'class' => 'avatar']);
+    // echo $this->insert('admin/partials/objects/text', ['value' => $foot->name, 'class' => 'avatar'])."<br>\n";
 }
 ?>
