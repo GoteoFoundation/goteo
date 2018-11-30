db66aebe3f963606348246bee9aca5ad11a3c91f
Javier <javicarrillo83@gmail.com>
2018-11-30 17:44:07 +0100

    Donate fixes.

--- Resources/templates/responsive/pool/select_amount.php
+++ Resources/templates/responsive/pool/select_amount.php
@@ -16,12 +16,19 @@ $type=$this->type;
       	<p><?= $this->text('dashboard-my-wallet-pool-info') ?> <a data-toggle="modal" data-target="#poolModal" href=""><?= $this->text('regular-here') ?></a></p>
       </div>
 
+    <?php elseif($this->pool): ?>
+
+    <div class="wallet-available">
+      <?= $this->text('dashboard-my-wallet-available', amount_format($this->pool->amount)) ?>
+    </div>
+
     <?php endif; ?>
 
     <?= $this->insert('pool/partials/amount_box', [
       'description' => $type=='pool' ? $this->text('pool-recharge-amount-text') : $this->text($type.'-select-amount-description'),
       'button_text'  => $type=='pool' ? $this->text('recharge-button') : $this->text('landing-donor-button'),
-      'form_action' => $type=='pool' ? '/pool/payment' : '/donate/payment'
+      'form_action' => $type=='pool' ? '/pool/payment' : '/donate/payment',
+      'amount'  => $this->amount
     ]) ?>
 
 </div>
