f92a1d31ab298442afbe01282bb41ee629e9cc1c
David Igón <david.igon@riseup.net>
2019-02-12 12:56:31 +0100

    Add new methods in Model Normalizer and new class PromoteTransformer

---
+++ src/Goteo/Util/ModelNormalizer/Transformer/PromoteTransformer.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of the Goteo Package.
+ *
+ * (c) Platoniq y Fundación Goteo <fundacion@goteo.org>
+ *
+ * For the full copyright and license information, please view the README.md
+ * and LICENSE files that was distributed with this source code.
+ */
+namespace Goteo\Util\ModelNormalizer\Transformer;
+
+use Goteo\Core\Model;
+use Goteo\Library\Text;
+
+/**
+ * Transform a Model
+ */
+class PromoteTransformer extends AbstractTransformer {
+
+    protected $keys = ['id', 'image', 'title', 'subtitle'];
+
+    public function getInfo() {
+        $prj = $this->model->getProject();
+        return '<strong>'.($prj ? $prj->name.' - ' : '') . $this->getAuthor() . '</strong><br>' . Text::recorta($this->getReview(), 30);
+    }
+
+    public function getReview() {
+        return $this->model->review;
+    }
+    public function getAuthor() {
+        return $this->model->getUser() ? $this->model->getUser()->name : ('<span class="label label-info">' . Text::get('admin-no-project') . '</span>');
+    }
+
+    function getProject() {
+        return $this->model->getProject();
+    }
+
+    function getStatus() {
+        $status = $this->model->getStatus();
+        print_r($status);
+        return $status;
+    }
+
+
+    public function getActions() {
+        if(!$u = $this->getUser()) return [];
+        $ret = ['edit' => '/admin/promote/edit/' . $this->model->id];
+
+        if($this->getUser()->hasPerm('translate-language')) {
+            $ret['translate'] = '/translate/' . $this->getModelName() . '/' . $this->model->id;
+        }
+
+        // $ret['preview'] = '/promote/' . $this->model->id . '?preview';
+        return $ret;
+    }
+
+    public function getLink($type = 'public', $key = null) {
+        if($key !== 'id') return '';
+        if($type === 'public') {
+            return '/promote/' . $this->model->id .'?preview';
+        }
+        return '';
+    }
+}
