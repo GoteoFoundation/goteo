87b29223afcc5446fdc8dc8633b41a76bec27fd9
David Igón <david.igon@riseup.net>
2019-02-12 12:53:24 +0100

    Add layout.php and list.php for promote in the responsive view

---
+++ Resources/templates/responsive/admin/promote/list.php
@@ -0,0 +1,20 @@
+<?php
+
+$this->layout('admin/promote/layout');
+
+$this->section('admin-search-box-addons');
+?>
+<!-- <a class="btn btn-cyan" href="/admin/home/add"><i class="fa fa-plus"></i> <?= $this->text('admin-home-add') ?></a> -->
+
+<?php $this->replace() ?>
+
+<?php $this->section('admin-container-body') ?>
+
+    <h5><?= $this->text('admin-list-total', $this->total) ?></h5>
+
+    <?= $this->insert('admin/partials/material_table', ['list' => $this->model_list_entries($this->list, $this->fields)]) ?>
+
+  </div>
+</div>
+
+<?php $this->replace() ?>
