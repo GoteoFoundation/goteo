5137d202c1f2e9203acdce5d2c8fe027e78c52ef
Javier <javicarrillo83@gmail.com>
2019-04-05 16:31:08 +0200

    Contract migration

---
+++ db/migrations/20190404174654_goteo_electronic_contract.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Migration Task class.
+ */
+class GoteoElectronicContract
+{
+  public function preUp()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postUp()
+  {
+      // add the post-migration code here
+  }
+
+  public function preDown()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postDown()
+  {
+      // add the post-migration code here
+  }
+
+  /**
+   * Return the SQL statements for the Up migration
+   *
+   * @return string The SQL string to execute for the Up migration.
+   */
+  public function getUpSQL()
+  {
+     return "
+      ALTER TABLE `contract` ADD COLUMN `electronic` TINYINT(1) DEFAULT 1 NOT NULL AFTER `number`;
+     ";
+  }
+
+  /**
+   * Return the SQL statements for the Down migration
+   *
+   * @return string The SQL string to execute for the Down migration.
+   */
+  public function getDownSQL()
+  {
+     return "
+      ALTER TABLE `contract` DROP COLUMN `electronic`;
+     ";
+  }
+
+}
