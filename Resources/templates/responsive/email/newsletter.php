cb5147f0d345a2bab1eb05228cc53afa764f7361
Javier <javicarrillo83@gmail.com>
2019-05-09 16:20:39 +0200

    Revert "Parser css"
    This reverts commit b544bfe28c85cdf1092606ad0aba238d74927794.

--- Resources/templates/responsive/email/newsletter.php
+++ Resources/templates/responsive/email/newsletter.php
@@ -6,8 +6,194 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Goteo Newsletter</title>
     <style>
-    @import url(https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic|Open+Sans+Condensed:300|Kalam);
-    @media only screen and (max-width: 620px) {
+        @import url(https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic|Open+Sans+Condensed:300|Kalam);
+        /* A simple css reset */
+        body,table,thead,tbody,tr,td,img {
+            padding: 0;
+            margin: 0;
+            border: none;
+            border-spacing: 0px;
+            border-collapse: collapse;
+            vertical-align: top;
+            font-family: Roboto,sans-serif;
+            color: #3a3a3a;
+        }
+        a{
+            text-decoration: none;
+            color: #337ab7;
+        }
+        /* Add some padding for small screens */
+        .wrapper {
+            padding-left: 0px;
+            padding-right: 0px;
+        }
+        h1,h2,h3,h4,h5,h6,p {
+            margin: 0;
+            padding: 0 20px 20px 20px;
+            line-height: 1.6;
+        }
+        .margin-btn{
+        	margin-top: 40px;
+        	margin-bottom: 80px;
+        }
+        .pd-footer{
+        	padding: 40px 0px 40px 0px !important;
+        }
+        .pd-description{
+        	padding: 30px 0px 30px 0px !important;
+            font-weight: 200;
+        }
+        .mt-40{
+            margin-top: 40px;
+        }
+
+        h1{
+        	font-size: 28px;
+        	color: #149290;
+        	text-align: center;
+        	margin-top: 20px;
+        }
+        h2{
+        	font-size: 18px;
+        	color: #a63c98;
+        	text-align: center;
+        }
+
+        h2.title-projects-section{
+            font-size: 21px;
+            margin-top: 35px;
+            text-align: center;
+            color: #3a3a3a;
+            margin-bottom: 20px;
+        }
+
+        .title-projects{
+        	padding-bottom: 20px;
+            padding-top: 10px;
+            padding-left: 20px;
+            font-size: 22px;
+            line-height: 1.;
+            color: #2bbbb7;
+            font-weight: 400;
+            display: block;
+        }
+
+        .subtitle-projects{
+        	padding-bottom: 4px;
+        	font-size: 15px;
+        	line-height: 0.8;
+            font-weight: 400;
+        }
+        .icon-info{
+        	padding-bottom: 4px;
+        	font-size: 13px;
+        	color: #919193;
+        	line-height: 0.8;
+        }
+        .icons{
+        	width: 8% !important;
+        	display: inline-block;
+        }
+        .logo-fundacion{
+        	width: 80% !important;
+        	display: inline-block;
+        }
+        .claim-fundacion{
+            padding: 20px 0 20px 60px;
+        }
+        hr{
+        	width: 55%;
+        	margin-left: 20px;
+        	border: 1px solid #c6cdcc;
+        }
+        .pd-all{
+        	padding: 10px 0px 10px 0px !important;
+        }
+        .pd-fundacion{
+        	padding: 40px 0px 40px 370px;
+        }
+        .pd-fundacion-dos{
+        	padding: 0px 100px 30px 0px !important;
+        }
+        .btn-fundacion{
+			color: #58595b;
+    		padding: 13px 0;
+    		background-color: #fff;
+    		display: inline-block;
+		    padding: 6px 12px;
+		    margin-bottom: 0;
+		    font-size: 14px;
+		    font-weight: 400;
+		    line-height: 1.42857143;
+		    text-align: center;
+		    white-space: nowrap;
+		    cursor: pointer;
+		    border: 1px solid transparent;
+		    border-radius: 4px;
+		    text-decoration: none;
+		}
+        .btn-fundacion:hover{
+            background-color: #e6e6e6;
+        }
+        .btn-proyectos{
+			color: #ffffff;
+    		padding: 13px 0;
+    		background-color: #19b4b2;
+    		display: inline-block;
+		    padding: 6px 12px;
+		    margin-bottom: 0;
+		    font-size: 14px;
+		    font-weight: 400;
+		    line-height: 1.42857143;
+		    text-align: center;
+		    white-space: nowrap;
+		    cursor: pointer;
+		    border: 1px solid transparent;
+		    border-radius: 4px;
+		    text-decoration: none;
+		}
+        .btn-proyectos:hover{
+            background-color: #1DAAA9;
+        }
+		.proyectos{
+			margin-bottom: 30px;
+		}
+		.footer{
+			color: #FFF;
+			text-align: center;
+		}
+		.footer p a{
+			color: #FFF;
+			text-align: center;
+			padding-bottom: 0px;
+            font-weight: 400;
+            text-decoration: underline;
+		}
+
+        .footer p{
+            color: #FFF;
+            text-align: center;
+            padding-bottom: 0px;
+            font-weight: 200;
+            font-weight: 400;
+        }
+        
+        .footer img{
+            width: 100%;
+        }
+
+        li{
+            margin-bottom: 10px;
+        }
+        
+		.text-center{
+			text-align: center;
+		}
+        img {
+            max-width: 100%;
+            display: block;
+        }
+        @media only screen and (max-width: 620px) {
             .wrapper .section {
                 width: 100%;
             }
@@ -92,7 +278,7 @@
                                     <tbody>
                                         <tr>
                                             <td align="left">
-                                            	<img class="pd-all" src="<?= $this->get_url.'/assets/img/goteo-white.svg' ?>" alt="" />
+                                            	<img class="pd-all" src="<?= $this->asset('img/goteo-white.svg') ?>" alt="" />
                                             </td>
                                         </tr>
                                     </tbody>
@@ -120,7 +306,7 @@
                                     <tbody>
                                         <tr>
                                             <td align="left">
-                                            	<img class="img-header" src="<?= $this->get_url.'/assets/img/newsletter/header.png' ?>" alt="Goteo" />
+                                            	<img class="img-header" src="<?= $this->asset('img/newsletter/header.png') ?>" alt="Goteo" />
                                             </td>
                                         </tr>
                                     </tbody>
@@ -161,20 +347,20 @@
                                         <tr>
                                             <td style="padding-right: 10px; padding-top:20px;" colspan="0" align="center">
                                                 <a style="text-decoration: none; color: #212121;" href="https://t.me/goteofunding" target="_blank">
-                                                    <img src="<?= $this->get_url.'/assets/img/newsletter/telegram.png' ?>" alt="Telegram" width="30" height="30" border="0" style="display: block;" />
+                                                    <img src="<?= $this->asset('img/newsletter/telegram.png') ?>" alt="Telegram" width="30" height="30" border="0" style="display: block;" />
                                                 </a>
                                             </td>
                                             <td style="padding-right: 10px; padding-top:20px;" colspan="0" align="center">
                                                 <a style="text-decoration: none; color: #212121;" href="http://twitter.com/goteofunding" target="_blank">
-                                                    <img src="<?= $this->get_url.'/assets/img/newsletter/twitter.png' ?>" alt="Twitter" width="30" height="30" border="0" style="display: block;" /></a>
+                                                    <img src="<?= $this->asset('img/newsletter/twitter.png') ?>" alt="Twitter" width="30" height="30" border="0" style="display: block;" /></a>
                                             </td>
                                             <td style="padding-right: 10px; padding-top:20px;" colspan="0" align="center">
                                                 <a style="text-decoration: none; color: #212121;" href="https://www.facebook.com/goteofunding/" target="_blank">
-                                                    <img src="<?= $this->get_url.'/assets/img/newsletter/facebook.png' ?>" alt="Facebook" width="30" height="30" border="0" style="display: block;" /></a>
+                                                    <img src="<?= $this->asset('img/newsletter/facebook.png') ?>" alt="Facebook" width="30" height="30" border="0" style="display: block;" /></a>
                                             </td>
                                             <td style="padding-right: 10px; padding-top:20px;" colspan="0" align="center">
                                                 <a style="text-decoration: none; color: #212121;" href="https://instagram.com/goteofunding/" target="_blank">
-                                                    <img src="<?= $this->get_url.'/assets/img/newsletter/instagram.png' ?>" alt="Instagram" width="30" height="30" border="0" style="display: block;" /></a>
+                                                    <img src="<?= $this->asset('img/newsletter/instagram.png') ?>" alt="Instagram" width="30" height="30" border="0" style="display: block;" /></a>
                                             </td>
                                         </tr>
                                 </td>
@@ -250,7 +436,7 @@
                                                 </p>
                                             </td>
                                             <td style="padding-right: 10px;" colspan="0" align="center">
-                                                <a style="text-decoration: none; color: #212121;" href="http://fundacion.goteo.org/" target="_blank"><img src="<?= $this->get_url.'/assets/img/logo-fg-white.png' ?>" alt="Fundación Goteo" width="30" height="30" border="0" style="display: block;" /></a>
+                                                <a style="text-decoration: none; color: #212121;" href="http://fundacion.goteo.org/" target="_blank"><img src="<?= $this->asset('img/logo-fg-white.png') ?>" alt="Fundación Goteo" width="30" height="30" border="0" style="display: block;" /></a>
                                             </td>
                                         </tr>
                                 </td>
