95443ae0d07d2e5278c604b9ad02e2818512c979
David Igón <david.igon@riseup.net>
2019-06-11 13:32:25 +0200

    Add Communication view. Currently it's just the filter form

---
+++ Resources/templates/responsive/admin/communication/layout.php
@@ -0,0 +1,24 @@
+<?php
+
+$this->layout('admin/container');
+
+$this->section('admin-container-head');
+
+?>
+    <h2><?= $this->text('admin-communication') ?></h2>
+
+    <?= $this->insert('admin/partials/search_box') ?>
+
+    <?= $this->supply('admin-communication-head') ?>
+
+    
+
+
+<?php $this->replace() ?>
+
+
+<?php $this->section('footer') ?>
+
+<script type="text/javascript" src="<?= $this->asset('js/admin/communication.js') ?>"></script>
+
+<?php $this->append() ?>
