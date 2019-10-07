da0fdd2f84d59a6120128ed787ba68e2f929f795
David Igón <david.igon@riseup.net>
2019-10-07 17:48:39 +0200

    fix buttons on communication detail, now it depends of the state of the communication activate/cancel

--- Resources/templates/responsive/admin/communication/detail.php
+++ Resources/templates/responsive/admin/communication/detail.php
@@ -25,8 +25,11 @@ $this->section('admin-container-head');
 
     <div class="spacer-20 forms">
         <p class="buttons">
-            <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/cancel/<?= $this->communication->id?>"><i class="fa fa-ban"></i> <?= $this->text('admin-communications-cancel') ?></a>
-            <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/send/<?= $this->communication->id?>"><i class="fa fa-send"></i> <?= $this->text('admin-communications-send') ?></a>
+            <?php if (!$this->communication->isSent() & $this->communication->isActive()): ?>
+              <a class="show-form btn btn-danger btn-lg" href="/admin/communication/cancel/<?= $this->communication->id?>"><i class="fa fa-ban"></i> <?= $this->text('admin-communications-cancel') ?></a>
+            <?php elseif (!$this->communication->isActive()): ?>
+              <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/send/<?= $this->communication->id?>"><i class="fa fa-send"></i> <?= $this->text('admin-communications-send') ?></a>
+            <?php endif ?>
             <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/preview/<?= $this->communication->id?>"><i class="fa fa-eye"></i> <?= $this->text('regular-preview') ?></a>
             <?php if(!$this->communication->isActive()) : ?>
             <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/edit/<?= $this->communication->id?>"><i class="fa fa-pencil"></i> <?= $this->text('regular-edit') ?></a>
