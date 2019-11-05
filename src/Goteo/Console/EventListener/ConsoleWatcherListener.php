051a2544881a9cef205550b36d655800d0f207ea
Javier <javicarrillo83@gmail.com>
2019-11-05 17:41:06 +0100

    Add contract reminder

--- src/Goteo/Console/EventListener/ConsoleWatcherListener.php
+++ src/Goteo/Console/EventListener/ConsoleWatcherListener.php
@@ -172,7 +172,10 @@ class ConsoleWatcherListener extends AbstractListener {
             }
         }
 
-        if($days_funded == 15 && $contract_status['owner']) {
+       print_r($contract_status);
+       echo $days_funded;
+
+        if($days_funded == 15 && $contract_status->owner) {
                 $this->info("Contract form reminder", [$project, 'days_active' => $days_active, 'days_funded' => $days_funded, 'days_succeeded' => $days_succeeded, 'contract_status' => $contract_status]);
 
                 $this->send($project, '15d_after', ['owner']);
@@ -419,7 +422,7 @@ class ConsoleWatcherListener extends AbstractListener {
                 $this->send($project, '7d_after', ['owner']);
         }
 
-        if($days_funded == 15 && $contract_status['owner']) {
+        if($project->one_round && $days_funded == 15 && $contract_status->owner) {
                 $this->info("Contract form reminder", [$project, 'days_active' => $days_active, 'days_funded' => $days_funded, 'days_succeeded' => $days_succeeded, 'contract_status' => $contract_status]);
 
                 $this->send($project, '15d_after', ['owner']);
