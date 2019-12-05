64b85d7f80d73526b8c4ff0196aa6380d2524c23
Javier <javicarrillo83@gmail.com>
2019-12-05 17:21:19 +0100

    Migrate rename

---
+++ db/migrations/20191204175323_goteo_questionnaire.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Migration Task class.
+ */
+class GoteoQuestionnaire
+{
+  public function preUp()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postUp()
+  {
+      // add the post-migration code here
+  }
+
+  public function preDown()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postDown()
+  {
+      // add the post-migration code here
+  }
+
+  /**
+   * Return the SQL statements for the Up migration
+   *
+   * @return string The SQL string to execute for the Up migration.
+   */
+  public function getUpSQL()
+  {
+     return "
+
+      CREATE TABLE `questionnaire` (
+            `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `matcher` VARCHAR(50) NOT NULL,
+            `vars` text,
+            PRIMARY KEY (`id`)
+        );
+        
+      CREATE TABLE `questionnaire_answer` (
+          `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+          `questionnaire` BIGINT(20) UNSIGNED NOT NULL,
+          `answer` text,
+          PRIMARY KEY (`id`),
+          CONSTRAINT `questionnaire_answer_ibfk_1` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+      );
+
+      ALTER TABLE `document` ALTER COLUMN `contract` varchar(50) NULL;
+
+     ";
+  }
+
+  /**
+   * Return the SQL statements for the Down migration
+   *
+   * @return string The SQL string to execute for the Down migration.
+   */
+  public function getDownSQL()
+  {
+     return "
+        DROP TABLE `questionnaire_answer`;      
+        DROP TABLE `questionnaire`;
+    ";
+  }
+
+}
