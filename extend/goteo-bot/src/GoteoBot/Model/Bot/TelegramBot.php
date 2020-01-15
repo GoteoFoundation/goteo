cb2f37dca81ed0c9411a05dd3352391e831a67da
David Igón <david.igon@riseup.net>
2020-01-15 11:45:24 +0100

    fix link in sendImage and sendAnimation

--- extend/goteo-bot/src/GoteoBot/Model/Bot/TelegramBot.php
+++ extend/goteo-bot/src/GoteoBot/Model/Bot/TelegramBot.php
@@ -61,7 +61,7 @@ Class TelegramBot implements \GoteoBot\Model\Bot {
     public function sendAnimation($chatId, $animation, $caption) {
         $sendAnimation = new SendAnimation();
         $sendAnimation->chat_id = $chatId;
-        $sendAnimation->animation = $animation->getLink(300,300, true);
+        $sendAnimation->animation = $animation->getLink(300,300, true, true);
         $sendAnimation->caption = $caption;
         $result = $this->tgLog->performApiRequest($sendAnimation);
 
