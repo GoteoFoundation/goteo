aa7a59277573b5ead879bce78ce9aab5909ab4f7
David Igón <david.igon@riseup.net>
2020-01-27 17:45:43 +0100

    change save functin in matcherconfig, use of replace into

--- db/migrations/20191209105949_goteo_matcher_admin.php
+++ db/migrations/20191209105949_goteo_matcher_admin.php
@@ -43,6 +43,7 @@ class GoteoMatcherAdmin
                 `max_donation_per_project`INT(6),
                 `percent_of_donation` INT(6),
                 `donation_per_project` INT(6),
+                PRIMARY KEY (`matcher`)
                 CONSTRAINT `matcher_conf_ibfk_1` FOREIGN KEY (`matcher`) REFERENCES `matcher` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
             )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
      ";
