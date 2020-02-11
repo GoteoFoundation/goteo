2f01c73e5a064a258386cfb475a6c20130a82b4e
David Igón <david.igon@riseup.net>
2020-02-11 15:10:06 +0100

    add score to question view

--- Resources/templates/responsive/dashboard/partials/question_item.php
+++ Resources/templates/responsive/dashboard/partials/question_item.php
@@ -4,10 +4,17 @@
 <div class="panel section-content">
   <div class="panel-body reward-item">
     <div class="row">
-      <div class="amount">
-        <?= $this->form_row($form[$this->question->id . "_typeofquestion"]) ?>
+      <div class="form-group col-md-4">
+        <div class="input-wrap">
+          <?= $this->form_row($form[$this->question->id . "_typeofquestion"]) ?>
+        </div>
       </div>
-      <div class="form-group">
+      <div class="form-group col-md-4">
+        <div class="input-wrap">
+          <?= $this->form_row($form[$this->question->id . "_max_score"]) ?>
+        </div>
+      </div>
+      <div class="form-group col-md-4">
         <div class="input-wrap">
           <?= $this->form_row($form[$this->question->id . "_required"]) ?>
         </div>
