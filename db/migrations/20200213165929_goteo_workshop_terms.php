49c3d30968191e4465c5cebb4026a8f2f7cea52b
Javier <javicarrillo83@gmail.com>
2020-02-17 13:25:11 +0100

    Workshops translation fix

--- db/migrations/20200213165929_goteo_workshop_terms.php
+++ db/migrations/20200213165929_goteo_workshop_terms.php
@@ -33,6 +33,10 @@ class GoteoWorkshopTerms
   {
      return "
       ALTER TABLE `workshop` ADD COLUMN `terms_file_url` TINYTEXT NULL AFTER `schedule_file_url`;
+      ALTER TABLE `workshop` CHANGE `type` `event_type` TINYTEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+      ALTER TABLE `workshop` ADD COLUMN `type` VARCHAR(10) DEFAULT 'md' NULL AFTER `modified`;
+      ALTER TABLE `workshop` CHANGE `how_to_get` `how_to_get` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+      ALTER TABLE `workshop_lang` CHANGE `how_to_get` `how_to_get` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
      ";
   }
 
@@ -44,7 +48,11 @@ class GoteoWorkshopTerms
   public function getDownSQL()
   {
      return "
-      ALTER TABLE `workshop` DROP COLUMN `terms_file_url`
+      ALTER TABLE `workshop` DROP COLUMN `terms_file_url`;
+      ALTER TABLE `workshop` DROP COLUMN `type`;
+      ALTER TABLE `workshop` CHANGE `event_type` `type` TINYTEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+      ALTER TABLE `workshop` CHANGE `how_to_get` `how_to_get` TINYTEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+      ALTER TABLE `workshop_lang` CHANGE `how_to_get` `how_to_get` TINYTEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
      ";
   }
 
