a250c315df5ddde3986f6ec809e89db4ec694448
David Igón <david@goteo.org>
2020-03-10 13:07:45 +0100

    fix bug on criteriainvest

--- src/Goteo/Util/MatcherProcessor/CriteriaInvestMatcherProcessor.php
+++ src/Goteo/Util/MatcherProcessor/CriteriaInvestMatcherProcessor.php
@@ -37,20 +37,20 @@ class CriteriaInvestMatcherProcessor extends AbstractMatcherProcessor {
         $vars = $this->getVars();
         $amount = $invest->amount;
         $invested = Invest::getList(['methods' => 'pool', 'status' => Invest::$ACTIVE_STATUSES, 'projects' => $project,'users' => $matcher->getUsers()], null, 0, 0, 'money');
-        $has_reached_percent = ($project->getAmountPercent() <= $vars['percent_of_donation']);
+        $has_reached_percent = ($project->getAmountPercent() >= $vars['percent_of_donation']);
         $has_reached_donors = ($vars['min_number_of_donors']) ? ($vars['min_number_of_donors'] < $project->num_investors) : true;
         if ($has_reached_percent && $has_reached_donors && !$invested) {
             $amount = $vars['donation_per_project'];
         } else {
             $amount = 0;
         }
-
+        
         // Check if there's enough total to extract from user's pool
         if($matcher->getAvailableAmount() < $amount) {
             $error = 'Matcher funds exhausted';
             $amount = $matcher->getAvailableAmount();
         }
-
+        
         return $amount;
 
     }
