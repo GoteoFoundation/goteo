47455ed1dc137b7397e7e70ae34e94d8e893d71f
David Igón <david@goteo.org>
2020-03-27 13:02:21 +0100

    fix bug on criteriainvest

--- src/Goteo/Util/MatcherProcessor/CriteriaInvestMatcherProcessor.php
+++ src/Goteo/Util/MatcherProcessor/CriteriaInvestMatcherProcessor.php
@@ -35,8 +35,8 @@ class CriteriaInvestMatcherProcessor extends AbstractMatcherProcessor {
         $amount = $invest->amount;
         $invested = Invest::getList(['methods' => 'pool', 'status' => Invest::$ACTIVE_STATUSES, 'projects' => $project,'users' => $matcher->getUsers()], null, 0, 0, 'money');
         $has_reached_percent = ($vars['percent_of_donation'])? ($project->getAmountPercent() >= $vars['percent_of_donation']) : false;
-        $has_reached_min_amount = ($vars['min_amount_per_project'])? ($vars['min_amount_per_project'] < $project->amount) : false;
-        $has_reached_donors = ($vars['min_number_of_donors']) ? ($vars['min_number_of_donors'] < $project->num_investors) : true;
+        $has_reached_min_amount = ($vars['min_amount_per_project'])? ($vars['min_amount_per_project'] <= $project->amount) : false;
+        $has_reached_donors = ($vars['min_number_of_donors']) ? ($vars['min_number_of_donors'] <= $project->num_investors) : true;
 
         if ($has_reached_percent && $has_reached_donors && !$invested) {
             $amount = $vars['donation_per_project'];
