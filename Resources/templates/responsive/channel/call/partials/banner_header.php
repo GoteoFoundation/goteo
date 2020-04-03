efd239b66734d6033aaa9d97525f25e2c567076e
Javier <javicarrillo83@gmail.com>
2020-04-03 14:29:30 +0200

    Landing header

--- Resources/templates/responsive/channel/call/partials/banner_header.php
+++ Resources/templates/responsive/channel/call/partials/banner_header.php
@@ -1,4 +1,13 @@
 <div class="section banner-header">
+	<div class="custom-header">
+		<div class="pull-left">
+			<img src="/assets/img/channel/logo_crowdcoop.png" height="30px">
+		</div>
+		<div class="pull-right">
+			<span><?= $this->text('call-header-powered-by') ?></span>
+          	<img height="30" src="<?= '/assets/img/goteo-white-green.png' ?>" >
+      	</div>
+	</div>
 	<div class="image">
 		<?php if($this->channel->header_image): ?>
 			<img src="<?= $this->post->header_image->getLink(1920, 600, true) ?>" class="display-none-important header-default img-responsive  hidden-xs visible-up-1400">
@@ -7,10 +16,10 @@
 			<img src="<?= $this->post->header_image->getLink(750, 450, true) ?>" class="img-responsive visible-xs">
 
 		<?php else: ?>
-			<img src="/assets/img/blog/header_default.png" width="1920" class="display-none-important header-default img-responsive  hidden-xs visible-up-1400">
-			<img src="/assets/img/blog/header_default.png" width="1400" class="display-none-important header-default img-responsive  hidden-xs visible-1051-1400">
-			<img src="/assets/img/blog/header_default.png" width="1051" class="display-none-important header-default img-responsive  hidden-xs visible-768-1050">
-			<img src="/assets/img/blog/header_default.png" width="750"  class="img-responsive header-default visible-xs">
+			<img src="/assets/img/channel/header_default.png" width="1920" class="display-none-important header-default img-responsive  hidden-xs visible-up-1400">
+			<img src="/assets/img/channel/header_default.png" width="1400" class="display-none-important header-default img-responsive  hidden-xs visible-1051-1400">
+			<img src="/assets/img/channel/header_default.png" width="1051" class="display-none-important header-default img-responsive  hidden-xs visible-768-1050">
+			<img src="/assets/img/channel/header_default.png" width="750"  class="img-responsive header-default visible-xs">
 		<?php endif; ?>
 
 	</div>
