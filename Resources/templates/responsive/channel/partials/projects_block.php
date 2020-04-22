372cae1942fd26100b13f07f635b20d566b14a30
Javier <javicarrillo83@gmail.com>
2020-04-22 19:27:09 +0200

    Fix projects block error

--- Resources/templates/responsive/channel/partials/projects_block.php
+++ Resources/templates/responsive/channel/partials/projects_block.php
@@ -1,13 +1,14 @@
-<?= 'Num elements: '.$this->num_elements ?> <?php die; ?>
 <?php if ($this->projects) : ?>
+	<div class="row">
     <?php foreach ($this->projects as $project): ?>
-        <div class="<?= if($this->num_elements=4) ? 'col-md-3' : 'col-md-4'?> col-sm-6 col-xs-12 spacer widget-element">
+        <div class="<?= $this->num_elements=4 ? 'col-md-3' : 'col-md-4'?> col-sm-6 col-xs-12 spacer widget-element">
             <?= $this->insert('project/widgets/normal', [
                 'project' => $project,
                 'admin' => (bool)$this->admin
             ]) ?>
         </div>
     <?php endforeach; ?>
+	</div>
 <?php else : ?>
     <?= $this->text('discover-results-empty') ?>
 <?php endif ?>
