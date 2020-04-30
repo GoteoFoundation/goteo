391cd1a646a7127fdd31164cb20b0b90dab92e8c
David Igón <david@goteo.org>
2020-04-30 12:32:06 +0200

    add copies for channel call posts section

--- Resources/templates/responsive/channel/call/partials/posts_section.php
+++ Resources/templates/responsive/channel/call/partials/posts_section.php
@@ -6,10 +6,10 @@
 
   <div class="section posts">
     <div class="container">
-      <h2 class="title"><span class="icon icon-news icon-3x"></span>Últimas noticias</h2>
+      <h2 class="title"><span class="icon icon-news icon-3x"></span><?= $this->t('channel-call-posts-section-title') ?></h2>
       
       <div class="description">
-        Entérate de las últimas novedades sobre CrowdCoop
+        <?= $this->t('channel-call-posts-section-description') ?>
       </div>
       
       <div class="row spacer-20 slider slider-post">
