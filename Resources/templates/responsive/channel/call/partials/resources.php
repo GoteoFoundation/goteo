74fecb284774ce56dd7d52e287a9b6ef408161c5
David Igón <david@goteo.org>
2020-05-12 19:15:12 +0200

    add first approach to resources section

---
+++ Resources/templates/responsive/channel/call/partials/resources.php
@@ -0,0 +1,22 @@
+<div class="section resources">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <img class="img-responsive" src="/assets/img/channel/call/create_project.png" >
+      </div>
+      <div class="col-md-6">
+        <div class="info">
+          <div class="title">
+            <?= $this->t('channel-call-resources-title') ?>
+          </div>
+          <div class="description">
+            <?= $this->t('channel-call-resources-description') ?>
+          </div>
+          <div class="col-button">
+            <a href="<?= '/channel/'.$this->channel->id.'/create' ?>" class="btn btn-transparent"><i class="icon icon-plus icon-2x"></i><?= $this->text('channel-call-resources-button') ?></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
