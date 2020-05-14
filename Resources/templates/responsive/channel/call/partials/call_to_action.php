dd32ad174d1fd23dd6a7bc85dfc92c077f72d13b
Javier <javicarrillo83@gmail.com>
2020-05-14 18:37:58 +0200

    Update call to action

--- Resources/templates/responsive/channel/call/partials/call_to_action.php
+++ Resources/templates/responsive/channel/call/partials/call_to_action.php
@@ -14,7 +14,7 @@
                   <?= $this->t('channel-call-cta-create-project-description') ?>
                 </div>
                 <div class="col-button">
-                    <a href="/project/create" class="btn btn-transparent"><i class="icon icon-plus icon-2x"></i><?= $this->text('landing-more-info') ?></a>
+                    <a href="/project/create" class="btn btn-transparent"><i class="icon icon-plus icon-2x"></i><?= $this->text('regular-create') ?></a>
                 </div>
               </div>
             </div>
@@ -22,7 +22,7 @@
   
           <div class="col-md-6 col-sm-12 join-call">
             <div class="join-call">
-              <img class="img-responsive" src="/assets/img/channel/call/join_program.png" >
+              <img class="img-responsive" src="/assets/img/channel/call/training.png" >
               <div class="info">
                 <div class="title">
                   <?= $this->t('channel-call-cta-join-program-title') ?>
@@ -31,7 +31,7 @@
                   <?= $this->t('channel-call-cta-join-program-description') ?>
                 </div>
                 <div class="col-button">
-                    <a href="/project/create" class="btn btn-yellow"><i class="icon icon-plus icon-2x"></i><?= $this->text('landing-more-info') ?></a>
+                    <a href="/channel/crowdcoop/faq/training" class="btn btn-yellow"><i class="icon icon-plus icon-2x"></i><?= $this->text('landing-more-info') ?></a>
                 </div>
               </div>
             </div>
