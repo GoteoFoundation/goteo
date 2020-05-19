3ea6e63cb51f8d6d99370035dd8961ffd6e5e3bc
David Igón <david@goteo.org>
2020-05-19 13:42:20 +0200

    change program widget to not show button when there's no action

--- Resources/templates/responsive/channel/call/partials/program.php
+++ Resources/templates/responsive/channel/call/partials/program.php
@@ -34,7 +34,9 @@
           <img class="img-responsive" src="<?= $program->getHeader()->getLink(350,150) ?>" >
           <h1><?= $program->title ?></h1>
           <p><?= $program->description ?></p>
-          <a href="<?= $program->action_url ?>" class="btn btn-transparent"><i class="icon icon-<?= $program->icon ?> icon-2x"></i> <?= $program->action ?></a>
+          <?php if ($program->action): ?>
+            <a href="<?= $program->action_url ?>" class="btn btn-transparent"><i class="icon icon-<?= $program->icon ?> icon-2x"></i> <?= $program->action ?></a>
+          <?php endif; ?>
         </div>
       </div>
     <?php endforeach; ?>
