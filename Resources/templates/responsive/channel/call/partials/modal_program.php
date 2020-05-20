33c34b005a5e03c3c7cd5b8497b6700052265b16
Javier <javicarrillo83@gmail.com>
2020-05-20 18:47:32 +0200

    Fix program modal title

--- Resources/templates/responsive/channel/call/partials/modal_program.php
+++ Resources/templates/responsive/channel/call/partials/modal_program.php
@@ -8,7 +8,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="programModalLabel"><?= $program->title ?></h4>
+        <h4 class="modal-title" id="programModalLabel"><?= $program->description ?></h4>
       </div>
       <div class="modal-body">
        <?= $program->modal_description ?>
