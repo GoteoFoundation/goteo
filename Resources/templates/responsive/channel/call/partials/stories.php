85539d25e5353eb55412874cc909840c06a27f9d
David Igón <david@goteo.org>
2020-06-09 19:49:48 +0200

    add background image to stories model and channel call stories view

--- Resources/templates/responsive/channel/call/partials/stories.php
+++ Resources/templates/responsive/channel/call/partials/stories.php
@@ -7,9 +7,8 @@
 <div class="section stories">
   <div class="spacer-20 slider slider-stories">
     <?php foreach ($stories as $key => $story): ?>
-    <?php $story->image = 'story-background.png'; ?>
       <div class="container">
-        <img src='<?= $story->getImage()->getLink(1350,400,true) ?>'>
+        <img src='<?= $story->getBackgroundImage()->getLink(1350,400,true) ?>'>
         <div class="story">
           <div class="row">
             <div class="col-md-7 col-sm-8">
@@ -26,7 +25,9 @@
           <div class="info col-md-10 col-sm-8">
             <q><?= $story->description ?></q>
             <div class="author" >
-              <img class="author-avatar" src="<?= $story->getImage()->getLink(50,50) ?>">
+              <?php if ($story->pool_image): ?>
+                <img class="author-avatar" src="<?= $story->getImage()->getLink(50,50) ?>">
+              <?php endif; ?>
               <?= "- ".$story->title ?>
             </div>
           </div>
--- src/Goteo/Model/Stories.php
+++ src/Goteo/Model/Stories.php
@@ -42,7 +42,8 @@ class Stories extends \Goteo\Core\Model {
         $type,
         $landing_match = 0,
         $landing_pitch = 0,
-        $sphere
+        $sphere,
+        $background_image
         ;
 
 
@@ -84,6 +85,7 @@ class Stories extends \Goteo\Core\Model {
                 stories.landing_pitch as `landing_pitch`,
                 stories.landing_match as `landing_match`,
                 stories.sphere as `sphere`,
+                stories.background_image as `background_image`,
 
                 project.id as project_id,
                 project.name as project_name,
@@ -160,6 +162,7 @@ class Stories extends \Goteo\Core\Model {
                 stories.landing_pitch as `landing_pitch`,
                 stories.landing_match as `landing_match`,
                 stories.sphere as `sphere`,
+                stories.background_image as `background_image`,
                 project.id as project_id,
                 project.name as project_name,
                 project.amount as project_amount,
@@ -291,6 +294,7 @@ class Stories extends \Goteo\Core\Model {
                 stories.landing_pitch as `landing_pitch`,
                 stories.landing_match as `landing_match`,
                 stories.sphere as `sphere`,
+                stories.background_image as `background_image`,
 
                 project.id as project_id,
                 project.name as project_name,
@@ -395,6 +399,13 @@ class Stories extends \Goteo\Core\Model {
         return $this->PoolImageInstance;
     }
 
+    public function getBackgroundImage() {
+        if(!$this->BackgroundImageInstance instanceOf Image) {
+            $this->BackgroundImageInstance = new Image($this->background_image);
+        }
+        return $this->BackgroundImageInstance;
+    }
+
     public function save (&$errors = array()) {
         if (!$this->validate($errors)) return false;
 
@@ -446,7 +457,8 @@ class Stories extends \Goteo\Core\Model {
             'description',
             'review',
             'url',
-            'post'
+            'post',
+            'background_image'
             );
 
         try {
