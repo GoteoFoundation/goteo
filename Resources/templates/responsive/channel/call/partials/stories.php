b726ce151b9c77b89a8d09315703519b04a7d076
David Igón <david@goteo.org>
2020-06-09 19:31:54 +0200

    rework of stories view for channel call

--- Resources/templates/responsive/channel/call/partials/stories.php
+++ Resources/templates/responsive/channel/call/partials/stories.php
@@ -5,36 +5,34 @@
 ?>
 
 <div class="section stories">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 col-sm-6">
-        <h2 class="title">
-            <span class="icon icon-channel-chat icon-3x"></span>
-            <span><?= $this->t('channel-call-stories-title') ?></span>
-        </h2>
-      </div>
-      <div class="col-md-8 col-sm-6">
-        <div class="white-line"></div>
-      </div>
-    </div>
-
-    <div class="spacer-20 slider slider-stories">
+  <div class="spacer-20 slider slider-stories">
     <?php foreach ($stories as $key => $story): ?>
-      <div class="story">
-        <div class="col-md-4 col-sm-4" align="center">
-          <div class="image">
-            <img src="<?= $story->getImage()->getLink(150,150) ?>" >
+    <?php $story->image = 'story-background.png'; ?>
+      <div class="container">
+        <img src='<?= $story->getImage()->getLink(1350,400,true) ?>'>
+        <div class="story">
+          <div class="row">
+            <div class="col-md-7 col-sm-8">
+              <h2 class="title">
+                <span class="icon icon-channel-chat icon-3x"></span>
+                <span><?= $this->t('channel-call-stories-title') ?></span>
+              </h2>
+            </div>
+            <div class="col-md-4 col-sm-3">
+              <div class="green-line"></div>
+            </div>
           </div>
-        </div>
-        <div class="col-md-8 col-sm-8">
-          <p><?= $story->description ?></p>
-          <div class="author" >
-            <?= "- ".$story->title ?>
+      
+          <div class="info col-md-10 col-sm-8">
+            <q><?= $story->description ?></q>
+            <div class="author" >
+              <img class="author-avatar" src="<?= $story->getImage()->getLink(50,50) ?>">
+              <?= "- ".$story->title ?>
+            </div>
           </div>
         </div>
       </div>
-    <?php endforeach; ?>
+      <?php endforeach; ?>
     </div>
-  </div>
 </div>
 <?php endif; ?>
