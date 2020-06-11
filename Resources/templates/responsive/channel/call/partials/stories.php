afa1e6c2aea3eba9638ad88cc7472a790b8bf7d3
David Igón <david@goteo.org>
2020-06-11 17:45:07 +0200

    add xs view for stories in channel call

--- Resources/templates/responsive/channel/call/partials/stories.php
+++ Resources/templates/responsive/channel/call/partials/stories.php
@@ -6,38 +6,59 @@
 
 <div class="section stories">
   <div class="spacer-20 slider slider-stories">
+
     <?php foreach ($stories as $key => $story): ?>
+      <?php $title=explode("/", $story->title); ?>
       <div class="story-container">
-        <img src='<?= $story->getBackgroundImage()->getLink(1350,400,true) ?>'>
-        <div class="story">
-          <div class="row">
-            <div class="col-md-7 col-sm-9">
-              <h2 class="title">
-                <span class="icon icon-channel-chat icon-3x"></span>
-                <span><?= $this->t('channel-call-stories-title') ?></span>
-              </h2>
+        <div class="story-desktop-container">
+          <img src='<?= $story->getBackgroundImage()->getLink(1350,400,true) ?>'>
+          <div class="story">
+            <div class="row">
+              <div class="col-md-7 col-sm-9">
+                <h2 class="title">
+                  <span class="icon icon-channel-chat icon-3x"></span>
+                  <span><?= $this->t('channel-call-stories-title') ?></span>
+                </h2>
+              </div>
+              <div class="col-md-4 col-sm-2">
+                <div class="green-line"></div>
+              </div>
             </div>
-            <div class="col-md-4 col-sm-2">
-              <div class="green-line"></div>
-            </div>
-          </div>
-      
-          <div class="info col-md-10 col-sm-11">
-            <q><?= $story->description ?></q>
-            <div class="author" >
-              <?php if ($story->pool_image): ?>
-                <img class="author-avatar" src="<?= $story->getImage()->getLink(70,70, true) ?>">
-              <?php endif; ?>
-              <?php $title=explode("/", $story->title); ?>
-              <div class="author-name">
-                <?= $title[0] ?> <br>
-                <?= $title[1] ?>
+        
+            <div class="info col-md-10 col-sm-11">
+              <q><?= $story->description ?></q>
+              <div class="author" >
+                <?php if ($story->pool_image): ?>
+                  <img class="author-avatar" src="<?= $story->getImage()->getLink(70,70, true) ?>">
+                <?php endif; ?>
+                <div class="author-name">
+                  <?= $title[0] ?> <br>
+                  <?= $title[1] ?>
+                </div>
               </div>
             </div>
           </div>
         </div>
+
+        <div class="story-xs">
+          <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="<?= $story->getBackgroundImage()->getLink(600, 400, true) ?>" >
+            </div>
+            <div class="col-md-6">
+                <div class="info-container">
+                    <q><?= $story->description ?></q>
+
+                    <div class="author" >
+                      <?= $title[0] ?> <br>
+                      <?= $title[1] ?>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <?php endforeach; ?>
-    </div>
+    <?php endforeach; ?>
+  </div>
 </div>
 <?php endif; ?>
