2aeeb2b17c8b5645a0996943ef22b54addd4f01b
Javier <javicarrillo83@gmail.com>
2020-07-09 19:11:50 +0200

    Fix download url Fix responsive

--- Resources/templates/responsive/channel/call/partials/resources/downloads.php
+++ Resources/templates/responsive/channel/call/partials/resources/downloads.php
@@ -6,17 +6,17 @@
 <?php foreach ($this->resources as $item): ?>
 		<?php $image=$item->getImage(); ?>
 		<div class="row item">
-			<div class="col-md-2 col-xs-3 img">
+			<div class="col-md-2 col-xs-12 img">
 				<img class="responsive" src="<?= $image->getLink(200, 150, true) ?>">
 			</div>
-			<div class="col-md-8 col-xs-9 content">
+			<div class="col-md-8 col-xs-12 content">
 				<h2 class="category">
 					<i class="icon icon-resource-<?= $item->getCategory()->icon ?> icon-2x"></i> <?= $item->getCategory()->name ?></h2>
 				<div class="description">
 					<h3 class="title"><?= $item->title ?></h2>
 					<?= $item->description ?>
 				</div>
-				<a href="<?= $item->url ?>" class="btn btn-transparent pull-right">
+				<a href="<?= $item->action_url ?>" class="btn btn-transparent pull-right">
 					<img class="pdf" src="/assets/img/icons/channel-call/pdf.svg" width="20px"><?= $this->text('regular-download').' .PDF' ?>
 				</a>
 			</div>
