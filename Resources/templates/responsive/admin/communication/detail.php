a1b961a0c48c6016d18b0ad57607cf83c3d2eba4
David Igón <david@goteo.org>
2020-09-04 17:17:26 +0200

    fix bug in communication detail and edit buttons

--- Resources/templates/responsive/admin/communication/detail.php
+++ Resources/templates/responsive/admin/communication/detail.php
@@ -33,11 +33,10 @@ $this->section('admin-container-head');
               <a class="show-form btn btn-danger btn-lg" href="/admin/communication/cancel/<?= $this->communication->id?>"><i class="fa fa-ban"></i> <?= $this->text('admin-communications-cancel') ?></a>
             <?php elseif (!$this->communication->isActive() && !$this->communication->isSent()): ?>
               <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/send/<?= $this->communication->id?>"><i class="fa fa-send"></i> <?= $this->text('admin-communications-send') ?></a>
+              <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/edit/<?= $this->communication->id?>"><i class="fa fa-pencil"></i> <?= $this->text('regular-edit') ?></a>
             <?php endif ?>
+
             <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/preview/<?= $this->communication->id?>"><i class="fa fa-eye"></i> <?= $this->text('regular-preview') ?></a>
-            <?php if(!$this->communication->isActive()) : ?>
-            <a class="show-form btn btn-cyan btn-lg" href="/admin/communication/edit/<?= $this->communication->id?>"><i class="fa fa-pencil"></i> <?= $this->text('regular-edit') ?></a>
-            <?php endif ?>
           </p>
 
     </div>
