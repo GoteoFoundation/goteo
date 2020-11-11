a38c598bed32159a66aee6d3333f5effd932842d
David Igón <david.igon@riseup.net>
2020-11-11 17:59:10 +0100

    add new buttons for call to action

--- db/migrations/20201109115539_goteo_add_node_config_and_call_to_action_new_button.php
+++ db/migrations/20201109115539_goteo_add_node_config_and_call_to_action_new_button.php
@@ -33,9 +33,14 @@ class GoteoAddChannelConfig
   {
      return "
         ALTER TABLE `node` ADD COLUMN `config` TEXT;
+
         ALTER TABLE `call_to_action` ADD COLUMN `icon_2` varchar(255) NULL,
                                     ADD COLUMN `action_2` VARCHAR(255) NULL,
                                     ADD COLUMN `action_url_2` TINYTEXT NULL;
+
+        ALTER TABLE `call_to_action_lang` ADD COLUMN `action_2` VARCHAR(255) NULL,
+                                        ADD COLUMN `action_url_2` TINYTEXT NULL;
+
      ";
   }
 
@@ -51,6 +56,10 @@ class GoteoAddChannelConfig
         ALTER TABLE `call_to_action` DROP COLUMN `icon_2`,
                                     DROP COLUMN `action_2`,
                                     DROP COLUMN `action_url_2`;
+
+        ALTER TABLE `call_to_action_lang` DROP COLUMN `action_2`,
+                                        DROP COLUMN `action_url_2`;
+
      ";
   }
 
