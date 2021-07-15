93b531087ebebd6a3892fce3218d0ba573f6585b
David Igón <david@goteo.org>
2021-07-15 20:10:19 +0200

    add sdgs in home values based on footprint

--- Resources/templates/responsive/home/partials/values.php
+++ Resources/templates/responsive/home/partials/values.php
@@ -9,8 +9,6 @@
                 <?php endforeach; ?>
             </ul>
         </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <?php foreach($this->footprints as $index => $footprint): ?>
             <div class="row <?= ($index != 0)? "hidden" : '' ?>" id="goteo-values-<?= $footprint->id ?>">
                 <div class="col footprint-briefing">
@@ -24,33 +22,10 @@
                         <?php foreach($this->sdg_by_footprint[$footprint->id] as $sdg): ?>
                             <li><a href="<?= $sdg->link ?>"><img src="assets/img/ods/ods<?= $sdg->id ?>.svg" width="75" height="75" alt="<?= $sdg->name ?>"/></a></li>
                         <?php endforeach; ?>
-=======
-        <?php foreach($this->footprints as $footprint): ?>
-            <div class="row" id="goteo-values-<?= $footprint->id ?>">
-=======
-        <?php foreach($this->footprints as $index => $footprint): ?>
-            <div class="row <?= ($index != 0)? "hidden" : '' ?>" id="goteo-values-<?= $footprint->id ?>">
->>>>>>> add dynamic projects based on footprints and minimum amount and random selection
-                <div class="col footprint-briefing">
-                    <img src="assets/img/footprint/<?= $footprint->id ?>.svg" heigh="70" width="70" alt="<?= $footprint->name ?>" class="footprint" />
-                    <p><span class="footprint-label">Huella ecológica</span></p>
-                    <h2>Cómo dejar una mejor huella en el planeta</h2>
-                    <p>El impacto ambiental generado por la demanda humana hace de los recursos existentes en los ecosistemas del planeta se debiliten. Aquí apoyamos iniciativas de preservación de la capacidad ecológica de la Tierra de regenerar sus recursos</p>
-                    <h3>Objetivos de desarrollo sostenible relacionados:</h3>
-                    <p>Haz click para saber más:</p>
-                    <ul>
-                        <li><a href=""><img src="assets/img/ods/ods7.svg" width="75" height="75" alt="ODS"/></a></li>
-                        <li><a href=""><img src="assets/img/ods/ods12.svg" width="75" height="75" alt="ODS"/></a></li>
-                        <li><a href=""><img src="assets/img/ods/ods13.svg" width="75" height="75" alt="ODS"/></a></li>
-                        <li><a href=""><img src="assets/img/ods/ods14.svg" width="75" height="75" alt="ODS"/></a></li>
-                        <li><a href=""><img src="assets/img/ods/ods15.svg" width="75" height="75" alt="ODS"/></a></li>
->>>>>>> add dynamic footprints from database into home values and changes in footprint images
                     </ul>
                 </div>
                 <div class="col footprint-info">
                     <div class="slider slider-footprint-data">
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <?php foreach($footprint->getAllImpactData() as $impact_data): ?>
                             <div class="">
                                 <?php if ($impact_data->image): ?>
@@ -84,45 +59,6 @@
                 </div>
                 <div class="footprint-action">
                     <a href="/impact-discover?footprint=<?= $footprint->id ?>"><?= $this->t('home-footprint-values-see-projects') ?> <span class="icon glyphicon glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>
-=======
-=======
-
->>>>>>> add dynamic projects based on footprints and minimum amount and random selection
-                        <div class="">
-                            <img src="https://data.goteo.org/165x240c/cambio-climatico-1.jpg">
-                            <h2>Datos huella ecológica</h2>
-                            <h3><span>2,8</span> Planetas</h3>
-                            <p>Serían necesarios si todo el mundo consumiera como un ciudadano medio de la UE.</p>
-                        </div>
-                        <div class="">
-                            <img src="https://data.goteo.org/165x240c/captura-de-pantalla-2020-09-23-a-les-8.10.40.png">
-                            <h2>Datos huella ecológica</h2>
-                            <h3><span>2,8</span> Planetas</h3>
-                            <p>Serían necesarios si todo el mundo consumiera como un ciudadano medio de la UE.</p>
-                        </div>
-                        <div class="">
-                            <img src="https://data.goteo.org/165x240c/mrlr1gd-2.jpg">
-                            <h2>Datos huella ecológica</h2>
-                            <h3><span>2,8</span> Planetas</h3>
-                            <p>Serían necesarios si todo el mundo consumiera como un ciudadano medio de la UE.</p>
-                        </div>
-                    </div>
-                    <div class="slider slider-footprint-projects">
-                        <?php foreach($this->projects_by_footprint[$footprint->id] as $index => $project): ?>
-                            <div class="footprint-project">
-                                <img src="<?= $project->image->getLink(600, 416, true); ?>" class="bg-project eco">
-                                <div class="project-footprint">
-                                    <img src="assets/img/footprint/<?= $footprint->id ?>.svg" height="70" width="70" alt="<?= $footprint->name ?>" class="footprint" />
-                                </div>
-                                <h2><a href="/project/<?= $project->id ?>"><?= $this->text_truncate($this->ee($project->name), 80); ?></a></h2>
-                                <p><a href="/user/profile/<?= $this->project->user->id ?>"><?= $this->text('regular-by') . ' ' . $this->project->user->name ?></a></p>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-                <div class="footprint-action">
-                    <a href="">Ver proyectos de huella ecológica <span class="icon glyphicon glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>
->>>>>>> add dynamic footprints from database into home values and changes in footprint images
                 </div>
             </div>
         <?php endforeach; ?>
