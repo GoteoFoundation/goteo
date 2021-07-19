5375633d16cc9afb41a7e6c99dd28a4fef81e2cb
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2021-07-19 14:36:09 +0200

    Got rid of index_dev.php in favor of index.php that read ENV DEBUG parameter

--- public/index.php
+++ public/index.php
@@ -13,6 +13,8 @@ use Goteo\Application\App;
 use Goteo\Application\Config;
 use Symfony\Component\HttpFoundation\Request;
 
+$isDebugEnv = getenv("DEBUG");
+
 //Public Web path
 define('GOTEO_WEB_PATH', __DIR__ . '/');
 
@@ -21,29 +23,38 @@ require_once __DIR__ . '/../src/autoload.php';
 // Create first the request object (to avoid other classes reading from php://input specially)
 $request = Request::createFromGlobals();
 
-ini_set('display_errors', 0);
 error_reporting(E_ALL & ~E_NOTICE & ~E_USER_DEPRECATED); // for symfony user deprecated errors
+
+ini_set('display_errors', $isDebugEnv);
+if ($isDebugEnv) {
+    App::debug(true);
+}
+
+// Bored? Try the hard way and fix some notices:
+//Symfony\Component\Debug\Debug::enable();
 // error handle needs to go after autoload
 set_error_handler('Goteo\Application\App::errorHandler');
 
-// Config file...
 $config = getenv('GOTEO_CONFIG_FILE');
+if ($isDebugEnv) {
+    if (!is_file($config)) $config = __DIR__ . '/../config/dev-settings.yml';
+}
 if(!is_file($config)) $config = __DIR__ . '/../config/settings.yml';
 Config::load($config);
 Config::autosave();
 
-// Error traces
-if(Config::get('debug')) {
-    ini_set('display_errors', 1);
-    App::debug(true);
-}
-
 //Get from globals defaults
 App::setRequest($request);
 
+if ($isDebugEnv) {
+    $handler = new Monolog\Handler\StreamHandler('php://stdout', Monolog\Logger::DEBUG);
+    $handler->setFormatter(new Bramus\Monolog\Formatter\ColoredLineFormatter());
+
+    // Add a log level debug to stderr
+    App::getService('logger')->pushHandler($handler);
+    App::getService('syslogger')->pushHandler($handler);
+    App::getService('paylogger')->pushHandler($handler);
+}
+
 // Get the app
-$app = App::get();
-
-// handle routes, flush buffer out
-$app->run();
-
+App::get()->run();
