e75ef20ae06045c0b27dcd512e1aad118cee17d9
David Igón <david@goteo.org>
2021-11-02 16:35:06 +0100

    improve new model test using dataProviders

--- tests/Goteo/Model/LegalEntityTest.php
+++ tests/Goteo/Model/LegalEntityTest.php
@@ -3,6 +3,8 @@
 namespace Goteo\Model\Tests;
 
 use Goteo\Model\LegalEntity;
+use Goteo\Model\LegalEntity\NaturalPerson;
+use Goteo\Model\LegalEntity\LegalPerson;
 use Goteo\TestCase;
 
 class LegalEntityTest extends TestCase {
@@ -14,5 +16,36 @@ class LegalEntityTest extends TestCase {
         $this->assertInstanceOf(LegalEntity::class, $ob);
 
         return $ob;
+    }
+
+    /**
+     * @depends testInstance
+     */
+    public function testGetEntity(LegalEntity $legalEntity) {
+        $this->assertEquals(LegalEntity::NATURAL_PERSON, $legalEntity->getEntity());
+    }
+
+    public function testGetNaturalPerson() {
+        $this->assertEquals(LegalEntity::NATURAL_PERSON, LegalEntity::getNaturalPerson());
+    }
+
+    public function testGetLegalPerson() {
+        $this->assertEquals(LegalEntity::LEGAL_PERSON, LegalEntity::getLegalPerson());
+    }
+
+    public function testLegalEntities() {
+        $this->assertIsArray(LegalEntity::getLegalEntities());
+    }
+
+    /**
+     * @dataProvider legalEntitiesDataProvider
+     */
+    public function testCreate($inputType, $expectedOutput) {
+        $this->assertInstanceOf($expectedOutput, LegalEntity::create($inputType));
+    }
+
+    public function legalEntitiesDataProvider(): Iterable {
+            yield [LegalEntity::NATURAL_PERSON, NaturalPerson::class];
+            yield [LegalEntity::LEGAL_PERSON, LegalPerson::class];
     }
 }
