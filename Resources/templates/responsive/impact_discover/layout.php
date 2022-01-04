f67e008ac40098d5b8ec8d4d04ba55d742284501
David Igón <david@goteo.org>
2022-01-04 13:13:16 +0100

    fixes'

--- Resources/templates/responsive/impact_discover/layout.php
+++ Resources/templates/responsive/impact_discover/layout.php
@@ -22,7 +22,7 @@ $this->section('content');
 <?php $this->section('footer') ?>
     <?= $this->insert('impact_discover/partials/javascript') ?>
 
-    <?php if ($this->view = 'map'): ?>
+    <?php if ($this->view == 'map'): ?>
         <?= $this->insert('map/partials/javascript'); ?>
 
         <link rel="stylesheet" type="text/css" href="<?= SRC_URL ?>/assets/vendor/leaflet/dist/leaflet.css"/>
