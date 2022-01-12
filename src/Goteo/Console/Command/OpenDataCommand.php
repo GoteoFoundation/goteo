58873bc179aaa8b246024c87eef0fab56db043c6
David Igón <david@goteo.org>
2022-01-12 16:42:56 +0100

    add sdg extraction functions to open data command

--- src/Goteo/Console/Command/OpenDataCommand.php
+++ src/Goteo/Console/Command/OpenDataCommand.php
@@ -10,71 +10,161 @@
 
 namespace Goteo\Console\Command;
 
+use Goteo\Application\Exception\ModelNotFoundException;
+use Goteo\Core\Exception;
 use Goteo\Library\FileHandler\File;
 use Goteo\Model\Call;
 use Goteo\Model\Invest;
-use Goteo\Model\Matcher;
-use Goteo\Model\Node;
 use Goteo\Model\Origin;
 use Goteo\Model\Project;
+use Goteo\Model\Sdg;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class OpenDataCommand extends AbstractCommand {
 
     protected function configure()
     {
         $this->setName("opendata")
-             ->setDescription("Generates OpenData files")
-             ->setDefinition(array(
-                      new InputOption('update', 'u', InputOption::VALUE_NONE, 'Actually does the job. If not specified, nothing is done, readonly process.'),
-                      new InputOption('call', 'c', InputOption::VALUE_OPTIONAL, "If specified, extracts data for the given call ")
-                ))
-             ->setHelp(<<<EOT
+            ->setDescription("Generates OpenData files")
+            ->setDefinition([
+                new InputOption('call', 'c', InputOption::VALUE_OPTIONAL, "If specified, extracts data for the given call "),
+                new InputOption('sdg', 's', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'If specified, extracts data for the given sdgs')
+            ])
+            ->setHelp(<<<EOT
 This command generates files using the data from different sources and saves them. The sources can be channels, matchers, calls or projects.
 
 Usage:
 
 Extract Open Data for a call
 <info>./console opendata --call goteo </info>
-
-Update the provided call's summary data
-<info>./console opendata --call call_id </info>
-
 EOT
 );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $call_id  = $input->getOption('call');
 
         $this->log('Extract OpenData info', [], 'info');
 
-        if (isset($call_id)) {
+        if ($callOption = $input->getOption('call')) {
+            $call_id  = $callOption;
             $this->log("Retrieving {$call_id}'s data", [], 'info');
 
-            $call = Call::get($call_id);
+            try {
+                $call = Call::get($call_id);
+            } catch (ModelNotFoundException $e) {
+                $this->log("Call $call_id does not exist", [], 'error');
+                return;
+            }
+
             $this->extractCallOpenData($call);
+        }
+
+        if ($listSdg = $input->getOption(('sdg'))) {
+            foreach ($listSdg as $sdg_id) {
+                $this->log("Retrieving {$sdg_id}'s data", [], 'info');
+
+                $sdg = Sdg::get($sdg_id);
+                $this->extractSdgOpenData($sdg);
+            }
+        }
+
+    }
+
+    private function extractSdgOpenData(Sdg $sdg): void {
+
+        $this->extractSdgProjects($sdg);
+        $this->extractSdgInvests($sdg);
+    }
+
+    private function extractSdgProjects(Sdg $sdg) {
+        $fileName = time() . '-' . $sdg->id . '-projects.csv';
+        $file = File::factory(['bucket' => AWS_S3_BUCKET_DOCUMENT]);
+        $file->connect();
+        $file->setPath("open_data/sdg/$sdg->id/projects");
+
+        $projects_count = Project::getBySDGs([$sdg->id], 0, 0, true);
+        $projects = Project::getBySDGs([$sdg->id], 0, $projects_count);
+        $this->extractProjectOpenData($fileName, $projects);
+
+        if ($file->upload('/tmp/' . $fileName, $fileName)) {
+            $this->log("\nUpload of file {$fileName} completed!", [], 'info');
+        } else {
+            $this->log("\nUpload of file {$fileName} failed!", [], 'error');
+        }
+    }
+
+    private function extractSdgInvests(Sdg $sdg) {
+        $fileName = time() . '-' . $sdg->id . '-invests.csv';
+        $file = File::factory(['bucket' => AWS_S3_BUCKET_DOCUMENT]);
+        $file->connect();
+        $file->setPath("open_data/sdg/$sdg->id/invests");
+
+        $projects_count = Project::getBySDGs([$sdg->id], 0, 0, true);
+        $projects = Project::getBySDGs([$sdg->id], 0, $projects_count);
+
+        $invests_count = Invest::getList(['projects' => $projects, 'status' => Invest::STATUS_CHARGED], null, 0, 0, true);
+        $invests = Invest::getList(['projects' => $projects, 'status' => Invest::STATUS_CHARGED], null, 0, $invests_count);
+
+        $this->extractInvestOpenData($fileName, $invests);
+
+        if ( $file->upload('/tmp/' . $fileName, $fileName) ) {
+            $this->log("\nUpload of file {$fileName} completed!", [], 'info');
+        } else {
+            $this->log("\nUpload of file {$fileName} failed!", [], 'error');
         }
     }
 
     private function extractCallOpenData(Call $call): void {
 
-        $this->extractProjectsData($call);
-        $this->extractInvestsData($call);
+        $this->extractCallProjectsData($call);
+        $this->extractCallInvestsData($call);
     }
 
-    private function extractProjectsData(Call $call): void {
+    private function extractCallProjectsData(Call $call): void {
         $fileName = time() . '-' . $call->id . '-projects.csv';
         $file = File::factory(['bucket' => AWS_S3_BUCKET_DOCUMENT]);
         $file->connect();
-        $file->setPath('open_data');
-        $buffer = fopen('/tmp/' . $fileName, 'w+');
+        $file->setPath("open_data/call/$call->id/projects");
 
+        $projects_count = Project::getList(['called' => $call->id], 0, 0, true);
+        $projects = Project::getList(['called' => $call->id], 0, $projects_count);
+        $this->extractProjectOpenData($fileName, $projects);
+
+        if ($file->upload('/tmp/' . $fileName, $fileName)) {
+            $this->log("\nUpload of file {$fileName} completed!", [], 'info');
+        } else {
+            $this->log("\nUpload of file {$fileName} failed!", [], 'error');
+        }
+    }
+
+    private function extractCallInvestsData(Call $call): void {
+        $fileName = time() . '-' . $call->id . '-invests.csv';
+        $file = File::factory(['bucket' => AWS_S3_BUCKET_DOCUMENT]);
+        $file->connect();
+        $file->setPath("open_data/call/$call->id/invests");
+
+        $invests_count = Invest::getList(['calls' => $call->id, 'types' => 'nondrop', 'status' => Invest::STATUS_CHARGED], null, 0, 0, true);
+        $invests = Invest::getList(['calls' => $call->id, 'types' => 'nondrop', 'status' => Invest::STATUS_CHARGED], null, 0, $invests_count);
+
+        $this->extractInvestOpenData($fileName, $invests);
+
+        if ( $file->upload('/tmp/' . $fileName, $fileName) ) {
+            $this->log("\nUpload of file {$fileName} completed!", [], 'info');
+        } else {
+            $this->log("\nUpload of file {$fileName} failed!", [], 'error');
+        }
+    }
+
+    /**
+     * @param Project[] $projects
+     */
+    private function extractProjectOpenData(string $fileName, array $projects): void
+    {
+        $buffer = fopen('/tmp/' . $fileName, 'w+');
         $data = ['name',
                 'subtitle',
                 'description',
@@ -93,16 +183,12 @@ EOT
                 'donors',
                 'matched_donors'
         ];
-
         fputcsv($buffer, $data);
 
-        $projects_count = Project::getList(['called' => $call->id], 0, 0, true);
-        $projects = Project::getList(['called' => $call->id], 0, $countProjects);
-
-        $progress_bar = new ProgressBar($this->output, $projects_count);
+        $progress_bar = new ProgressBar($this->output, count($projects));
         $progress_bar->start();
 
-        foreach($projects as $project) {
+        foreach ($projects as $project) {
             $originVisits = Origin::getList(['project' => $project->id, 'type' => 'referer'], 0, 0, true);
             $projectInvestCount = Invest::getList(['projects' => $project->id, 'types' => 'drop'], null, 0, 0, true);
 
@@ -111,7 +197,7 @@ EOT
                 $project->subtitle,
                 $project->description,
                 $project->node,
-                implode(',',$project->getCategories()),
+                implode(',', $project->getCategories()),
                 implode(',', array_column($project->getSdgs(), 'name')),
                 $project->getSocialCommitment()->name,
                 $project->published,
@@ -131,20 +217,14 @@ EOT
 
         $progress_bar->finish();
         fclose($buffer);
-        if ($file->upload('/tmp/' . $fileName, $fileName)) {
-            $this->log("\nUpload of file {$fileName} completed!", [], 'info');
-        } else {
-            $this->log("\nUpload of file {$fileName} failed!", [], 'error');
-        }
     }
 
-    private function extractInvestsData(Call $call): void {
-        $fileName = time() . '-' . $call->id . '-invests.csv';
-        $file = File::factory(['bucket' => AWS_S3_BUCKET_DOCUMENT]);
-        $file->connect();
-        $file->setPath('open_data');
-
-        $buffer = fopen('/tmp/' . $fileName , 'w+');
+    /**
+     * @param Invest[] $invests
+     */
+    private function extractInvestOpenData(string $fileName, array $invests): void
+    {
+        $buffer = fopen('/tmp/' . $fileName, 'w+');
         $data = ['project',
                 'amount',
                 'date',
@@ -152,13 +232,10 @@ EOT
         ];
         fputcsv($buffer, $data);
 
-        $invests_count = Invest::getList(['calls' => $call->id, 'types' => 'nondrop', 'status' => Invest::STATUS_CHARGED], null, 0, 0, true);
-        $invests = Invest::getList(['calls' => $call->id, 'types' => 'nondrop', 'status' => Invest::STATUS_CHARGED], null, 0, $invests_count);
-
-        $progress_bar = new ProgressBar($this->output, $invests_count);
+        $progress_bar = new ProgressBar($this->output, count($invests));
         $progress_bar->start();
 
-        foreach($invests as $invest) {
+        foreach ($invests as $invest) {
             fputcsv($buffer, [
                 $invest->project,
                 $invest->amount,
@@ -170,11 +247,5 @@ EOT
         }
         $progress_bar->finish();
         fclose($buffer);
-
-        if ( $file->upload('/tmp/' . $fileName, $fileName) ) {
-            $this->log("\nUpload of file {$fileName} completed!", [], 'info');
-        } else {
-            $this->log("\nUpload of file {$fileName} failed!", [], 'error');
-        }
     }
 }
