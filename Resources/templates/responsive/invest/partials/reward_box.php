6d5802689d76e99ec5dfd8fd0a79532a87a7bc2d
David Igón <david@goteo.org>
2022-02-16 18:06:58 +0100

    improve accesibility in project rewards

--- Resources/templates/responsive/invest/partials/reward_box.php
+++ Resources/templates/responsive/invest/partials/reward_box.php
@@ -4,7 +4,6 @@ $selected = $selected;
 $available = $reward->available();
 ?>
     <form class="form-horizontal" role="form" method="GET" action="/invest/<?= $this->project->id ?>/payment">
-
         <div class="row padding-sm no-padding col-sm-10 col-sm-offset-1">
             <label class="label-reward <?= $selected ? 'reward-choosen' : '' ?><?= $available ? '' : ' label-disabled' ?>" for="reward-<?= $reward->id?>">
             <div class="col-sm-2 no-padding">
@@ -14,29 +13,28 @@ $available = $reward->available();
             <div class="col-sm-9">
                 <strong><?= $reward->reward ?></strong>
                 <div class="reward-description">
-                <?= $this->markdown($reward->description) ?>
+                    <?= $this->markdown($reward->description) ?>
                 </div>
                 <div style="margin-top:10px">
                     <?php if (!$available) : // no quedan ?>
-                    <span class="limit-reward"><?= $this->text('invest-reward-none') ?></span>
-                        <?php elseif (!empty($reward->units)) : // unidades limitadas ?>
+                        <span class="limit-reward"><?= $this->text('invest-reward-none') ?></span>
+                    <?php elseif (!empty($reward->units)) : // unidades limitadas ?>
                         <strong class="limit-reward"><?= $this->text('project-rewards-individual_reward-limited');?></strong><br>
-                        <?php $units = ($reward->units - $reward->taken); // resto?>
+                    <?php $units = ($reward->units - $reward->taken); // resto?>
                         <span class="limit-reward-number">
-                        <?= $this->text('project-rewards-individual_reward-units_left', $units) ?><br>
+                            <?= $this->text('project-rewards-individual_reward-units_left', $units) ?><br>
                         </span>
                     <?php endif ?>
                 </div>
             </div>
 
             <div class="amount-box">
-
                 <div class="row col-sm-10 col-sm-offset-2 margin-2" id="amount-container">
                     <div class="col-sm-1 col-sm-offset-1 no-padding col-xs-1">
-                        <strong class="reward-amount"><?= $this->get_currency('html') ?></strong>
+                        <label for="amount-<?= $reward->id ?>" class="reward-amount"><?= $this->get_currency('html') ?></label>
                     </div>
                     <div class="no-padding container-input-amount col-sm-4 col-xs-10">
-                        <input type="number" class="form-control input-amount" name="amount" value="<?= $this->amount ? $this->amount : amount_format($reward->amount, 0, true) ?>" id="amount" min="<?= amount_format($reward->amount, 0, true) ?>" required>
+                        <input type="number" class="form-control input-amount" name="amount" value="<?= $this->amount ? $this->amount : amount_format($reward->amount, 0, true) ?>" id="amount-<?= $reward->id ?>" min="<?= amount_format($reward->amount, 0, true) ?>" required>
                     </div>
                     <div class="col-sm-5 col-sm-offset-1 reward-button">
                         <button type="submit" class="btn btn-block btn-success col-xs-3 margin-2"><?= $this->text('invest-button') ?></button>
--- Resources/templates/responsive/invest/partials/steps_bar.php
+++ Resources/templates/responsive/invest/partials/steps_bar.php
@@ -9,11 +9,11 @@
         $class = 'disabled';
     ?>
 
-     <div class="bs-wizard-step <?= $class ?>">
-       <div class="text-center bs-wizard-stepnum"><?= $this->text('invest-step-' . $i) ?></div>
-       <div class="progress"><div class="progress-bar"></div></div>
-       <a href="#" class="bs-wizard-dot"></a>
-    </div>
+         <div class="bs-wizard-step <?= $class ?>">
+           <div class="text-center bs-wizard-stepnum"><?= $this->text('invest-step-' . $i) ?></div>
+           <div class="progress"><div class="progress-bar"></div></div>
+           <div class="bs-wizard-dot"></div>
+        </div>
 
     <?php endfor; ?>
 
