b9d2bb8e5851ac15002de26fc92214417d956276
David Igón <david@goteo.org>
2022-02-17 16:47:29 +0100

    accessibility fixes on register and login

--- Resources/templates/responsive/invest/login.php
+++ Resources/templates/responsive/invest/login.php
@@ -14,26 +14,24 @@ $query = $this->query . '&amp;return=' . urlencode($this->raw('return')) . '&amp
     <div class="row row-form">
         <div class="panel panel-default panel-form">
             <div class="panel-body">
-
-              <div style="padding:0 8%">
                 <h2 class="col-md-offset-1"><?= $this->text('login-title') ?></h2>
 
                 <form role="form" method="POST" action="/invest/<?= $this->project->id ?>/login?<?= $query ?>">
 
-                <?= $this->insert('auth/partials/form_login', ['signup_link' => '/invest/' . $this->project->id . '/signup?' . $query]) ?>
+                    <?= $this->insert('auth/partials/form_login', ['signup_link' => '/invest/' . $this->project->id . '/signup?' . $query]) ?>
 
-                <?= $this->insert('auth/partials/social_login') ?>
+                    <?= $this->insert('auth/partials/social_login') ?>
 
-                <?php if($this->skip_login): ?>
-                <div class="form-group">
-                    <div class="col-md-10 col-md-offset-1">
-                        <a href="/invest/<?= $this->project->id ?>/payment?<?= $query ?>&email" ><i class="fa fa-sign-out"></i> <?= $this->text('invest-no-register') ?></a>
-                    </div>
-                </div>
-                <?php endif ?>
+                    <?php if($this->skip_login): ?>
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1">
+                                <a href="/invest/<?= $this->project->id ?>/payment?<?= $query ?>&email" >
+                                    <i class="fa fa-sign-out"></i> <?= $this->text('invest-no-register') ?>
+                                </a>
+                            </div>
+                        </div>
+                    <?php endif ?>
                 </form>
-              </div>
-
             </div>
         </div>
     </div>
