9b6e90f488ffbb67ed9bbe74fe6cf261cc089550
David Igón <david@goteo.org>
2022-02-21 12:18:16 +0100

    fix label on input query

--- Resources/templates/responsive/partials/search.php
+++ Resources/templates/responsive/partials/search.php
@@ -1,8 +1,8 @@
 <div id="search">
     <button type="button" class="close">×</button>
     <form action="<?= $this->lang_host() ?>discover">
-        <input type="search" name="q" value="" placeholder="<?= $this->text('regular-search-desc') ?>" title="<?= $this->text('regular-search-desc') ?>" autocomplete="off"/>
-        <div class="tooltip"><?= $this->text('regular-search-tooltip') ?></div>
+        <input id="search" type="search" name="q" value="" placeholder="<?= $this->text('regular-search-desc') ?>" title="<?= $this->text('regular-search-desc') ?>" autocomplete="off"/>
+        <label for="search" class="tooltip"><?= $this->text('regular-search-tooltip') ?></label>
         <button type="submit" class="btn btn-white"><?= $this->text('regular-search') ?>
     </form>
 </div>
