6dbba2b70cf8560abee8fdee8e5ff27768e09e14
Javier Carrillo <javier@goteo.org>
2022-05-03 10:25:18 +0200

    FIx class name

--- Resources/templates/responsive/channel/partials/posts_section.php
+++ Resources/templates/responsive/channel/partials/posts_section.php
@@ -1,12 +1,12 @@
 <?php $channel=$this->channel; ?>
 
 <?php if($channel->getPosts()): ?>
-	<div class="section posts">
+	<div class="section posts-section">
 		<div class="container">
             <h2 class="title"><?= $this->text('home-posts-header') ?></h2>
 			<div class="row">
 				<?php foreach($channel->getPosts() as $related_post): ?>
-					<div class="col-md-4 related-post">
+					<div class="col-md-4 post">
                         <?= $this->insert('post/widgets/normal', [
                             'post' => $related_post
                         ]) ?>
