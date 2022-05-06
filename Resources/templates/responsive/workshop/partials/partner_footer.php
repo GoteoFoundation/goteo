b3dd9b1783857b282efdb601d2219dafe5e3d958
Javier Carrillo <javier@goteo.org>
2022-05-06 17:15:59 +0200

    fix logo size

--- Resources/templates/responsive/workshop/partials/partner_footer.php
+++ Resources/templates/responsive/workshop/partials/partner_footer.php
@@ -4,7 +4,7 @@
 			<?php foreach($this->footer_sponsors as $sponsor): ?>
 				<div class="col-xs-12">
 					<a target="_blank" href="<?= $sponsor->url ?>" class="center-block">
-	            		<img class="img-responsive no-title center-block" src="<?= $sponsor->getImage()->getLink(1200, 0, false) ?>" alt="<?= $sponsor->name ?>">
+	            		<img class="img-responsive no-title center-block" src="<?= $sponsor->getImage()->getLink(500, 0, false) ?>" alt="<?= $sponsor->name ?>">
 	        		</a>
 	        	</div>
 	    	<?php endforeach; ?>
