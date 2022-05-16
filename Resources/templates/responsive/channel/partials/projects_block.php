80039a43780dfc001119f925de86927a9023b86b
David Igón <david@goteo.org>
2022-05-16 12:22:55 +0200

    add copies and center dataset sections title
    (cherry picked from commit 6e859e416ea74a7eb08ba0b7547c3d37d8ea467a)

--- Resources/templates/responsive/channel/partials/projects_block.php
+++ Resources/templates/responsive/channel/partials/projects_block.php
@@ -1,7 +1,7 @@
 <?php if ($this->projects) : ?>
 	<div class="row">
     <?php foreach ($this->projects as $project): ?>
-        <div class="col-md-4'col-sm-6 col-xs-12 spacer widget-element">
+        <div class="col-md-4 col-sm-6 col-xs-12 spacer widget-element">
             <?= $this->insert('project/widgets/normal', [
                 'project' => $project,
                 'admin' => (bool)$this->admin
