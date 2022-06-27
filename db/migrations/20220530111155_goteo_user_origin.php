462f5c4519c5fb56af9bd7f0919b99c46c65778c
Javier Carrillo <javier@goteo.org>
2022-06-27 09:50:08 +0200

    Add origin register in profile form (#438)
    * Add origin register in profile form
    
    * Fix english translation and remove contrain in userform
    
    * Fix search label
    
    * Add new orgiin options

---
+++ db/migrations/20220530111155_goteo_user_origin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Migration Task class.
+ */
+class GoteoUserOrigin
+{
+  public function preUp()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postUp()
+  {
+      // add the post-migration code here
+  }
+
+  public function preDown()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postDown()
+  {
+      // add the post-migration code here
+  }
+
+  /**
+   * Return the SQL statements for the Up migration
+   *
+   * @return string The SQL string to execute for the Up migration.
+   */
+  public function getUpSQL()
+  {
+     return "
+        ALTER TABLE `user` ADD COLUMN `origin_register` VARCHAR(255) AFTER `legal_entity`;
+     ";
+  }
+
+  /**
+   * Return the SQL statements for the Down migration
+   *
+   * @return string The SQL string to execute for the Down migration.
+   */
+  public function getDownSQL()
+  {
+     return "
+        ALTER TABLE `user` DROP COLUMN `origin_register`;
+
+     ";
+  }
+
+}
