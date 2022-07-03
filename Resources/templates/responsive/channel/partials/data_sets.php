3997e309c87e1053134b429bbc24eae6a4fce13d
David Igón <david@goteo.org>
2022-07-04 01:52:37 +0200

    add if to show datasets in channel layout if not empty

--- Resources/templates/responsive/channel/partials/data_sets.php
+++ Resources/templates/responsive/channel/partials/data_sets.php
@@ -1,14 +1,14 @@
 <?php
     $section = $this->dataSetsSection;
+    if ($section):
 ?>
-
-<div class="section data-sets-section">
-    <div class="container">
-        <h2 class="title text-center"><?= $section->main_title ?></h2>
-        <p class="description">
-            <?= $section->main_description ?>
-        </p>
-        <?= $this->insert('partials/components/data_sets', ['dataSets' => $this->dataSets]) ?>
+    <div class="section data-sets-section">
+        <div class="container">
+            <h2 class="title text-center"><?= $section->main_title ?></h2>
+            <p class="description">
+                <?= $section->main_description ?>
+            </p>
+            <?= $this->insert('partials/components/data_sets', ['dataSets' => $this->dataSets]) ?>
+        </div>
     </div>
-</div>
-
+<?php endif; ?>
