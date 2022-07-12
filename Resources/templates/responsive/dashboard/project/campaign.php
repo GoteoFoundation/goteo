44a903e6ce5e22dbb7062b6eafadd4230299033f
David <david@goteo.org>
2022-07-12 11:08:55 +0200

    Deprecate Paypal fields from Project and Contract (#388)
    * deprecate paypal and paypal_owner and switch uses to allowpp field
    
    * fix copy refering to paypal account
    
    * remove deprecated function in project dashboard controller
    
    * add missing copies for english and catalan
    
    * create migration to update allowpp field in project_account table
    
    * Rename migration
    
    Co-authored-by: Javier Carrillo <javier@goteo.org>

--- Resources/templates/responsive/dashboard/project/campaign.php
+++ Resources/templates/responsive/dashboard/project/campaign.php
@@ -6,38 +6,12 @@
   <div class="inner-container">
 
     <h1>6. <?= $this->text('project-campaign-configuration') ?></h1>
-<!--     <div class="auto-hide">
-        <div class="inner"><?= $this->text('guide-campaign-description') ?></div>
-        <div class="more"><i class="fa fa-info-circle"></i> <?= $this->text('regular-help') ?></div>
-    </div>
- -->
 
     <?= $this->insert('dashboard/project/partials/goto_first_error') ?>
 
-    <?= $this->supply('dashboard-content-form', function() {
-        $form = $this->raw('form');
-        echo $this->form_start($form);
+    <?= $this->supply('dashboard-content-form') ?>
 
-        echo $this->form_row($form['one_round']);
-
-        echo $this->form_row($form['phone']);
-
-        echo '<div class="form-group spacer-10">
-          <div class="material-switch">
-              <input id="paypal_switch" type="checkbox"' . ($form['paypal']->vars['value'] ? ' checked="true"' : '') . '>
-              <label for="paypal_switch" class="label-cyan"></label>
-          </div>
-          <label for="paypal_switch"><i class="fa fa-paypal"></i> ' . $this->text('project-campaign-use-paypal') . '</label>
-          </div>';
-
-        echo '<div class="paypal' . ($form['paypal']->vars['value'] ? ' show' : '') . '">';
-            echo $this->form_row($form['paypal']);
-        echo '</div>';
-
-        echo $this->form_end($form);
-
-
-    }) ?>
+    <?= $this->form_form($this->raw('form')) ?>
 
     <?= $this->insert('dashboard/project/partials/partial_validation') ?>
 
@@ -56,19 +30,6 @@ $(function(){
         var $help = $(this).closest('.input-wrap').find('.help-text');
         $active = $help.find('span').eq(1-$(this).val()).removeClass('hidden');
         $help.find('span').not($active).addClass('hidden');
-    });
-
-
-    $('#paypal_switch').on('change', function() {
-        $paypal = $(this).closest('.form-group').next('.paypal');
-        // console.log('switch', $paypal.attr('class'));
-        if($(this).prop('checked')) {
-            $paypal.addClass('show');
-        } else {
-            $paypal.removeClass('show');
-            $paypal.find('input[type="email"]').val(' ');
-            $paypal.find('input[type="email"]').select();
-        }
     });
 });
 
--- src/Goteo/Model/Project/Account.php
+++ src/Goteo/Model/Project/Account.php
@@ -11,24 +11,32 @@
 namespace Goteo\Model\Project;
 
 use Goteo\Application\Config;
+use Goteo\Core\Exception;
+use Goteo\Core\Model;
 
-class Account extends \Goteo\Core\Model {
+class Account extends Model {
 
     public
         $project,
         $bank,
         $bank_owner,
-        $paypal,
-        $paypal_owner,
         $fee,
         $skip_login = false,
         $allowpp; // para permitir usar el boton paypal
 
+    /**
+     * @deprecated
+     */
+    public $paypal;
 
     /**
-     * Get the accounts for a project
-     * @param varcahr(50) $id  Project identifier
-     * @return array of accounts
+     * @deprecated
+     */
+    public $paypal_owner;
+
+    /**
+     * @throws Config\ConfigException
+     * @throws Exception
      */
  	public static function get ($id) {
 
@@ -48,15 +56,17 @@ class Account extends \Goteo\Core\Model {
                 return $accounts;
             }
         } catch(\PDOException $e) {
-			throw new \Goteo\Core\Exception($e->getMessage());
+			throw new Exception($e->getMessage());
         }
 	}
 
-	public function validate(&$errors = array()) {
-        // Estos son errores que no permiten continuar
+    /**
+     * @throws Config\ConfigException
+     */
+    public function validate(&$errors = array()): bool
+    {
         if (empty($this->project)) {
             $errors[] = 'No hay ningun proyecto al que asignar cuentas';
-            //Text::get('validate-account-noproject');
             return false;
         }
         if (!isset($this->fee)) $this->fee = Config::get('fee');
@@ -64,7 +74,11 @@ class Account extends \Goteo\Core\Model {
         return true;
     }
 
-	public function save (&$errors = array()) {
+    /**
+     * @throws Config\ConfigException
+     */
+    public function save (&$errors = array()): bool
+    {
         if (!$this->validate($errors)) return false;
 
 		try {
@@ -73,9 +87,6 @@ class Account extends \Goteo\Core\Model {
             $values = array(':project' => $this->project, ':bank' => $this->bank, ':bank_owner' => $this->bank_owner, ':paypal' => $this->paypal, ':paypal_owner' => $this->paypal_owner, ':allowpp' => $this->allowpp, ':fee' => $this->fee, ':skip_login' => $this->skip_login);
 			self::query($sql, $values);
 
-            // Update in contract if exists
-            $sql = "UPDATE IGNORE contract SET paypal=:paypal WHERE project=:project";
-            self::query($sql, [':project' => $this->project, ':paypal' => $this->paypal]);
 			return true;
 		} catch(\PDOException $e) {
 			$errors[] = "Las cuentas no se han asignado correctamente. Por favor, revise los datos." . $e->getMessage();
@@ -84,28 +95,18 @@ class Account extends \Goteo\Core\Model {
 
 	}
 
-    // comprobar, para aportar con PayPal tiene que tener puesta la cuenta
-    public static function getAllowpp ($id) {
-
-        try {
-            $query = static::query("SELECT paypal FROM project_account WHERE project = ?", array($id));
-            $paypal = $query->fetchColumn();
-            return (!empty($paypal));
-        } catch(\PDOException $e) {
-            return false;
-        }
-
-        /*
-         * Esto al final no se usa, se podrá aprovechar el campo para marcar que la cuenta está verificada (o algo así)
+    /**
+     * @throws Exception
+     */
+    public static function getAllowpp (string $id): bool
+    {
         try {
             $query = static::query("SELECT allowpp FROM project_account WHERE project = ?", array($id));
-            $allowpp = $query->fetchColumn(0);
-            return $allowpp;
+            return $query->fetchColumn();
         } catch(\PDOException $e) {
-			throw new \Goteo\Core\Exception($e->getMessage());
+			throw new Exception($e->getMessage());
             return false;
         }
-        */
     }
 
 
