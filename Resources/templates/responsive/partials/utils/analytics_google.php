80966b15b21b601cb00d0f2fec0c4b97d192af2d
David Igón <david@goteo.org>
2022-09-27 16:15:20 +0200

    add new google analytics

--- Resources/templates/responsive/partials/utils/analytics_google.php
+++ Resources/templates/responsive/partials/utils/analytics_google.php
@@ -2,22 +2,27 @@
 if($google = $this->a('google')):
     $id = array_shift($google);
 ?>
-<script type="text/javascript">
-    // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    <script type="text/javascript">
+        // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt
 
-    // Unamed first tracker
-    ga('create', '<?= $id ?>', 'auto');
-    ga('send', 'pageview');
+        <?php if (preg_match("/\bUA-\d{4,10}-\d{1,4}\b/", $id)): ?>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            // Unamed first tracker
+            ga('create', '<?= $id ?>', 'auto');
+            ga('send', 'pageview');
+        <?php endif; ?>
 
 <?php if($google): ?>
     <?php foreach($google as $k => $id): ?>
-        ga('create', '<?= $id ?>', 'auto', 'tracker<?= $k ?>');
-        ga('tracker<?= $k ?>.send', 'pageview');
+        <?php if (preg_match("\bUA-\d{4,10}-\d{1,4}\b", $id)): ?>
+            ga('create', '<?= $id ?>', 'auto', 'tracker<?= $k ?>');
+            ga('tracker<?= $k ?>.send', 'pageview');
+        <?php endif; ?>
     <?php endforeach ?>
 
     // Pronto ajax compatibility for named analytics
@@ -28,8 +33,10 @@ if($google = $this->a('google')):
             // Strip domain
             url = url.replace(window.location.protocol + "//" + window.location.host, "");
             <?php foreach($google as $k => $i): ?>
-                console.log('tracker', url, 'tracker<?= $k ?>', e);
-                ga('tracker<?= $k ?>'+'.send', 'pageview', url);
+                 <?php if (preg_match("\bUA-\d{4,10}-\d{1,4}\b", $id)): ?>
+                    console.log('tracker', url, 'tracker<?= $k ?>', e);
+                    ga('tracker<?= $k ?>'+'.send', 'pageview', url);
+                <?php endif; ?>
             <?php endforeach ?>
          });
      });
