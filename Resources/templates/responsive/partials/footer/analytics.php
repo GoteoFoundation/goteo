794ac39a64717069083a5c94535de65410c1495a
David <david@goteo.org>
2022-09-28 15:27:02 +0200

    Add new analytics gtag (#482)
    * add new google analytics
    
    * fixes in regex for google analytics codes
    
    * change in references to google analytics format
    
    * fixes in yml files

--- Resources/templates/responsive/partials/footer/analytics.php
+++ Resources/templates/responsive/partials/footer/analytics.php
@@ -1,5 +1,9 @@
 <?php
 
+echo $this->insert('partials/utils/gtag', [
+    'google' => $this->get_config('analytics.google')
+]);
+
 echo $this->insert('partials/utils/analytics_google', [
   'google' => $this->get_config('analytics.google')
 ]);
