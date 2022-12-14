8de5295a23accffa8cf23b1c3696f1ddb0569983
David Igón <david.igon@riseup.net>
2022-12-15 00:12:15 +0100

    add footprints to project partial impact by footprint

--- Resources/templates/responsive/project/partials/impact_by_footprint.php
+++ Resources/templates/responsive/project/partials/impact_by_footprint.php
@@ -29,5 +29,57 @@
                 </div>
             </div>
         </article>
+        <article class="card-impact-by-footprint footprint-2">
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <i class="icon icon-person"></i>
+                </div>
+                <div class="col-md-9 col-xs-9">
+                    <p>3 puestos de trabajo generados con un coste de 36.000€</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <i class="icon icon-money-bag"></i>
+                </div>
+                <div class="col-md-9 col-xs-9">
+                    <p>920 horas voluntarias que aportan el equivalente a 23000€ de valor económico al proyecto</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <i class="icon icon-medal"></i>
+                </div>
+                <div class="col-md-9 col-xs-9">
+                    <p>8 personas formadas con un coste de 7500€</p>
+                </div>
+            </div>
+        </article>
+        <article class="card-impact-by-footprint footprint-3">
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <i class="icon icon-person"></i>
+                </div>
+                <div class="col-md-9 col-xs-9">
+                    <p>3 puestos de trabajo generados con un coste de 36.000€</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <i class="icon icon-money-bag"></i>
+                </div>
+                <div class="col-md-9 col-xs-9">
+                    <p>920 horas voluntarias que aportan el equivalente a 23000€ de valor económico al proyecto</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <i class="icon icon-medal"></i>
+                </div>
+                <div class="col-md-9 col-xs-9">
+                    <p>8 personas formadas con un coste de 7500€</p>
+                </div>
+            </div>
+        </article>
     </div>
 </div>
