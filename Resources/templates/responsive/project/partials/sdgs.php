63ca2f7dc9154378b743565a1b2f91b89e490550
David Igón <david.igon@riseup.net>
2022-12-14 12:45:44 +0100

    improve style in project sdgs partial

--- Resources/templates/responsive/project/partials/sdgs.php
+++ Resources/templates/responsive/project/partials/sdgs.php
@@ -4,12 +4,12 @@
 <article class="card-sdgs">
     <h3>El proyecto ayuda al cumplimiento de los siguientes ODS</h3>
     <div class="col-md-4">
-        <img src="//goteo.test:8081/assets/img/sdg/sdg5.svg" alt="Igualtat de gènere" width="100%">
+        <img src="//goteo.test:8081/assets/img/sdg/sdg5.svg" alt="Igualtat de gènere" width="90px">
     </div>
     <div class="col-md-4">
-        <img src="//goteo.test:8081/assets/img/sdg/sdg10.svg" alt="Reducció de la desigualtat" width="100%">
+        <img src="//goteo.test:8081/assets/img/sdg/sdg10.svg" alt="Reducció de la desigualtat" width="90px">
     </div>
     <div class="col-md-4">
-        <img src="//goteo.test:8081/assets/img/sdg/sdg10.svg" alt="Reducció de la desigualtat" width="100%">
+        <img src="//goteo.test:8081/assets/img/sdg/sdg10.svg" alt="Reducció de la desigualtat" width="90px">
     </div>
 </article>
