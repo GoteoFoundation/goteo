012491574b7a843b8543c0353e90eb54ef328677
Daniel <subiabrewd@gmail.com>
2023-02-22 17:07:47 +0100

    Start new filter as plugin, I hope this approach works lmao

---
+++ extend/filter-popular/Resources/templates/responsive/home/partials/projects_nav.php
@@ -0,0 +1,14 @@
+<ul class="project-filters list-inline center-block text-center">
+    <li data-status="promoted" class="active">
+        <?= $this->text('home-projects-team-favourites') ?>
+    </li>
+    <li data-status="outdated">
+        <?= $this->text('home-projects-outdate') ?>
+    </li>
+    <li data-status="well-liked">
+        <?= $this->text('home-projects-well-liked') ?>
+    </li>
+    <li data-status="recent">
+        <?= $this->text('discover-group-recent-header') ?>
+    </li>
+</ul>
