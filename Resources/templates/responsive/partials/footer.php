c01468a0ce0fcfdacb69bf6000ffd5e6a4f47d2c
David Igón <davidbenabarre@platoniq.net>
2023-03-17 16:33:20 +0100

    add footer reference to platoniq foundation

--- Resources/templates/responsive/partials/footer.php
+++ Resources/templates/responsive/partials/footer.php
@@ -17,7 +17,7 @@
             <div class="col-sm-4 hidden-xs">
               <ul class="list-inline text-right">
                 <li class="label-img"><a href="#"><?=$this->text('footer-platoniq-iniciative') ?></a></li>
-                <li><a href="<?= $this->lang_host() ?>"><img src="<?= $this->asset('/img/platoniq.svg') ?>" class="img-responsive logo" alt="Fundación Platoniq" width="200"></a></li>
+                <li><a href="<?= $this->lang_host() ?>"><img src="<?= $this->asset('/img/platoniq.svg') ?>" class="img-responsive logo" alt="<?= $this->t('footer-platoniq-foundation') ?>" width="200"></a></li>
               </ul>
             </div>
         </div>
