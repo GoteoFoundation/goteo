25ca3e12a025024bac604c1a592e3c1b326fb845
fabou.diawaraeso@gmail.com <fabou.diawaraeso@gmail.com>
2023-04-05 17:02:13 +0200

    En el home.yml añadimos popular y en proects_nav le hacemos referencia

--- Resources/templates/responsive/home/partials/projects_nav.php
+++ Resources/templates/responsive/home/partials/projects_nav.php
@@ -5,8 +5,11 @@
     <li data-status="outdated">
         <?= $this->text('home-projects-outdate') ?>
     </li>
-    <li data-status="near">
-        <?= $this->text('home-projects-near') ?>
+    <!--<li data-status="near"> Eliminamos del navegador el cerca de ti y añadimos el popular
+    </li>
+    -->
+    <li data-status="popular">
+                <?= $this->text('home-projects-popular') ?>
     </li>
     <li data-status="recent">
         <?= $this->text('discover-group-recent-header') ?>
