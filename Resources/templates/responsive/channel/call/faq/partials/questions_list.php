6bd41062c221f3b9526e9b479e6f593f21eaba71
David Igón <davidbenabarre@platoniq.net>
2023-11-09 17:36:31 +0100

    fix styles for channel call faqs

--- Resources/templates/responsive/channel/call/faq/partials/questions_list.php
+++ Resources/templates/responsive/channel/call/faq/partials/questions_list.php
@@ -1,24 +1,22 @@
 <section class="section faq-list">
     <div class="container">
-        <div class="row">
+        <div class="channel-faqs">
             <?php foreach ($this->nodeFaqs as $faq): ?>
-                <article class="col-sm-6 col-lg-3">
-                    <div class="faqs_module card">
-                        <header>
-                            <a href="<?= "/channel/{$this->channel->id}/faq/{$faq->slug}" ?>">
-                                <h2 style="<?= $this->colors['primary'] ? "color:".$this->colors['primary'] : '' ?>"><?= $faq->name ?></h2>
-                            </a>
-                        </header>
-                        <ul>
-                            <?php foreach ($faq->getQuestions() as $faqQuestion): ?>
-                                <li>
-                                    <a style="<?= $this->colors['secondary'] ? "color:".$this->colors['secondary'] : '' ?>" href="<?= "/channel/{$this->channel->id}/faq/{$faq->slug}#faq-{$faqQuestion->id}" ?>">
-                                        <?= $faqQuestion->title ?>
-                                    </a>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
+                <article class="faqs_module card">
+                    <header>
+                        <a href="<?= "/channel/{$this->channel->id}/faq/{$faq->slug}" ?>">
+                            <h2 style="<?= $this->colors['primary'] ? "color:".$this->colors['primary'] : '' ?>"><?= $faq->name ?></h2>
+                        </a>
+                    </header>
+                    <ul>
+                        <?php foreach ($faq->getQuestions() as $faqQuestion): ?>
+                            <li>
+                                <a style="<?= $this->colors['secondary'] ? "color:".$this->colors['secondary'] : '' ?>" href="<?= "/channel/{$this->channel->id}/faq/{$faq->slug}#faq-{$faqQuestion->id}" ?>">
+                                    <?= $faqQuestion->title ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
                 </article>
             <?php endforeach; ?>
         </div>
