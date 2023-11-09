9a096cf3dc70917578e8fa092dd2af34e156e8e7
David Igón <davidbenabarre@platoniq.net>
2023-11-09 16:59:33 +0100

    clean channel call faq view

--- Resources/templates/responsive/channel/call/faq.php
+++ Resources/templates/responsive/channel/call/faq.php
@@ -1,18 +1,13 @@
 <?php
-
-//$meta_img = $this->workshop->header_image ? $this->workshop->getHeaderImage()->getLink(700, 700, false, true) : $this->asset('img/blog/header_default.png') ;
-
 $this->layout('channel/call/layout', [
 	'bodyClass' => 'channel-call',
     'title' => ucfirst($this->faq->name).' :: FAQ',
-    'meta_description' => $this->faq->banner_description,
-    'tw_image' => $meta_img
+    'meta_description' => $this->faq->banner_description
     ]);
 
 $this->section('channel-content');
 
 ?>
-
 <?= $this->insert('channel/call/partials/faq/banner') ?>
 
 <?= $this->insert('channel/call/partials/faq/questions') ?>
