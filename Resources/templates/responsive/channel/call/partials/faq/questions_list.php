d0110af8646c67cad6c4e8d564719ccfc5a33099
David Igón <davidbenabarre@platoniq.net>
2023-11-09 17:00:38 +0100

    add new action to see all faqs for a channel

---
+++ Resources/templates/responsive/channel/call/partials/faq/questions_list.php
@@ -0,0 +1,26 @@
+<section class="section faq-list">
+    <div class="container">
+        <div class="row">
+            <?php foreach ($this->nodeFaqs as $faq): ?>
+                <article class="col-sm-6 col-lg-3">
+                    <div class="faqs_module card">
+                        <header>
+                            <a href="<?= "/channel/{$this->channel->id}/faq/{$faq->slug}" ?>">
+                                <h2 style="<?= $this->colors['primary'] ? "color:".$this->colors['primary'] : '' ?>"><?= $faq->name ?></h2>
+                            </a>
+                        </header>
+                        <ul>
+                            <?php foreach ($faq->getQuestions() as $faqQuestion): ?>
+                                <li>
+                                    <a style="<?= $this->colors['secondary'] ? "color:".$this->colors['secondary'] : '' ?>" href="<?= "/channel/{$this->channel->id}/faq/{$faq->slug}#faq-{$faqQuestion->id}" ?>">
+                                        <?= $faqQuestion->title ?>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </article>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
