b2877477a4f7d0a2e9976df6c390f0928e0abe77
Daniel Subiabre <daniel.subiabre@platoniq.net>
2023-11-14 12:40:35 +0100

    Update subscription data vis

--- Resources/templates/responsive/dashboard/subscriptions/cancel.php
+++ Resources/templates/responsive/dashboard/subscriptions/cancel.php
@@ -4,6 +4,7 @@
 
 <div class="dashboard-content">
     <div class="inner-container">
+        <strong><?= $this->t('subscription-cancel-description') ?></strong>
         <?= $this->form_start($form); ?>
         <?= $this->form_row($form["remove"], [], true) ?>
         <?= $this->form_end($form); ?>
--- Resources/templates/responsive/dashboard/subscriptions/subscription.php
+++ Resources/templates/responsive/dashboard/subscriptions/subscription.php
@@ -3,7 +3,39 @@
 
 <div class="dashboard-content">
     <div class="inner-container">
-        <?php var_dump($this->subscription) ?>
+        <?php $data = $this->subscription ?>
+        <?php $product = $data['product'] ?>
+        <?php $subscription = $data['subscription'] ?>
+        <?php $plan = $subscription['plan'] ?>
+        <?php $price = $subscription['items']['data'][0]['price']; ?>
+        <h2><?= $product['description'] ?></h2>
+        <table class="table">
+            <tr>
+                <td><?= $this->t('subscription-data-id') ?></td>
+                <td><?= $subscription['id'] ?></td>
+            </tr>
+            <tr>
+                <td>Creada</td>
+                <td><?= $this->t('subscription-data-start-date') ?></td>
+                <td><?= \date_formater(date('Y-m-d', $subscription['start_date'])) ?></td>
+            </tr>
+            <tr>
+                <td><?= $this->t('subscription-data-valid-until') ?></td>
+                <td><?= \date_formater(date('Y-m-d', $subscription['current_period_end'])) ?></td>
+            </tr>
+            <tr>
+                <td><?= $this->t('subscription-data-description') ?></td>
+                <td><?= $product['description'] ?></td>
+            </tr>
+            <tr>
+                <td><?= $this->t('subscription-data-status') ?></td>
+                <td><?= $subscription['status'] ?></td>
+            </tr>
+            <tr>
+                <td><?= $this->t('subscription-data-price') ?></td>
+                <td><?= \amount_format($price['unit_amount'] / 100) . '/' . $plan['interval'] ?></td>
+            </tr>
+        </table>
     </div>
 </div>
 
