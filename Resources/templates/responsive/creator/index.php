8461ed5803349ef1582d4fed91801ac41bf3ca97
David Igón <davidbenabarre@platoniq.net>
2023-11-27 11:53:37 +0100

    add links to share creator landing page

--- Resources/templates/responsive/creator/index.php
+++ Resources/templates/responsive/creator/index.php
@@ -4,6 +4,24 @@
 
 $permanentProject = $this->permanentProject;
 $listOfProjects = $this->listOfProjects;
+
+$share_url = $this->get_url() . '/creator/' . $this->user->id;
+
+$user_twitter = str_replace(
+    [
+        'https://',
+        'http://',
+        'www.',
+        'twitter.com/',
+        '#!/',
+        '@'
+    ], '', $this->user->twitter);
+$author_share = !empty($user_twitter) ? ' '.$this->text('regular-by').' @'.$user_twitter.' ' : '';
+$share_title = $author_share;
+
+$facebook_url = 'http://facebook.com/sharer.php?u=' . urlencode($share_url) . '&t=' . urlencode($share_title);
+$twitter_url = 'http://twitter.com/intent/tweet?text=' . urlencode($share_title . ': ' . $share_url . ' #Goteo');
+
 ?>
 
 <?php $this->section('head'); ?>
@@ -24,6 +42,14 @@ $listOfProjects = $this->listOfProjects;
                 <?= $this->insert('project/partials/media', ['project' => $permanentProject ]) ?>
             </div>
         </div>
+
+        <section class="share-social">
+            <ul class="info-extra list-inline">
+                <li><a class="fa fa-2x fa-twitter" title="" target="_blank" href="<?= $twitter_url ?>"></a></li>
+                <li><a class="fa fa-2x fa-facebook" title="" target="_blank" href="<?= $facebook_url ?>"></a></li>
+                <li><a class="fa fa-2x fa-telegram" title="" target="_blank" href="https://telegram.me/share/url?url=<?= $share_url ?>&text=<?= urlencode($share_title) ?>"></a></li>
+            </ul>
+        </section>
 
         <?= $this->insert('creator/partials/subscriptions', ['project' => $permanentProject, 'subscriptions' => $permanentProject->getRewardsOrderBySubscribable()]) ?>
 
