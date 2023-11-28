5084a2881052322687d27cb7e54f982a274e55b3
David Igón <davidbenabarre@platoniq.net>
2023-11-28 09:58:24 +0100

    add styles to slider arrows in creator subscriptions

--- Resources/templates/responsive/creator/partials/channel.php
+++ Resources/templates/responsive/creator/partials/channel.php
@@ -9,7 +9,7 @@
     <div class="slider slider-channel">
         <?php foreach($this->channels as $channel): ?>
             <article class="channel">
-                <a href="/chanel/<?= $channel->id ?>">
+                <a href="/channel/<?= $channel->id ?>">
                     <img width="100" height="100" src="<?= $channel->getLogo()->getLink(100, 0, false) ?>" alt="<?= $channel->name ?>" title="<?= $channel->name ?>">
                 </a>
             </article>
