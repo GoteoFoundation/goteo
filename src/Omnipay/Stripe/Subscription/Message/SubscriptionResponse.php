eb5953e367e30b0b47a89a3508bf49c6dafbd65c
Daniel Subiabre <daniel.subiabre@platoniq.net>
2023-12-11 15:26:04 +0100

    Use stripe to validate success on payment response

--- src/Omnipay/Stripe/Subscription/Message/SubscriptionResponse.php
+++ src/Omnipay/Stripe/Subscription/Message/SubscriptionResponse.php
@@ -2,14 +2,30 @@
 
 namespace Omnipay\Stripe\Subscription\Message;
 
+use Goteo\Application\Config;
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
+use Omnipay\Common\Message\RequestInterface;
+use Stripe\Checkout\Session as StripeSession;
+use Stripe\StripeClient;
 
 class SubscriptionResponse extends AbstractResponse implements RedirectResponseInterface
 {
+    private StripeClient $stripe;
+
+    private StripeSession $checkout;
+
+    public function __construct(RequestInterface $request, string $checkoutSessionId)
+    {
+        parent::__construct($request, $checkoutSessionId);
+
+        $this->stripe = new StripeClient(Config::get('payments.stripe.secretKey'));
+        $this->checkout = $this->stripe->checkout->sessions->retrieve($checkoutSessionId);
+    }
+
     public function isSuccessful()
     {
-        return $this->data['success'];
+        return $this->checkout->status === StripeSession::STATUS_COMPLETE;
     }
 
     public function isRedirect()
@@ -19,6 +35,6 @@ class SubscriptionResponse extends AbstractResponse implements RedirectResponseI
 
     public function getRedirectUrl()
     {
-        return $this->data->url;
+        return $this->checkout->url;
     }
 }
