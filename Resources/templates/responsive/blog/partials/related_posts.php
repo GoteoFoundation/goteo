199c07a3613ac5253939478c96ea6730e5076cef
David <david@goteo.org>
2023-12-11 16:25:51 +0100

    fix iframe on blog (#585)

--- Resources/templates/responsive/blog/partials/related_posts.php
+++ Resources/templates/responsive/blog/partials/related_posts.php
@@ -4,7 +4,7 @@
             <h2 class="title"><?= $this->text('blog-related-posts') ?></h2>
 			<div class="row">
 				<?php foreach($this->related_posts as $related_post): ?>
-					<div class="col-md-4 related-post">
+					<div class="col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 related-post spacer-20">
                         <?= $this->insert('post/widgets/normal', [
                             'post' => $related_post
                         ]) ?>
