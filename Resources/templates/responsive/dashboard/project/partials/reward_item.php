033203007f495dbbe539444b7e90aaa53ede655c
David Igón <davidbenabarre@platoniq.net>
2023-12-14 15:42:15 +0100

    only show subscribable input if user is admin

--- Resources/templates/responsive/dashboard/project/partials/reward_item.php
+++ Resources/templates/responsive/dashboard/project/partials/reward_item.php
@@ -15,15 +15,17 @@ $unlimited = (int)$form["units_$id"]->vars['value'] == 0;
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="subscribable form-group">
-                <label for="subscribable_<?= $id ?>"><?= $this->text('project-reward-subscribable') ?></label>
-                <?= $this->text('project-reward-stripe-based-subscriptions') ?>
-                <div class="input-wrap">
-                    <?= $this->form_row($form["subscribable_$id"]) ?>
+        <?php if ($this->is_admin()):?>
+            <div class="row">
+                <div class="subscribable form-group">
+                    <label for="subscribable_<?= $id ?>"><?= $this->text('project-reward-subscribable') ?></label>
+                    <?= $this->text('project-reward-stripe-based-subscriptions') ?>
+                    <div class="input-wrap">
+                        <?= $this->form_row($form["subscribable_$id"]) ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php endif; ?>
         <?= $this->form_row($form["reward_$id"]) ?>
         <?= $this->form_row($form["description_$id"]) ?>
         <div class="remove"><?= $this->form_row($form["remove_$id"], [],  true) ?></div>
