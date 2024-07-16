af660b66eea405e4e6a7c785cec1b463110a2687
David <david@goteo.org>
2024-07-16 14:04:27 +0200

    Fix node project get list return type (#615)
    * make nodeproject get list return not only arrays
    
    * change php doc definition of getList function

--- src/Goteo/Model/Node/NodeProject.php
+++ src/Goteo/Model/Node/NodeProject.php
@@ -25,9 +25,9 @@ class NodeProject extends Model {
     }
 
     /**
-     * @return NodeProject[]
+     * @return NodeProject[] | int
      */
-    static public function getList(array $filters = [], int $offset = 0, int $limit = 10, bool $count = false, string $lang = null): array
+    static public function getList(array $filters = [], int $offset = 0, int $limit = 10, bool $count = false, string $lang = null)
     {
         $filter = [];
         $values = [];
