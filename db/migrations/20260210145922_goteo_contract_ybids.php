20ef8c02d5037550e199fc70e06d558cbdedad0a
Daniel Subiabre García <subiabrewd@gmail.com>
2026-02-17 11:10:21 +0100

    feat: Contracts with year-based ids (#656)
    * fix: code style of Contract.php
    
    * feat: add migration for ybids in contracts table
    
    * feat: generate ybid value for new contracts
    
    * fix: show ybid in project report
    
    * fix: ybid count sequence bad type and no calc
    
    * feat: search charges by contract number or ybid in same filter

---
+++ db/migrations/20260210145922_goteo_contract_ybids.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Migration Task class.
+ */
+class GoteoContractYbids
+{
+  public function preUp()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postUp()
+  {
+      // add the post-migration code here
+  }
+
+  public function preDown()
+  {
+      // add the pre-migration code here
+  }
+
+  public function postDown()
+  {
+      // add the post-migration code here
+  }
+
+  /**
+   * Return the SQL statements for the Up migration
+   *
+   * @return string The SQL string to execute for the Up migration.
+   */
+  public function getUpSQL()
+  {
+     return "ALTER TABLE `contract` ADD COLUMN `ybid` VARCHAR(255) DEFAULT NULL";
+  }
+
+  /**
+   * Return the SQL statements for the Down migration
+   *
+   * @return string The SQL string to execute for the Down migration.
+   */
+  public function getDownSQL()
+  {
+     return "ALTER TABLE `contract` DROP COLUMN `ybid`";
+  }
+
+}
