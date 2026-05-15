c8fd1566ec51e55ca53d4f65353728c8e80f706a
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-05-15 14:05:47 +0200

    feat: add metricool analytics partial

---
+++ Resources/templates/responsive/partials/utils/analytics_metricool.php
@@ -0,0 +1,14 @@
+<!-- Metricool -->
+<script>
+    function loadScript(a) {
+        var b = document.getElementsByTagName("head")[0],
+            c = document.createElement("script");
+        c.type = "text/javascript", c.src = "https://tracker.metricool.com/resources/be.js", c.onreadystatechange = a, c.onload = a, b.appendChild(c)
+    }
+    loadScript(function() {
+        beTracker.t({
+            hash: "392185c313af0d18cf57bb3f0ae54f87"
+        })
+    });
+</script>
+<!-- End Metricool -->
